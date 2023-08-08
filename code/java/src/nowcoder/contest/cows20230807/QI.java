5ead5f07a13f339b49a3480863ba3b63ef46cb61
Rocky Haotian Du <2712479005@qq.com>
2023-08-08 17:25:33 +0800

    Updated

---
+++ code/java/src/nowcoder/contest/cows20230807/QI.java
@@ -0,0 +1,92 @@
+package nowcoder.contest.cows20230807;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class QI {
+    static final int CONST = 401, MOD = 998244353, ZERO_ONE = '0' + '1';
+    static List<List<String>> stringSet;
+
+    static int count(String s) {
+        int res = 0;
+        for (char ch : s.toCharArray()) {
+            if ('?' == ch) {
+                ++res;
+            }
+        }
+        return res;
+    }
+
+    static void addNewString(String sb) {
+        int len = sb.length(), a, b;
+        List<String> subSet = stringSet.get(len);
+        String sa;
+        boolean match, undone = true;
+        for (int i = 0; i < subSet.size(); i++) {
+            sa = subSet.get(i);
+            match = true;
+            for (int j = 0; j < len; j++) {
+                if (ZERO_ONE == sa.charAt(j) + sb.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                a = count(sa);
+                b = count(sb);
+                if (b > a) {
+                    subSet.remove(i);
+                    subSet.add(sb);
+                }
+                undone = false;
+                break;
+            }
+        }
+        if (undone) {
+            subSet.add(sb);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        stringSet = new ArrayList<>(CONST);
+        for (int i = 0; i < CONST; i++) {
+            stringSet.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            addNewString(scanner.next());
+        }
+
+        long res = 0;
+        for (List<String> subSet : stringSet) {
+            for (String sa : subSet) {
+//                res += fastExponentiation(count(sa));
+                res += (long) Math.pow(2, count(sa));
+                res %= MOD;
+            }
+        }
+        System.out.println(res);
+    }
+
+    static long fastExponentiation(int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+
+        long result = 1;
+        long baseValue = 2;
+        while (exponent > 0) {
+            if (1 == (1 & exponent)) {
+                result *= baseValue;
+            }
+            baseValue <<= 1;
+            exponent >>= 1;
+        }
+
+        return result;
+    }
+}
---
+++ code/java/src/nowcoder/contest/cows20230807/QM.java
@@ -0,0 +1,41 @@
+package nowcoder.contest.cows20230807;
+
+import java.util.Scanner;
+
+class QM {
+    static int pow(int x) {
+        int res = 1;
+        for (int i = 1; i < x; i++) {
+            res *= 10;
+        }
+        return res;
+    }
+
+    static int pow1(int x) {
+        int res = 0;
+        for (int i = -1; i < x; i++) {
+            res = res * 10 + 9;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+        int n, num, i;
+        long res;
+        while (0 < t--) {
+            n = scanner.nextInt();
+            res = 0;
+            num = 9;
+            for (i = 1; num < n; i++) {
+                res += 9L * i * pow(i);
+                num = pow1(i);
+            }
+            res += (long) (n - pow1(i - 2)) * i;
+
+            System.out.println(res);
+        }
+    }
+}
