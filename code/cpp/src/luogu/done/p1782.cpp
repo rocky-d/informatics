465ba0a6f12c4bc635dfa7e37ff63f48882fc685
Rocky Haotian Du <2712479005@qq.com>
2023-08-10 19:05:59 +0800

    Updated

---
+++ code/cpp/src/luogu/done/p1782.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    long i, j, n, m, c, vv, ww, dd, count, v, w, aa, bb, cc, x;
+    cin >> n >> m >> c;
+
+    vector<long> dp(c + 1, 0);
+
+    for (i = 0; i < n; i++) {
+        cin >> vv >> ww >> dd;
+        count = 1;
+        while (dd > count) {
+            dd -= count;
+            v = count * vv;
+            w = count * ww;
+            for (j = c; j >= v; j--) {
+                dp[j] = max(dp[j], dp[j - v] + w);
+            }
+            count <<= 1;
+        }
+        v = dd * vv;
+        w = dd * ww;
+        for (j = c; j >= v; j--) {
+            dp[j] = max(dp[j], dp[j - v] + w);
+        }
+    }
+
+    for (i = 0; i < m; i++) {
+        cin >> aa >> bb >> cc;
+        for (j = c; j > -1; j--) {
+            for (x = 0; x <= j; x++) {
+                dp[j] = max(dp[j], dp[j - x] + aa * x * x + bb * x + cc);
+            }
+        }
+    }
+
+    cout << dp[c] << endl;
+
+    return 0;
+}
---
+++ code/py/src/luogu/undone/p1782.py
@@ -0,0 +1,31 @@
+def main():
+    n, m, c = map(int, input().split())
+
+    dp = [0 for _ in range(c + 1)]
+
+    for _ in range(n):
+        vv, ww, dd = map(int, input().split())
+        count = 1
+        while dd > count:
+            dd -= count
+            v = count * vv
+            w = count * ww
+            for j in range(c, v - 1, -1):
+                dp[j] = max(dp[j], dp[j - v] + w)
+            count <<= 1
+        v = dd * vv
+        w = dd * ww
+        for j in range(c, v - 1, -1):
+            dp[j] = max(dp[j], dp[j - v] + w)
+
+    for _ in range(m):
+        aa, bb, cc = map(int, input().split())
+        for j in range(c, -1, -1):
+            for x in range(j + 1):
+                dp[j] = max(dp[j], dp[j - x] + aa * x ** 2 + bb * x + cc)
+
+    print(dp[c])
+
+
+if __name__ == '__main__':
+    main()
