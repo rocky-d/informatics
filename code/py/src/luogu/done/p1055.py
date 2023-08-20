5407732e35228cd57766801247e27d6da036f052
Rocky Haotian Du <2712479005@qq.com>
2023-08-20 16:06:50 +0800

    Updated

--- code/py/src/luogu/done/p1055.py
+++ code/py/src/luogu/done/p1055.py
@@ -1,8 +1,8 @@
 def main():
     isbn = input()
-    id = eval(f'({isbn[0]}+2*{isbn[2]}+3*{isbn[3]}+4*{isbn[4]}+5*{isbn[6]}+6*{isbn[7]}+7*{isbn[8]}+8*{isbn[9]}+9*{isbn[10]})%11')
-    id = 'X' if 10 == id else str(id)
-    print('Right' if id == isbn[-1] else isbn[:12] + id)
+    isbn_id = eval(f'({isbn[0]}+2*{isbn[2]}+3*{isbn[3]}+4*{isbn[4]}+5*{isbn[6]}+6*{isbn[7]}+7*{isbn[8]}+8*{isbn[9]}+9*{isbn[10]})%11')
+    isbn_id = 'X' if 10 == isbn_id else str(isbn_id)
+    print('Right' if isbn_id == isbn[-1] else isbn[:12] + isbn_id)
 
 
 if __name__ == '__main__':
