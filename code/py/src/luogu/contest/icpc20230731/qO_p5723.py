f8d1a2cee768d67f64e09b4e42205452eff18b6a
Rocky Haotian Du <2712479005@qq.com>
2023-08-27 11:11:26 +0800

    Updated

--- code/py/src/luogu/contest/icpc20230731/qO_p5723.py
+++ code/py/src/luogu/contest/icpc20230731/qO_p5723.py
@@ -1,5 +1,40 @@
-def main():
-    ...
+def get_primes(n: int) -> list:
+    primes = []
+    is_prime = [True for _ in range(n + 1)]
+    for i in range(2, n + 1):
+        if is_prime[i]:
+            primes.append(i)
+        for p in primes:
+            if i * p > n:
+                break
+            is_prime[i * p] = False
+            if i % p == 0:
+                break
+    return primes
+
+
+def main() -> None:
+    l = int(input())
+
+    total = 0
+    primes = []
+    is_prime = [True for _ in range(l + 2)]
+
+    for i in range(2, l + 1):
+        if is_prime[i]:
+            total += i
+            if total > l:
+                break
+            else:
+                print(i)
+                primes.append(i)
+        for p in primes:
+            if l < i * p:
+                break
+            is_prime[i * p] = False
+            if 0 == i % p:
+                break
+    print(len(primes))
 
 
 if __name__ == '__main__':
