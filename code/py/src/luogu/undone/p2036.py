aa014d4252e6bbe004a9a75af9fc97ed3170c0c6
Rocky Haotian Du <2712479005@qq.com>
2023-08-29 21:58:30 +0800

    Updated

--- code/py/src/luogu/undone/p2036.py
+++ code/py/src/luogu/undone/p2036.py
@@ -1,17 +1,22 @@
+class Solution:
+    def __init__(self) -> None:
+        self.res = int(1e9)
+        self.n = int(input())
+        self.sb = [list(map(int, input().split())) for _ in range(self.n)]
+
+    def dfs(self, i: int, s: int, b: int) -> None:
+        if i == self.n:
+            if not (s == 1 and b == 0):
+                self.res = min(self.res, abs(s - b))
+        else:
+            self.dfs(i + 1, s * self.sb[i][0], b + self.sb[i][1])
+            self.dfs(i + 1, s, b)
+
+
 def main() -> None:
-    n = int(input())
-    # input()
-    sb_table = [list(map(int, input().split())) for _ in range(n)]
-    print(sb_table)
-
-    for i in range(n):
-        init = sb_table[i]
-        stack = [i]
-        while stack:
-            vertex = stack.pop()
-            print(vertex)
-            # for nei in range(vertex + 1, )
-
+    solution = Solution()
+    solution.dfs(0, 1, 0)
+    print(solution.res)
 
 
 if __name__ == '__main__':
