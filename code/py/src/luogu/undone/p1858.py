ac346d9ad198f094316867baf7211ce06c100004
Rocky Haotian Du <2712479005@qq.com>
2023-09-06 22:05:01 +0800

    Updated

--- code/py/src/luogu/undone/p1858.py
+++ code/py/src/luogu/undone/p1858.py
@@ -1,9 +1,28 @@
 def main():
     k, v, n = map(int, input().split())
 
-    dp = [0 for _ in range(1 + v)]
+    dp = [[-99999999 for __ in range(60)] for _ in range(1 + v)]
+    dp[0][1] = 0
+    rnk = [0 for _ in range(2 + k)]
 
+    for _ in range(n):
+        a, b = map(int, input().split())
+        for j in range(v, a - 1, -1):
+            rn1 = 1
+            rn2 = 1
+            cnt = 0
+            while cnt <= k:
+                cnt += 1
+                if dp[j][rn1] > dp[j - a][rn2] + b:
+                    rnk[cnt] = dp[j][rn1]
+                    rn1 += 1
+                else:
+                    rnk[cnt] = dp[j - a][rn2] + b
+                    rn2 += 1
+                for h in range(1, k + 1):
+                    dp[j][h] = rnk[h]
 
+    print(sum(dp[v][1: k + 1]))
 
 
 if __name__ == '__main__':
