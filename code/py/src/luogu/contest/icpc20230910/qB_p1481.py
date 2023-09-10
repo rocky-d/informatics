7089c6fd32a22d03d42072cb4fde678a95a872f9
Rocky Haotian Du <2712479005@qq.com>
2023-09-10 15:06:25 +0800

    Updated

--- code/py/src/luogu/contest/icpc20230910/qB_p1481.py
+++ code/py/src/luogu/contest/icpc20230910/qB_p1481.py
@@ -3,9 +3,10 @@ def main() -> None:
     words = [input()[:-1] for _ in range(n)]
     dp = [1 for _ in range(n)]
     for i in range(n):
+        prefix = words[i]
         tag = False
         for j in range(i + 1, n):
-            if words[j].startswith(words[i]):
+            if words[j].startswith(prefix):
                 dp[j] += 1
                 tag = True
             elif tag:
