025611734330866fd30d9fdda1350f9516cddfef
Rocky Haotian Du <2712479005@qq.com>
2023-09-17 19:56:32 +0800

    Updated

---
+++ code/py/src/leetcode/done/p27.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        res = len_nums = len(nums)
+        j = len_nums - 1
+        for i in range(len_nums):
+            if nums[i] == val:
+                while -1 < j and nums[j] == val:
+                    j -= 1
+                if j < i:
+                    res = i
+                    break
+                else:
+                    tmp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = tmp
+        return res
+
+
+print(Solution.removeElement(..., [3, 2, 2, 3], 3))
