cb580b004f7bf221fe4215015b4f09e3daeed009
Rocky Haotian Du <2712479005@qq.com>
2023-09-18 21:28:16 +0800

    Updated

--- code/py/src/leetcode/undone/p24.py
+++ code/py/src/leetcode/undone/p24.py
@@ -1,3 +1,4 @@
+# Definition for singly-linked list.
 from typing import Optional
 
 
@@ -9,4 +10,26 @@ class ListNode:
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        res = head.next
+        old_head = head
+        while head is not None and old_head.next is not None:
+            # print(head.val)
+            old_head = head
+            old_next = head.next
+            head.next = old_head.next.next.next
+            old_next.next = head
+            head = head.next
+            # output(head)
+        output(head)
+        return res
+# 3, 4, None
+
+def output(head):
+    while head is not None:
+        print(head.val, end = ' > ')
+        head = head.next
+    print('')
+
+
+ls = ListNode(1, ListNode(2, ListNode(3, ListNode(4, None))))
+output(Solution.swapPairs(None, ls))
