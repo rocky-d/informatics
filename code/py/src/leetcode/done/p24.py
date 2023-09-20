a5f6ab7c90c44e20211f63e92851265a26c8bc10
Rocky Haotian Du <2712479005@qq.com>
2023-09-20 15:42:22 +0800

    Updated

--- code/py/src/leetcode/done/p24.py
+++ code/py/src/leetcode/done/p24.py
@@ -5,16 +5,17 @@ from leetcode.ln import *
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        res = pre_head = ListNode(next = head)
-        while pre_head.next and pre_head.next.next:
-            node1 = pre_head.next
-            node2 = pre_head.next.next
-            pre_head.next = node2
+        res = dummy = ListNode(next = head)
+        while dummy.next and dummy.next.next:
+            node1 = dummy.next
+            node2 = node1.next
+            dummy.next = node2
             node1.next = node2.next
             node2.next = node1
-            pre_head = node1
+            dummy = node1
         return res.next
 
 
-ls = ListNode(1, ListNode(2, ListNode(3, ListNode(4, None))))
-ln_to_list(Solution.swapPairs(..., ls))
+sol = Solution()
+ls = [1, 2, 3, 4, 5]
+print(ln_to_list(sol.swapPairs(list_to_ln(ls))))
---
+++ code/py/src/leetcode/done/p25.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from leetcode.ln import *
+
+
+class Solution:
+    @staticmethod
+    def is_long_enough1(node: Optional[ListNode], length: int) -> bool:
+        if length < 1:
+            return True
+        if node.next:
+            return Solution.is_long_enough1(node.next, length - 1)
+        else:
+            return False
+
+    @staticmethod
+    def is_long_enough2(node: Optional[ListNode], length: int) -> bool:
+        for _ in range(length):
+            node = node.next
+            if not node:
+                return False
+        return True
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        res = dummy = ListNode(next = head)
+        while Solution.is_long_enough1(dummy, k):
+            first = dummy.next
+            node0 = dummy
+            node1 = node0.next
+            node2 = node1.next
+            for _ in range(k - 1):
+                node0 = node1
+                node1 = node2
+                node2 = node2.next
+                node1.next = node0
+            first.next = node2
+            dummy.next = node1
+            dummy = first
+        return res.next
+
+
+sol = Solution()
+ls = [1, 2, 3, 4, 5, 6, 7]
+print(ln_to_list(sol.reverseKGroup(list_to_ln(ls), 3)))
