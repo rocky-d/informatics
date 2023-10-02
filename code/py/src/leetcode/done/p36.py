c1ec421b271fd85418dc52ad521a8fc10bd3c09b
Rocky Haotian Du <2712479005@qq.com>
2023-10-02 14:28:05 +0800

    Updated

--- code/py/src/leetcode/done/p36.py
+++ code/py/src/leetcode/done/p36.py
@@ -3,37 +3,33 @@ from leetcode.util import *
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        empty_ls = [False for _ in range(10)]
+        num_dict = {'.': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 
         for i in range(9):
-            ls = empty_ls.copy()
+            ls = [True, True, True, True, True, True, True, True, True, True]
             for j in range(9):
-                if board[i][j] != '.':
-                    num = int(board[i][j])
-                    if ls[num]:
-                        return False
-                    else:
-                        ls[num] = True
+                num = num_dict[board[i][j]]
+                if num == 0 or ls[num]:
+                    ls[num] = False
+                else:
+                    return False
 
         for j in range(9):
-            ls = empty_ls.copy()
+            ls = [True, True, True, True, True, True, True, True, True, True]
             for i in range(9):
-                if board[i][j] != '.':
-                    num = int(board[i][j])
-                    if ls[num]:
-                        return False
-                    else:
-                        ls[num] = True
+                num = num_dict[board[i][j]]
+                if num == 0 or ls[num]:
+                    ls[num] = False
+                else:
+                    return False
 
-        for i_origin in range(0, 7, 3):
-            for j_origin in range(0, 7, 3):
-                ls = empty_ls.copy()
-                for i in range(i_origin, i_origin + 3):
-                    for j in range(j_origin, j_origin + 3):
-                        if board[i][j] != '.':
-                            num = int(board[i][j])
-                            if ls[num]:
-                                return False
-                            else:
-                                ls[num] = True
+        for i_origin, j_origin in ((0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)):
+            ls = [True, True, True, True, True, True, True, True, True, True]
+            for i in range(i_origin, i_origin + 3):
+                for j in range(j_origin, j_origin + 3):
+                    num = num_dict[board[i][j]]
+                    if num == 0 or ls[num]:
+                        ls[num] = False
+                    else:
+                        return False
         return True
