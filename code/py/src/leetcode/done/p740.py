b7655cbac1e7bab41328ded15e32c2b23298f913
Rocky Haotian Du <2712479005@qq.com>
2023-10-05 21:57:02 +0800

    Updated

---
+++ code/py/src/leetcode/done/p740.py
@@ -0,0 +1,15 @@
+from leetcode.util import *
+
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        nums_dict = {}
+        for num in nums:
+            nums_dict[num] = nums_dict.get(num, 0) + 1
+        nums_order = sorted(nums_dict.keys())
+        last_num = nums_order.pop(0)
+        dp = [0, last_num * nums_dict[last_num]]
+        for num in nums_order:
+            dp.append(max(dp[-1], dp[-2] + num * nums_dict[num]) if last_num + 1 == num else dp[-1] + num * nums_dict[num])
+            last_num = num
+        return dp[-1]
