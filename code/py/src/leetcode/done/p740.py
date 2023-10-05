6d14ec012ec0acf4ed4232dab7cfb80bb6cf00da
Rocky Haotian Du <2712479005@qq.com>
2023-10-05 22:46:09 +0800

    Updated

--- code/py/src/leetcode/done/p740.py
+++ code/py/src/leetcode/done/p740.py
@@ -3,13 +3,29 @@ from leetcode.util import *
 
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        nums_dict = {}
-        for num in nums:
-            nums_dict[num] = nums_dict.get(num, 0) + num
-        nums_order = sorted(nums_dict.keys())
-        last_num = nums_order.pop(0)
-        dp = [0, nums_dict[last_num]]
-        for num in nums_order:
-            dp.append(max(dp[-1], dp[-2] + nums_dict[num]) if last_num + 1 == num else dp[-1] + nums_dict[num])
-            last_num = num
+        nums.sort()
+        index = 0
+        last_num = nums[0]
+        while last_num == nums[index]:
+            index += 1
+            if index == len(nums):
+                return index * last_num
+        dp = [0, index * last_num]
+        cnt = 0
+        curr_num = nums[index]
+        for num in nums[index:]:
+            if curr_num == num:
+                cnt += 1
+            else:
+                dp.append(max(dp[-1], dp[-2] + cnt * curr_num) if last_num + 1 == curr_num else dp[-1] + cnt * curr_num)
+                cnt = 1
+                last_num = curr_num
+                curr_num = num
+        dp.append(max(dp[-1], dp[-2] + cnt * curr_num) if last_num + 1 == curr_num else dp[-1] + cnt * curr_num)
         return dp[-1]
+
+
+sol = Solution()
+
+ls = [3, 4, 2]
+print(sol.deleteAndEarn(ls))
