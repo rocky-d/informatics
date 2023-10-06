62fb1ef93fc315cf9465ca8aa805beeb9ff548be
Rocky Haotian Du <2712479005@qq.com>
2023-10-06 11:22:21 +0800

    Updated

--- code/py/src/leetcode/done/p746.py
+++ code/py/src/leetcode/done/p746.py
@@ -3,7 +3,7 @@ from leetcode.util import *
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = [0, min(cost[0], cost[1])]
+        first, second = 0, min(cost[0], cost[1])
         for i in range(2, len(cost)):
-            dp.append(min(dp[-1] + cost[i], dp[-2] + cost[i - 1]))
-        return dp[-1]
+            first, second = second, min(second + cost[i], first + cost[i - 1])
+        return second
