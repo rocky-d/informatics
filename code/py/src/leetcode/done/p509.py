653e95e4044310e4c0b4919037c38a1169c0cfd3
Rocky Haotian Du <2712479005@qq.com>
2023-10-06 11:12:59 +0800

    Updated

--- code/py/src/leetcode/done/p509.py
+++ code/py/src/leetcode/done/p509.py
@@ -1,6 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp = [0, 1]
+        first, second = 0, 1
         for _ in range(n - 1):
-            dp.append(dp[-1] + dp[-2])
-        return dp[n]
+            first, second = second, first + second
+        return 0 if n == 0 else second
