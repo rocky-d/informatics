92d984b20dad0b88a214bef996f4e95438a92391
Rocky Haotian Du <2712479005@qq.com>
2023-10-06 12:38:33 +0800

    Updated

--- code/py/src/leetcode/done/p740.py
+++ code/py/src/leetcode/done/p740.py
@@ -10,19 +10,19 @@ class Solution:
             index += 1
             if index == len(nums):
                 return index * last_num
-        dp = [0, index * last_num]
+        first, second = 0, index * last_num
         cnt = 0
         curr_num = nums[index]
         for num in nums[index:]:
             if curr_num == num:
                 cnt += 1
             else:
-                dp.append(max(dp[-1], dp[-2] + cnt * curr_num) if last_num + 1 == curr_num else dp[-1] + cnt * curr_num)
+                first, second = second, max(second, first + cnt * curr_num) if last_num + 1 == curr_num else second + cnt * curr_num
                 cnt = 1
                 last_num = curr_num
                 curr_num = num
-        dp.append(max(dp[-1], dp[-2] + cnt * curr_num) if last_num + 1 == curr_num else dp[-1] + cnt * curr_num)
-        return dp[-1]
+        first, second = second, max(second, first + cnt * curr_num) if last_num + 1 == curr_num else second + cnt * curr_num
+        return second
 
 
 sol = Solution()
