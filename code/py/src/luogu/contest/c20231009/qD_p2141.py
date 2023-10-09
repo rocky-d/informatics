6c12720174f7fd48ea783183d4a6d218896fd73b
Rocky Haotian Du <2712479005@qq.com>
2023-10-09 20:38:22 +0800

    Updated

--- code/py/src/luogu/contest/c20231009/qD_p2141.py
+++ code/py/src/luogu/contest/c20231009/qD_p2141.py
@@ -2,10 +2,11 @@ def main() -> None:
     ans = 0
     input()
     nums = sorted(map(int, input().split()))
+    nums_set = set(nums)
     for i, numi in enumerate(nums):
         for j in range(i):
             diff = numi - nums[j]
-            if not diff == nums[j] and diff in nums:
+            if diff != nums[j] and diff in nums_set:
                 ans += 1
                 break
     print(ans)
