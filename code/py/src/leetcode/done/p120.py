0d9b9973a6f89f96244a82cab6a03e729a0450f9
Rocky Haotian Du <2712479005@qq.com>
2023-10-10 19:10:40 +0800

    Updated

--- code/py/src/leetcode/done/p120.py
+++ code/py/src/leetcode/done/p120.py
@@ -3,13 +3,13 @@ from leetcode.util import *
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [[], [triangle[0][0]]]
+        left, right = [], [triangle[0][0]]
         for i in range(1, len(triangle)):
-            dp[0], dp[1] = dp[1], [triangle[i][0] + dp[1][0]]
+            left, right = right, [triangle[i][0] + right[0]]
             for j in range(1, i):
-                dp[1].append(triangle[i][j] + min(dp[0][j - 1], dp[0][j]))
-            dp[1].append(triangle[i][-1] + dp[0][-1])
-        return min(dp[1])
+                right.append(triangle[i][j] + min(left[j - 1], left[j]))
+            right.append(triangle[i][-1] + left[-1])
+        return min(right)
 
 
 sol = Solution()
