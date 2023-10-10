c8000ae9f77762afe364fbc6be7d48896246553b
Rocky Haotian Du <2712479005@qq.com>
2023-10-10 21:07:31 +0800

    Updated

--- code/py/src/leetcode/done/p175.py
+++ code/py/src/leetcode/done/p175.py
@@ -2,15 +2,7 @@ from leetcode.util import *
 
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
-    person['city'] = [None for _ in range(len(person))]
-    person['state'] = [None for _ in range(len(person))]
-    for i, person_id in enumerate(person.loc[:, 'personId']):
-        for j, p_id in enumerate(address.loc[:, 'personId']):
-            if p_id == person_id:
-                person.loc[i, 'city'] = address.loc[j, 'city']
-                person.loc[i, 'state'] = address.loc[j, 'state']
-                break
-    return person.drop('personId', axis = 1)
+    return person.merge(address, on = 'personId', how = 'left')[['firstName', 'lastName', 'city', 'state']]
 
 
 person_df = pd.DataFrame({
