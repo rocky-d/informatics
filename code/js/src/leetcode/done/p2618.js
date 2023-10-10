b599b106e6bc8ae90350c23e6588871ca57cfbc7
Rocky Haotian Du <2712479005@qq.com>
2023-10-10 19:33:04 +0800

    Updated

--- code/js/src/leetcode/done/p2618.js
+++ code/js/src/leetcode/done/p2618.js
@@ -4,13 +4,7 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function (obj, classFunction) {
-    if (obj === null || obj === undefined || !(classFunction instanceof Function))
-        return false;
-    return Object(obj) instanceof classFunction;
+    return obj === null || obj === undefined || !(classFunction instanceof Function) ? false : Object(obj) instanceof classFunction;
 };
-// 作者：__sgf__
-// 链接：https://leetcode.cn/problems/check-if-object-instance-of-class/
-// 来源：力扣（LeetCode）
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 console.log(checkIfInstanceOf(new Date(), Date));
