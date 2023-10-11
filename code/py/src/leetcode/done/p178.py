1837173a17d8f74fc2e5861bff5676634058fa1e
Rocky Haotian Du <2712479005@qq.com>
2023-10-11 20:46:02 +0800

    Updated

--- code/py/src/leetcode/done/p178.py
+++ code/py/src/leetcode/done/p178.py
@@ -2,20 +2,5 @@ from leetcode.util import *
 
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
-    ans = scores.drop('id', axis = 1)
-
-    if 0 == ans.shape[0]:
-        ans['rank'] = []
-        return ans
-
-    ans = ans.sort_values('score', ascending = False)
-    rank_ls = [rank := 1]
-    last_score = ans.iloc[0, 0]
-    for i in range(1, ans.shape[0]):
-        curr_score = ans.iloc[i, 0]
-        if last_score != curr_score:
-            last_score = curr_score
-            rank += 1
-        rank_ls.append(rank)
-    ans['rank'] = rank_ls
-    return ans
+    scores['rank'] = scores['score'].rank(method = 'dense', ascending = False)
+    return scores[['score', 'rank']].sort_values('score', ascending = False)
