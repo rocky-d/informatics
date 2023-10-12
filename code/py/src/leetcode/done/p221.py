85d9f72ccf98157dff98224484f4af7534717074
Rocky Haotian Du <2712479005@qq.com>
2023-10-12 19:32:30 +0800

    Updated

--- code/py/src/leetcode/done/p221.py
+++ code/py/src/leetcode/done/p221.py
@@ -3,18 +3,21 @@ from leetcode.util import *
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m, n = len(matrix), len(matrix[0])
         dp = [list(map(int, matrix[0]))]
-        for i in range(1, len(matrix)):
+        for i in range(1, m):
             dp.append([int(matrix[i][0])])
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
+        ans_sqrt = 1 if any(1 == x for x in dp[0]) or any(1 == dp[i][0] for i in range(1, m)) else 0
+        for i in range(1, m):
+            for j in range(1, n):
                 for k in range(1 + dp[i - 1][j - 1]):
                     if '0' == matrix[i - k][j] or '0' == matrix[i][j - k]:
                         dp[i].append(k)
                         break
                 else:
                     dp[i].append(1 + dp[i - 1][j - 1])
-        return max([max(row) for row in dp]) ** 2
+                ans_sqrt = max(ans_sqrt, dp[i][j])
+        return ans_sqrt ** 2
 
 
 sol = Solution()
