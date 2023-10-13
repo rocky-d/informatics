6a985c99d9a6aaffb3741885af1143a8992c645d
Rocky Haotian Du <2712479005@qq.com>
2023-10-13 09:52:33 +0800

    Updated

--- code/py/src/leetcode/done/p5.py
+++ code/py/src/leetcode/done/p5.py
@@ -1,11 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ''
-        for tail in range(1, len(s) + 1):
-            head = max(0, tail - len(res) - 2)
-            tmp = s[head:tail]
-            if tmp == tmp[::-1]:
-                res = tmp
-            elif tmp[1:] == tmp[:0:-1]:
-                res = tmp[1:]
-        return res
+        ans = s[0]
+        len_ans = len(ans)
+        lens = len(s)
+        dp = [[True for __ in range(_ + 1)] + [False for __ in range(lens - _ - 1)] for _ in range(lens)]
+        for right in range(lens):
+            for left in range(right - 1, -1, -1):
+                if dp[left + 1][right - 1] and s[left] == s[right]:
+                    dp[left][right] = True
+                    s_right = right + 1
+                    if s_right - left > len_ans:
+                        ans = s[left: s_right]
+                        len_ans = len(ans)
+        return ans
+
+
+sol = Solution()
+
+example_s = "babad"
+print(sol.longestPalindrome(example_s))
