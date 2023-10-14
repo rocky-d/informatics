a87acb712a062795c03ce04a0e93995b6896019d
Rocky Haotian Du <2712479005@qq.com>
2023-10-14 21:14:02 +0800

    Updated

--- code/py/src/lanqiao/contest/c20231014/qD_p5130.py
+++ code/py/src/lanqiao/contest/c20231014/qD_p5130.py
@@ -3,23 +3,22 @@ def main() -> None:
     days_left = sorted(set(range(1, n + 1)) - set(map(int, input().split())))
     day_ls = []
     last_day = days_left[0]
-    cnt = 1
+    cnt = 2  # 1 + 1 (extra +1 for saving time)
     for day in days_left[1:]:
         if 1 == day - last_day:
             cnt += 1
         else:
             day_ls.append(cnt)
-            cnt = 1
+            cnt = 2
         last_day = day
     day_ls.append(cnt)
     table = [(lambda k_, s_: (2 ** int(k_), int(s_)))(*input().split()) for _ in range(m)]
 
     ans = 0
     for day in day_ls:
-        day1 = day + 1
-        dp = [0 for _ in range(day1)]
+        dp = [0 for _ in range(day)]
         for k, s in table:
-            for j in range(k, day1):
+            for j in range(k, day):
                 dp[j] = max(dp[j], dp[j - k] + s)
         ans += dp[-1]
     print(ans)
