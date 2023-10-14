e56ade25a5ffb639c6bc5f37df4d1c536a8a1b37
Rocky Haotian Du <2712479005@qq.com>
2023-10-14 11:40:10 +0800

    Updated

--- code/py/src/leetcode/done/p182.py
+++ code/py/src/leetcode/done/p182.py
@@ -2,5 +2,4 @@ from leetcode.util import *
 
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    df = person.groupby('email').size().reset_index(name = 'count')
-    return df[1 < df['count']][['email']]
+    return (lambda df: df[1 < df['count']][['email']])(df = person.groupby('email').size().reset_index(name = 'count'))
