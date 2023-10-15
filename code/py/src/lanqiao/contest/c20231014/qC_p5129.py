1a590749d491f87d894f1c3574130232bd84ed32
Rocky Haotian Du <2712479005@qq.com>
2023-10-15 16:28:00 +0800

    Updated

--- code/py/src/lanqiao/contest/c20231014/qC_p5129.py
+++ code/py/src/lanqiao/contest/c20231014/qC_p5129.py
@@ -1,27 +1,12 @@
 def main() -> None:
     n, k = map(int, input().split())
-    h_ls_1 = sorted(map(int, input().split()))
-    if 0 == n % k:
-        ans = 0
-        per_num = n // k
-        for i in range(0, n, per_num):
-            ans = max(ans, h_ls_1[i + per_num - 1] - h_ls_1[i])
-    else:
-        ans = h_ls_1[-1] - h_ls_1[0]
-        per_num = 1 + n // k
-        add_num = (per_num * k - n)
-        new_n = n + add_num
-        h_ls_2 = h_ls_1.copy()
-        for i, num in enumerate(h_ls_1):
-            for _ in range(add_num):
-                h_ls_2.insert(i, num)
-            diff = 0
-            for j in range(0, new_n, per_num):
-                diff = max(diff, h_ls_2[j + per_num - 1] - h_ls_2[j])
-            for _ in range(add_num):
-                h_ls_2.pop(i)
-            ans = min(ans, diff)
-    print(ans)
+    h_ls = sorted(map(int, input().split()))
+    diff = 0
+    last_h = h_ls[0]
+    for gap, i in sorted(sorted([(h_ls[i] - h_ls[i - 1], i) for i in range(1, n)], key = lambda x: x[0], reverse = True)[: k - 1], key = lambda x: x[1]):
+        diff = max(diff, h_ls[i - 1] - last_h)
+        last_h = h_ls[i]
+    print(max(diff, h_ls[-1] - last_h))
 
 
 if __name__ == '__main__':
