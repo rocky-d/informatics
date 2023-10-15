d24ac16b4f3849d060a2af762121531c87326d67
Rocky Haotian Du <2712479005@qq.com>
2023-10-15 15:40:54 +0800

    Updated

--- code/py/src/leetcode/done/p516.py
+++ code/py/src/leetcode/done/p516.py
@@ -5,10 +5,7 @@ class Solution:
         for right in range(lens):
             dp[right][right] = 1
             for left in range(right - 1, -1, -1):
-                if s[right] == s[left]:
-                    dp[left][right] = 2 + dp[left + 1][right - 1]
-                else:
-                    dp[left][right] = max(dp[left + 1][right], dp[left][right - 1])
+                dp[left][right] = 2 + dp[left + 1][right - 1] if s[right] == s[left] else max(dp[left + 1][right], dp[left][right - 1])
         return dp[0][-1]
 
 
