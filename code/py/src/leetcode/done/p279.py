6620be978b44ee185fc785a693068aee9ae1ef02
Rocky Haotian Du <2712479005@qq.com>
2023-10-16 18:39:20 +0800

    Updated

--- code/py/src/leetcode/done/p279.py
+++ code/py/src/leetcode/done/p279.py
@@ -2,7 +2,7 @@ class Solution:
     def numSquares(self, n: int) -> int:
         n1 = n + 1
         dp = [_i for _i in range(n1)]
-        for perfect_square in [_i ** 2 for _i in range(int(n ** 0.5), 0, -1)]:
+        for perfect_square in [_i ** 2 for _i in range(int(n ** 0.5), 1, -1)]:
             for j in range(perfect_square, n1):
                 dp[j] = min(dp[j], 1 + dp[j - perfect_square])
         return dp[-1]
---
+++ code/py/src/leetcode/undone/p72.py
@@ -0,0 +1,3 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        ...
