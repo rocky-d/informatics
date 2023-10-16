687e6170065afe6c3092e60e46b8f5eb17873a4e
Rocky Haotian Du <2712479005@qq.com>
2023-10-16 10:45:12 +0800

    Updated

--- code/py/src/leetcode/done/p595.py
+++ code/py/src/leetcode/done/p595.py
@@ -2,4 +2,4 @@ from leetcode.util import *
 
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    return world.loc[(3000000 <= world['area']) | (25000000 <= world['population']), ['name', 'population', 'area']]
+    return world[['name', 'population', 'area']][(3000000 <= world['area']) | (25000000 <= world['population'])]
