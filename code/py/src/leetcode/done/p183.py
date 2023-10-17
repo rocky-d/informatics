5f6ff387f58f6a71c6fb9533cd5fbf9a929399b3
Rocky Haotian Du <2712479005@qq.com>
2023-10-17 16:50:09 +0800

    Updated

--- code/py/src/leetcode/done/p183.py
+++ code/py/src/leetcode/done/p183.py
@@ -2,5 +2,5 @@ from leetcode.util import *
 
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    df = pd.merge(customers, orders, how = 'left', left_on = 'id', right_on = 'customerId')
-    return df[df['customerId'].isna()][['name']].rename(columns = {'name': 'Customers'})
+    return (lambda df: df[df['customerId'].isna()][['name']].rename(columns = {'name': 'Customers'}))(
+        df = pd.merge(customers, orders, how = 'left', left_on = 'id', right_on = 'customerId'))
