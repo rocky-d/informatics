2fb480592330f91f6100c5dd5a0063c912d59645
Rocky Haotian Du <2712479005@qq.com>
2023-10-17 18:21:50 +0800

    Updated

---
+++ code/py/src/leetcode/done/p518.py
@@ -0,0 +1,18 @@
+from leetcode.util import *
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        amount1 = amount + 1
+        dp = [1] + [0 for _i in range(amount)]
+        for coin in coins:
+            for j in range(coin, amount1):
+                dp[j] += dp[j - coin]
+        return dp[-1]
+
+
+sol = Solution()
+
+eg_amount = 5
+eg_coins = [1, 2, 5]
+print(sol.change(amount = eg_amount, coins = eg_coins))
