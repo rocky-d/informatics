4c19e855fe175ab4e1c5274a42a23529f0d23d1c
Rocky Haotian Du <2712479005@qq.com>
2023-10-19 10:08:34 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p1683.py
+++ code/py/src/leetcode/done/pandas/p1683.py
@@ -2,4 +2,4 @@ from leetcode.util import *
 
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    return tweets[['tweet_id']][15 < tweets['content'].str.len()]
+    return tweets.loc[15 < tweets['content'].str.len(), ['tweet_id']]
