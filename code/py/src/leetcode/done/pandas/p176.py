bc0cff4bf563595cdf915cf1ca099f969a2e2d96
Rocky Haotian Du <2712479005@qq.com>
2023-10-19 12:25:09 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p176.py
+++ code/py/src/leetcode/done/pandas/p176.py
@@ -3,8 +3,4 @@ from leetcode.util import *
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     employee.drop_duplicates(['salary'], inplace = True)
-    if len(employee['salary']) < 2:
-        ans = pd.DataFrame({'SecondHighestSalary': [None]})
-    else:
-        ans = employee.drop('id', axis = 1).sort_values('salary', ascending = False).rename({'salary': 'SecondHighestSalary'}, axis = 1).head(2).tail(1)
-    return ans
+    return pd.DataFrame({'SecondHighestSalary': [None]}) if 2 > len(employee['salary']) else employee.drop('id', axis = 1).sort_values('salary', ascending = False).rename({'salary': 'SecondHighestSalary'}, axis = 1).head(2).tail(1)
