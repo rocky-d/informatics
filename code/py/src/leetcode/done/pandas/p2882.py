48a09abfc19632f87d8ddb6bd48d56f41d452de1
Rocky Haotian Du <2712479005@qq.com>
2023-10-21 20:43:52 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p2882.py
+++ code/py/src/leetcode/done/pandas/p2882.py
@@ -2,4 +2,4 @@ from rockyutil.leetcode import *
 
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    return customers.drop_duplicates(subset = ['email'], keep = 'first', inplace = False, ignore_index = False)
+    return customers.drop_duplicates('email')
