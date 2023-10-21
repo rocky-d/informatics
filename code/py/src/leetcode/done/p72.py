2f7fc764d9c23b75bd5d440008f2b760adc1b311
Rocky Haotian Du <2712479005@qq.com>
2023-10-21 14:52:57 +0800

    Updated

--- code/py/src/leetcode/done/p72.py
+++ code/py/src/leetcode/done/p72.py
@@ -3,8 +3,9 @@ class Solution:
         len_word1, len_word2 = len(word1), len(word2)
         dp = [[_i for _i in range(1 + len_word2)]] + [[_i] for _i in range(1, 1 + len_word1)]
         for i in range(len_word1):
+            i1 = i + 1
             for j in range(len_word2):
-                dp[i + 1].append(dp[i][j] if word1[i] == word2[j] else 1 + min(dp[i][j], dp[i + 1][j], dp[i][j + 1]))
+                dp[i1].append(dp[i][j] if word1[i] == word2[j] else 1 + min(dp[i][j], dp[i1][j], dp[i][j + 1]))
         return dp[-1][-1]
 
 
