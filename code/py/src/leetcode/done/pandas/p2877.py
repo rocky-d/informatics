7408251398b0ca1c3ffaa41fc9c982a240ed06f3
Rocky Haotian Du <2712479005@qq.com>
2023-10-21 20:26:37 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p2877.py
+++ code/py/src/leetcode/done/pandas/p2877.py
@@ -2,4 +2,4 @@ from rockyutil.leetcode import *
 
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    return pd.DataFrame({'student_id': [row[0] for row in student_data], 'age': [row[1] for row in student_data]})
+    return pd.DataFrame(student_data, columns = ['student_id', 'age'])
