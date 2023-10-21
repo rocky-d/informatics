591a8dff8a4cbe9f70865e34599463c068efa578
Rocky Haotian Du <2712479005@qq.com>
2023-10-21 14:49:47 +0800

    Updated

--- code/py/src/leetcode/done/p72.py
+++ code/py/src/leetcode/done/p72.py
@@ -1,10 +1,10 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        len1_word1, len1_word2 = 1 + len(word1), 1 + len(word2)
-        dp = [[_i for _i in range(len1_word2)]] + [[_i] for _i in range(1, len1_word1)]
-        for i in range(1, len1_word1):
-            for j in range(1, len1_word2):
-                dp[i].append(dp[i - 1][j - 1] if word1[i - 1] == word2[j - 1] else 1 + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]))
+        len_word1, len_word2 = len(word1), len(word2)
+        dp = [[_i for _i in range(1 + len_word2)]] + [[_i] for _i in range(1, 1 + len_word1)]
+        for i in range(len_word1):
+            for j in range(len_word2):
+                dp[i + 1].append(dp[i][j] if word1[i] == word2[j] else 1 + min(dp[i][j], dp[i + 1][j], dp[i][j + 1]))
         return dp[-1][-1]
 
 
