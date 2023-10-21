7ff9a48dfafda5f941b1e8849c34f67bb642ae5b
Rocky Haotian Du <2712479005@qq.com>
2023-10-21 22:46:33 +0800

    Updated

---
+++ code/py/src/leetcode/done/pandas/p2885.py
@@ -0,0 +1,5 @@
+from rockyutil.leetcode import *
+
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(columns = {'id': 'student_id', 'first': 'first_name', 'last': 'last_name', 'age': 'age_in_years'})
