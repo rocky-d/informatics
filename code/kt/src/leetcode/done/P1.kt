389b7c393266366780350e7dcdaa34c75107d000
Rocky Haotian Du <2712479005@qq.com>
2023-10-22 20:52:56 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P1.kt
@@ -0,0 +1,15 @@
+package leetcode.done
+
+class P1 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>(nums.size)
+        nums.forEachIndexed { index, number ->
+            val other = target - number
+            map[other]?.let {
+                return intArrayOf(it, index)
+            }
+            map[number] = index
+        }
+        return intArrayOf()
+    }
+}
