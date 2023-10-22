0360373818165a4543110aa799f0e7147ce1a7bd
Rocky Haotian Du <2712479005@qq.com>
2023-10-22 21:18:37 +0800

    Updated

---
+++ code/py/src/leetcode/done/pandas/p2890.py
@@ -0,0 +1,5 @@
+from rockyutil.leetcode import *
+
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars = ['product'], value_vars = ['quarter_1', 'quarter_2', 'quarter_3', 'quarter_4'], var_name = 'quarter', value_name = 'sales')
