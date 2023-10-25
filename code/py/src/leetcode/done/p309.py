0d37bc5cf9c5f99085005453fc7fcc6adcccb6e5
Rocky Haotian Du <2712479005@qq.com>
2023-10-25 19:13:59 +0800

    Updated

--- code/py/src/leetcode/done/p309.py
+++ code/py/src/leetcode/done/p309.py
@@ -3,9 +3,9 @@ from rockyutil.leetcode import *
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp0, dp1, dp2 = -prices[0], 0, 0
-        for i in range(1, len(prices)):
-            new_dp0, new_dp1, new_dp2 = max(dp0, dp2 - prices[i]), dp0 + prices[i], max(dp1, dp2)
+        dp0, dp1, dp2 = -prices.pop(0), 0, 0
+        for price in prices:
+            new_dp0, new_dp1, new_dp2 = max(dp0, dp2 - price), dp0 + price, max(dp1, dp2)
             dp0, dp1, dp2 = new_dp0, new_dp1, new_dp2
         return max(dp1, dp2)
 
