fed7e3a4ae37a17c848883c5fd74276e2780a369
Rocky Haotian Du <2712479005@qq.com>
2023-10-25 20:01:08 +0800

    Updated

--- crun.cmd
+++ crun.cmd
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 set "script_name=crun"
 
 if ""=="%~1" (
-    echo !script_name! ^>^> One parameter missing.
+    echo !script_name! ^>^> One parameter missing. :(
     exit /b 1
 )
 
@@ -18,7 +18,7 @@ set "output_dir=.\code\cpp\out"
 gcc "%~1" -o "%output_dir%\!output_name!.exe"
 
 if errorlevel 1 (
-    echo !script_name! ^>^> Fail to compile.
+    echo !script_name! ^>^> Fail to compile. :(
     exit /b 1
 )
 
