808665c65cb15469f15ab51a0776713d38841a19
Rocky Haotian Du <2712479005@qq.com>
2023-10-25 19:18:42 +0800

    Updated

--- code/py/src/leetcode/done/p309.py
+++ code/py/src/leetcode/done/p309.py
@@ -5,8 +5,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp0, dp1, dp2 = -prices.pop(0), 0, 0
         for price in prices:
-            new_dp0, new_dp1, new_dp2 = max(dp0, dp2 - price), dp0 + price, max(dp1, dp2)
-            dp0, dp1, dp2 = new_dp0, new_dp1, new_dp2
+            dp0, dp1, dp2 = max(dp0, dp2 - price), dp0 + price, max(dp1, dp2)
         return max(dp1, dp2)
 
 
--- code/py/src/leetcode/done/p714.py
+++ code/py/src/leetcode/done/p714.py
@@ -5,6 +5,5 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         dp0, dp1 = -prices.pop(0), 0
         for price in prices:
-            new_dp0, new_dp1 = max(dp0, dp1 - price), max(dp0 + price - fee, dp1)
-            dp0, dp1 = new_dp0, new_dp1
+            dp0, dp1 = max(dp0, dp1 - price), max(dp0 + price - fee, dp1)
         return dp1
