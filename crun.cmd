825e153014e119a5456eaf3ce07afe19126d3b23
Rocky Haotian Du <2712479005@qq.com>
2023-10-25 19:54:55 +0800

    Updated

--- crun.cmd
+++ crun.cmd
@@ -2,23 +2,19 @@
 chcp 65001
 setlocal enabledelayedexpansion
 
-rem 设置脚本名称（用于输出报错信息）
 set "script_name=crun"
 
-rem 检查是否提供了文件路径参数
-if "%~1"=="" (
+if ""=="%~1" (
     echo !script_name! ^>^> One parameter missing.
     exit /b 1
 )
 
-rem 获取文件名（不包含扩展名）
+rem Get file name (without suffix)
 rem for %%i in ("%~1") do set "output_name=%%~ni"
 set "output_name=main"
 
-rem 设置编译输出目录
 set "output_dir=.\code\cpp\out"
 
-rem 编译C++文件并将可执行文件输出到指定目录
 gcc "%~1" -o "%output_dir%\!output_name!.exe"
 
 if errorlevel 1 (
---
+++ flushgitrepo.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+set "script_name=flushgitrepo"
+
+set "current_dir=%cd%"
+
+if not "informatics"=="%current_dir:~-11%" (
+    echo !script_name! ^>^> Current directory mismatch.
+    exit /b 1
+)
+
+git rm -r --cached .
+git add .
+
+echo Git repository flushed.
+
+endlocal
