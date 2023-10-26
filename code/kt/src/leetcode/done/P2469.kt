cf88b189066301f30530f8c87c809968290b1be0
Rocky Haotian Du <2712479005@qq.com>
2023-10-26 19:30:48 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P2469.kt
@@ -0,0 +1,10 @@
+package leetcode.done
+
+class P2469 {
+    fun convertTemperature(celsius: Double): DoubleArray {
+        val ans = DoubleArray(2)
+        ans[0] = celsius + 273.15
+        ans[1] = 1.80 * celsius + 32.00
+        return ans
+    }
+}
