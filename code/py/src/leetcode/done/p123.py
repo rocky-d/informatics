9fce6f3a4875dcc076b2f50a68e3edaf0f2c713f
Rocky Haotian Du <2712479005@qq.com>
2023-10-27 11:25:11 +0800

    Updated

--- code/py/src/leetcode/done/p123.py
+++ code/py/src/leetcode/done/p123.py
@@ -4,10 +4,10 @@ from rockyutil.leetcode import *
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         NEGATIVE_INFINITY = -10000000000
-        dp = [0, NEGATIVE_INFINITY, NEGATIVE_INFINITY, NEGATIVE_INFINITY, NEGATIVE_INFINITY]
+        dp = [[0, NEGATIVE_INFINITY, NEGATIVE_INFINITY], [NEGATIVE_INFINITY, NEGATIVE_INFINITY]]
         for price in prices:
-            dp[1], dp[2], dp[3], dp[4] = max(-price, dp[1]), max(dp[1] + price, dp[2]), max(dp[2] - price, dp[3]), max(dp[3] + price, dp[4])
-        return max(dp)
+            dp[0][2], dp[1][1], dp[0][1], dp[1][0] = max(dp[0][2], dp[1][1] + price), max(dp[1][1], dp[0][1] - price), max(dp[0][1], dp[1][0] + price), max(dp[1][0], dp[0][0] - price)
+        return max(dp[0])
 
 
 sol = Solution()
