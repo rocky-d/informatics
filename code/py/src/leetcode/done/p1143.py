3283a54536f8d41f387417d2e8b7c2e144514eb4
Rocky Haotian Du <2712479005@qq.com>
2023-10-28 13:00:10 +0800

    Updated

--- code/py/src/leetcode/done/p1143.py
+++ code/py/src/leetcode/done/p1143.py
@@ -1,13 +1,9 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        len_text1, len_text2 = len(text1), len(text2)
-        dp = [[0 for _j in range(1 + len_text2)] for _i in range(1 + len_text1)]
-        for i in range(len_text1):
-            for j in range(len_text2):
-                if text1[i] == text2[j]:
-                    dp[i + 1][j + 1] = dp[i][j] + 1
-                else:
-                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+        dp = [[0 for _j in range(1 + len(text2))] for _i in range(1 + len(text1))]
+        for i, text1_i in enumerate(text1):
+            for j, text2_j in enumerate(text2):
+                dp[i + 1][j + 1] = dp[i][j] + 1 if text1_i == text2_j else max(dp[i][j + 1], dp[i + 1][j])
         return dp[-1][-1]
 
 
