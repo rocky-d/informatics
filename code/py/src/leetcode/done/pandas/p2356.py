ef6d2786e681854265bae0e1b714a21f0c6d3ba9
Rocky Haotian Du <2712479005@qq.com>
2023-10-31 11:07:57 +0800

    Updated

---
+++ code/py/src/leetcode/done/pandas/p2356.py
@@ -0,0 +1,5 @@
+from rockyutil.leetcode import *
+
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    return teacher.drop_duplicates(['teacher_id', 'subject_id']).groupby('teacher_id').size().reset_index(name = 'cnt')
