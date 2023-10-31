4d28aaed55ab68926d467cbd1ebf77f6a4434326
Rocky Haotian Du <2712479005@qq.com>
2023-10-31 11:33:15 +0800

    Updated

--- code/py/src/leetcode/done/p96.py
+++ code/py/src/leetcode/done/p96.py
@@ -1,10 +1,9 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        dp = [0, 1]
+        dp = [1, 1] + [0 for _i in range(n - 1)]
         for i in range(2, n + 1):
-            dp.append(2 * dp[-1])
-            for j in range(2, i):
-                dp[-1] += dp[j - 1] * dp[i - j]
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - j - 1]
         return dp[-1]
 
 
