c505123a07ce6d6f7ef437272b7ad13b99886bca
Rocky Haotian Du <2712479005@qq.com>
2023-11-01 10:15:49 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p596.py
+++ code/py/src/leetcode/done/pandas/p596.py
@@ -2,5 +2,4 @@ from rockyutil.leetcode import *
 
 
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
-    cnt = courses.groupby('class').size()
-    return cnt[5 < cnt].reset_index()[['class']]
+    return (lambda series: series[5 < series].reset_index()[['class']])(courses.groupby('class').size())
