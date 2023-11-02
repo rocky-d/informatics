7b34beb9ff975d7d7b004f3fb04af9aea65421a8
Rocky Haotian Du <2712479005@qq.com>
2023-11-02 13:23:43 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p586.py
+++ code/py/src/leetcode/done/pandas/p586.py
@@ -2,4 +2,4 @@ from rockyutil.leetcode import *
 
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
-    return orders.groupby('customer_number').size().sort_values().tail(1).reset_index()[['customer_number']]
+    return orders['customer_number'].mode().to_frame()
