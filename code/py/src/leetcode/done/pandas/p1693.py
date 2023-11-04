72ff431b36cc26cb9b8a797dcd14927689300dbd
Rocky Haotian Du <2712479005@qq.com>
2023-11-04 10:44:10 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p1693.py
+++ code/py/src/leetcode/done/pandas/p1693.py
@@ -2,4 +2,4 @@ from rockyutil.leetcode import *
 
 
 def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
-    return daily_sales.groupby(['date_id', 'make_name']).agg({'lead_id': pd.Series.nunique, 'partner_id': pd.Series.nunique}).reset_index().rename({'lead_id': 'unique_leads', 'partner_id': 'unique_partners'}, axis = 1)
+    return daily_sales.groupby(['date_id', 'make_name'])[['lead_id', 'partner_id']].nunique().reset_index().rename({'lead_id': 'unique_leads', 'partner_id': 'unique_partners'}, axis = 1)
