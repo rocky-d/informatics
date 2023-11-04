e8bec2237c234d40b1069c0b9e8f71164240797f
Rocky Haotian Du <2712479005@qq.com>
2023-11-04 10:58:54 +0800

    Updated

--- code/py/src/leetcode/done/p300.py
+++ code/py/src/leetcode/done/p300.py
@@ -3,12 +3,14 @@ from rockyutil.leetcode import *
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        ans = 1
         dp = []
         for i, nums_i in enumerate(nums):
             dp.append(1)
             for j, nums_j in enumerate(nums[:i]):
                 if nums_j < nums_i:
                     dp[-1] = max(dp[-1], dp[j] + 1)
+            ans = max(ans, dp[-1])
         return max(dp)
 
 
