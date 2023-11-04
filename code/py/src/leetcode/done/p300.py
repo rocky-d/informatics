916ffe359a16c6f55a18bbd5a2590efdfacdc66b
Rocky Haotian Du <2712479005@qq.com>
2023-11-04 10:51:26 +0800

    Updated

--- code/py/src/leetcode/done/p300.py
+++ code/py/src/leetcode/done/p300.py
@@ -3,14 +3,17 @@ from rockyutil.leetcode import *
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        ans = 1
         dp = [(nums[0], 1)]
         for nums_i in nums[1:]:
             max_ = 0
             for dp_j in dp:
                 if nums_i > dp_j[0]:
                     max_ = max(max_, dp_j[1])
-            dp.append((nums_i, max_ + 1))
-        return sorted(dp, key = lambda x: x[1], reverse = True)[0][1]
+            max_ += 1
+            dp.append((nums_i, max_))
+            ans = max(ans, max_)
+        return ans
 
 
 sol = Solution()
