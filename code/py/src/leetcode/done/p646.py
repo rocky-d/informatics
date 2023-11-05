eeecaeb07b91326a992ed99d292925d8f0705856
Rocky Haotian Du <2712479005@qq.com>
2023-11-05 12:33:24 +0800

    Updated

--- code/py/src/leetcode/done/p646.py
+++ code/py/src/leetcode/done/p646.py
@@ -3,14 +3,13 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-        pairs.sort(key = lambda x: x[0])
-        dp = []
-        for i, pairs_i in enumerate(pairs):
-            dp.append(1)
-            for j, pairs_j in enumerate(pairs[:i]):
-                if pairs_j[1] < pairs_i[0]:
-                    dp[-1] = max(dp[-1], dp[j] + 1)
-        return dp[-1]
+        ans = 0
+        point = -1001
+        for left, right in sorted(pairs, key = lambda x: x[1]):
+            if point < left:
+                point = right
+                ans += 1
+        return ans
 
 
 sol = Solution()
