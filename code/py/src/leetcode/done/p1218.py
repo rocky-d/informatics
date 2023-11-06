27309bbf1d3aaa4c94e05e6c54ba09a22e7f0ad1
Rocky Haotian Du <2712479005@qq.com>
2023-11-06 10:24:38 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1218.py
@@ -0,0 +1,16 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        dp = defaultdict(lambda: 0)
+        for num in arr:
+            dp[num] = dp[num - difference] + 1
+        return max(dp.values())
+
+
+sol = Solution()
+
+eg_arr = [1, 5, 7, 8, 5, 3, 4, 2, 1]
+eg_difference = -2
+print(sol.longestSubsequence(arr = eg_arr, difference = eg_difference))
