8e8c31e771797df4a1fa87130a2ec1b473c6b841
Rocky Haotian Du <2712479005@qq.com>
2023-11-06 09:58:16 +0800

    Updated

---
+++ code/py/src/leetcode/done/pandas/p1378.py
@@ -0,0 +1,5 @@
+from rockyutil.leetcode import *
+
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    return pd.merge(employees, employee_uni, how = 'left', on = 'id')[['unique_id', 'name']]
