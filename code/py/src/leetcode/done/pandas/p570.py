630eef77aca2184fc48a48cf9b51b5f02232f621
Rocky Haotian Du <2712479005@qq.com>
2023-11-09 10:21:34 +0800

    Updated

---
+++ code/py/src/leetcode/done/pandas/p570.py
@@ -0,0 +1,5 @@
+from rockyutil.leetcode import *
+
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    return (lambda series: series[5 <= series].reset_index()[['managerId']].merge(employee, left_on = 'managerId', right_on = 'id')[['name']])(series = employee.dropna().groupby('managerId').size())
