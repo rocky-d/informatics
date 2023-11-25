02b5e816fa15c77cdcd563765c214be45218e87d
Rocky Haotian Du <2712479005@qq.com>
2023-11-25 21:35:19 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P1512.kt
@@ -0,0 +1,28 @@
+package leetcode.done
+
+class P1512 {
+    fun numIdenticalPairs(nums: IntArray): Int {
+        var ans: Int = 0
+        val notVed: BooleanArray = BooleanArray(nums.size) { true }
+        for (i in nums.indices) {
+            if (notVed[i]) {
+                var cnt: Int = 1
+                for (j in i + 1..nums.lastIndex) {
+                    if (nums[i] == nums[j]) {
+                        notVed[j] = false
+                        ++cnt
+                    }
+                }
+                ans += cnt * (cnt - 1) / 2
+            }
+        }
+        return ans
+    }
+}
+
+fun main() {
+    val sol: P1512 = P1512()
+
+    val egNums: IntArray = intArrayOf(1, 2, 3, 1, 1, 3)
+    println(sol.numIdenticalPairs(nums = egNums))
+}
