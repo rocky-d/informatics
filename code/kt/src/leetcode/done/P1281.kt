4bdb69afca8f6bd2544c7d9a765111ddacf667c5
Rocky Haotian Du <2712479005@qq.com>
2023-11-28 11:41:12 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P1281.kt
@@ -0,0 +1,16 @@
+package leetcode.done
+
+class P1281 {
+    fun subtractProductAndSum(n: Int): Int {
+        var varN: Int = n
+        var num1: Int = 1
+        var num2: Int = 0
+        while (varN > 0) {
+            val digit = varN % 10
+            num1 *= digit
+            num2 += digit
+            varN /= 10
+        }
+        return num1 - num2
+    }
+}
