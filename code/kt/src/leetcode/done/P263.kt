749893b6c6d0c9eeb77dee0315d0897e44fd0b84
Rocky Haotian Du <2712479005@qq.com>
2023-11-29 15:23:25 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P263.kt
@@ -0,0 +1,16 @@
+package leetcode.done
+
+class P263 {
+    fun isUgly(n: Int): Boolean {
+        if (1 > n) {
+            return false
+        }
+        var varN: Int = n
+        for (i in intArrayOf(2, 3, 5)) {
+            while (0 == varN % i) {
+                varN /= i
+            }
+        }
+        return 1 == varN
+    }
+}
