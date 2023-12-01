39044dbcc83219f75497b50d0cc562049364395c
Rocky Haotian Du <2712479005@qq.com>
2023-12-01 13:49:17 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1662.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
