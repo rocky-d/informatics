f8eefad040f017655f049703a793250e9ba31b5c
Rocky Haotian Du <2712479005@qq.com>
2023-12-01 17:05:59 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P867.kt
@@ -0,0 +1,13 @@
+package leetcode.done
+
+class P867 {
+    fun transpose(matrix: Array<IntArray>): Array<IntArray> {
+        val ans: Array<IntArray> = Array(matrix[0].size) { IntArray(matrix.size) }
+        for (i in matrix.indices) {
+            for (j in matrix[i].indices) {
+                ans[j][i] = matrix[i][j]
+            }
+        }
+        return ans
+    }
+}
