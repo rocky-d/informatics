eb1705c86a195e83cab20dcc340379170f4bfbee
Rocky Haotian Du <2712479005@qq.com>
2023-12-03 11:05:10 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P1266.kt
@@ -0,0 +1,20 @@
+package leetcode.done
+
+import kotlin.math.abs
+import kotlin.math.min
+
+class P1266 {
+    fun minTimeToVisitAllPoints(points: Array<IntArray>): Int {
+        var ans: Int = 0
+        var x: Int = points[0][0]
+        var y: Int = points[0][1]
+        for (point in points.slice(1..points.lastIndex)) {
+            val stepX: Int = abs(x - point[0])
+            val stepY: Int = abs(y - point[1])
+            ans += stepX + stepY - min(stepX, stepY)
+            x = point[0]
+            y = point[1]
+        }
+        return ans
+    }
+}
