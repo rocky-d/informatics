541c06a313dba6a002c9359444bff03a4dad13d7
Rocky Haotian Du <2712479005@qq.com>
2023-12-03 10:35:19 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P2586.kt
@@ -0,0 +1,13 @@
+package leetcode.done
+
+class P2586 {
+    fun vowelStrings(words: Array<String>, left: Int, right: Int): Int {
+        var ans: Int = 0
+        for (word in words.slice(left..right)) {
+            if ((word.startsWith('a') || word.startsWith('e') || word.startsWith('i') || word.startsWith('o') || word.startsWith('u')) && (word.endsWith('a') || word.endsWith('e') || word.endsWith('i') || word.endsWith('o') || word.endsWith('u'))) {
+                ++ans
+            }
+        }
+        return ans
+    }
+}
