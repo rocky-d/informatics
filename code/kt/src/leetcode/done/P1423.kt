8099ee5ec73eddb48d841cc6c6c47e2a72887799
Rocky Haotian Du <2712479005@qq.com>
2023-12-03 10:50:52 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P1423.kt
@@ -0,0 +1,19 @@
+package leetcode.done
+
+import kotlin.math.min
+
+class P1423 {
+    fun maxScore(cardPoints: IntArray, k: Int): Int {
+        val n: Int = cardPoints.size
+        val l: Int = n - k
+        val prefixSum: IntArray = IntArray(1 + n)
+        for (i in cardPoints.indices) {
+            prefixSum[i + 1] = prefixSum[i] + cardPoints[i]
+        }
+        var minWindow: Int = prefixSum[n]
+        for (i in 0..k) {
+            minWindow = min(minWindow, prefixSum[i + l] - prefixSum[i])
+        }
+        return prefixSum[n] - minWindow
+    }
+}
