b992239406c23827a9fe4610d814256a659b9754
Rocky Haotian Du <2712479005@qq.com>
2023-12-04 13:18:43 +0800

    Updated

---
+++ code/kt/src/leetcode/done/P852.kt
@@ -0,0 +1,18 @@
+package leetcode.done
+
+class P852 {
+    fun peakIndexInMountainArray(arr: IntArray): Int {
+        val n: Int = arr.size
+        var left: Int = 1
+        var right: Int = n - 2
+        while (left <= right) {
+            val middle: Int = (left + right) / 2
+            if (arr[middle] > arr[middle + 1]) {
+                right = middle - 1
+            } else {
+                left = middle + 1
+            }
+        }
+        return left
+    }
+}
