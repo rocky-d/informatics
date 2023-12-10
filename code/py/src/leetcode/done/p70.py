8e527caf4ac0ec24312e636f0e34f9f7f93b0ccf
Rocky Haotian Du <2712479005@qq.com>
2023-12-10 12:32:21 +0800

    Updated

--- code/py/src/leetcode/done/p70.py
+++ code/py/src/leetcode/done/p70.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        first, second = 1, 2
-        for _ in range(n - 2):
-            first, second = second, first + second
-        return 1 if n == 1 else second
+        last, this = 1, 2
+        for _i in range(2, n):
+            last, this = this, last + this
+        return last if 1 == n else this
