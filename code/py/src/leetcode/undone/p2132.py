f95e73cee085f5fb4d1831ffd6418f09d3b2ffce
Rocky Haotian Du <2712479005@qq.com>
2023-12-14 11:14:03 +0800

    Updated

--- code/py/src/leetcode/undone/p2132.py
+++ code/py/src/leetcode/undone/p2132.py
@@ -11,19 +11,19 @@ class Solution:
                 pre_sum[i + 1][j + 1] = grid[i][j] + pre_sum[i + 1][j] + pre_sum[i][j + 1] - pre_sum[i][j]
 
         diff = [[0 for _ in range(2 + n)] for _ in range(2 + m)]
-        for i in range(m + 1 - stampHeight):
-            for j in range(n + 1 - stampWidth):
+        for i in range(m - stampHeight + 1):
+            for j in range(n - stampWidth + 1):
                 x = i + stampHeight
                 y = j + stampWidth
-                if pre_sum[x][y] - pre_sum[x][j] - pre_sum[i][y] + pre_sum[i][j] == 0:
+                if 0 == pre_sum[x][y] - pre_sum[x][j] - pre_sum[i][y] + pre_sum[i][j]:
                     diff[i + 1][j + 1] += 1
                     diff[x + 1][y + 1] += 1
                     diff[i + 1][y + 1] -= 1
                     diff[x + 1][j + 1] -= 1
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1]
-                if diff[i][j] == 0 and grid[i - 1][j - 1] == 0:
+        for i in range(m):
+            for j in range(n):
+                diff[i + 1][j + 1] += diff[i][j + 1] + diff[i + 1][j] - diff[i][j]
+                if 0 == grid[i][j] and 0 == diff[i + 1][j + 1]:
                     return False
         return True
