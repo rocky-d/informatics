95efdbffd3579155a304dcda3002ff6284b12d31
Rocky Haotian Du <2712479005@qq.com>
2023-12-16 15:07:12 +0800

    Updated

---
+++ code/py/src/leetcode/done/p242.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            s_dict, t_dict = dict(), dict()
+            for s_char, t_char in zip(s, t):
+                s_dict[s_char] = 1 + s_dict.get(s_char, 0)
+                t_dict[t_char] = 1 + t_dict.get(t_char, 0)
+            ans = True if s_dict == t_dict else False
+        else:
+            ans = False
+        return ans
