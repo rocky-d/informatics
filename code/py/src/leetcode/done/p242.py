2b3c2392d93d6af8f34d0812b37536cdde7cfb00
Rocky Haotian Du <2712479005@qq.com>
2023-12-16 15:08:11 +0800

    Updated

--- code/py/src/leetcode/done/p242.py
+++ code/py/src/leetcode/done/p242.py
@@ -3,8 +3,7 @@ class Solution:
         if len(s) == len(t):
             s_dict, t_dict = dict(), dict()
             for s_char, t_char in zip(s, t):
-                s_dict[s_char] = 1 + s_dict.get(s_char, 0)
-                t_dict[t_char] = 1 + t_dict.get(t_char, 0)
+                s_dict[s_char], t_dict[t_char] = 1 + s_dict.get(s_char, 0), 1 + t_dict.get(t_char, 0)
             ans = True if s_dict == t_dict else False
         else:
             ans = False
