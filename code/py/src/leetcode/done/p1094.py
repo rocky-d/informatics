cad9d4715c73e7c035f240eb200e0ac19684301e
Rocky Haotian Du <2712479005@qq.com>
2023-12-16 20:27:15 +0800

    Updated

--- code/py/src/leetcode/done/p1094.py
+++ code/py/src/leetcode/done/p1094.py
@@ -3,12 +3,24 @@ from rockyutil.leetcode import *
 
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        states = []
+        diffs = [capacity]
         for trip in trips:
-            while len(states) <= trip[2]:
-                states.append(capacity)
-            for i in range(trip[1], trip[2]):
-                states[i] -= trip[0]
-                if states[i] < 0:
-                    return False
-        return True
+            while len(diffs) <= trip[2]:
+                diffs.append(0)
+            diffs[trip[1]] -= trip[0]
+            diffs[trip[2]] += trip[0]
+        seats = 0
+        for diff in diffs:
+            seats += diff
+            if seats < 0:
+                ans = False
+                break
+        else:
+            ans = True
+        return ans
+
+
+eg_trips = [[2, 1, 5], [3, 3, 7]]
+eg_capacity = 5
+
+print(Solution().carPooling(trips = eg_trips, capacity = eg_capacity))
