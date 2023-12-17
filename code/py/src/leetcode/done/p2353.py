19bc501a51a2e56b270cf1521a3102f138a01c78
Rocky Haotian Du <2712479005@qq.com>
2023-12-17 20:26:37 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2353.py
@@ -0,0 +1,19 @@
+from rockyutil.leetcode import *
+
+
+class FoodRatings:
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.food_dict = dict()
+        self.cuisine_dict = defaultdict(lambda: sc.SortedSet())
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.food_dict[food] = cuisine, rating
+            self.cuisine_dict[cuisine].add((-rating, food))
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine, rating = self.food_dict[food]
+        self.food_dict[food] = cuisine, newRating
+        self.cuisine_dict[cuisine].remove((-rating, food))
+        self.cuisine_dict[cuisine].add((-newRating, food))
+
+    def highestRated(self, cuisine: str) -> str:
+        return self.cuisine_dict[cuisine][0][1]
