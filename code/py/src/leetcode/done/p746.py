1000b5dfdc1ee4bc0c8df70baad649e8621480d3
Rocky Haotian Du <2712479005@qq.com>
2023-12-17 14:31:19 +0800

    Updated

--- code/py/src/leetcode/done/p746.py
+++ code/py/src/leetcode/done/p746.py
@@ -3,7 +3,7 @@ from rockyutil.leetcode import *
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        first, second = 0, min(cost[0], cost[1])
-        for i in range(2, len(cost)):
-            first, second = second, min(second + cost[i], first + cost[i - 1])
-        return second
+        dp = 0, 0
+        for cost_i in cost:
+            dp = min(dp) + cost_i, dp[0]
+        return min(dp)
