e530c6391e245271fd73d4f766568d2ebbb74a8e
Rocky Haotian Du <2712479005@qq.com>
2023-12-19 11:11:49 +0800

    Updated

--- code/py/src/leetcode/done/p162.py
+++ code/py/src/leetcode/done/p162.py
@@ -4,7 +4,7 @@ from rockyutil.leetcode import *
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         nums = [float('-inf')] + nums + [float('-inf')]
-        for i in range(1, len(nums) - 1):
+        for i in range(1, 1 + len(nums)):
             if nums[i - 1] < nums[i] and nums[i] > nums[i + 1]:
                 ans = i - 1
                 break
