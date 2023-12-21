a8038e02a815e9b526a2f578a55889048e4dd2ac
Rocky Haotian Du <2712479005@qq.com>
2023-12-21 21:42:38 +0800

    Updated

--- code/py/src/leetcode/undone/p2866.py
+++ code/py/src/leetcode/undone/p2866.py
@@ -4,33 +4,29 @@ from rockyutil.leetcode import *
 class Solution:
     def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
         n = len(maxHeights)
+        ans = 0
         left_stack, stack_sum, left_sums = [0], 0, []
         for i in range(0, n, +1):
             pop_times = 0
             while left_stack[-1] > maxHeights[i]:
                 stack_sum -= left_stack.pop()
                 pop_times += 1
-            for _ in range(pop_times):
-                left_stack.append(maxHeights[i])
+            left_stack += [maxHeights[i] for _ in range(pop_times)]
             stack_sum += pop_times * left_stack[-1]
             left_sums.append(stack_sum)
             left_stack.append(maxHeights[i])
             stack_sum += left_stack[-1]
-        right_stack, stack_sum, right_sums = [0], 0, []
+        right_stack, stack_sum = [0], 0
         for i in range(n - 1, -1, -1):
             pop_times = 0
             while right_stack[-1] > maxHeights[i]:
                 stack_sum -= right_stack.pop()
                 pop_times += 1
-            for _ in range(pop_times):
-                right_stack.append(maxHeights[i])
+            right_stack += [maxHeights[i] for _ in range(pop_times)]
             stack_sum += pop_times * right_stack[-1]
-            right_sums.append(stack_sum)
             right_stack.append(maxHeights[i])
             stack_sum += right_stack[-1]
-        ans = 0
-        for i in range(n):
-            ans = max(ans, left_sums[i] + maxHeights[i] + right_sums[-1 - i])
+            ans = max(ans, left_sums[i] + stack_sum)
         return ans
 
 
