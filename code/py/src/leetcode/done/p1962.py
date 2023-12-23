ce8fa6f44103cb96e3cfb60dddb9667dcafe675a
Rocky Haotian Du <2712479005@qq.com>
2023-12-23 11:36:52 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1962.py
@@ -0,0 +1,15 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        minus_piles = [-pile for pile in piles]
+        heapq.heapify(minus_piles)
+        for _ in range(k):
+            heapq.heappush(minus_piles, math.floor(heapq.heappop(minus_piles) / 2))
+        return -sum(minus_piles)
+
+
+eg_piles = [5, 4, 9]
+eg_k = 2
+print(Solution().minStoneSum(piles = eg_piles, k = eg_k))
