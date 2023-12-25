0bd7ce23a39b89903931e6ceffcdcf5a2a057e87
Rocky Haotian Du <2712479005@qq.com>
2023-12-25 14:09:56 +0800

    Updated

--- code/py/src/leetcode/done/p91.py
+++ code/py/src/leetcode/done/p91.py
@@ -4,7 +4,7 @@ class Solution:
             ans = 0
         else:
             dp = 1, 1
-            tens_digits = set(str(i) for i in range(10, 27))
+            tens_digits = {'10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'}
             for i in range(1, len(s)):
                 dp = dp[1], (dp[0] if s[i - 1:i + 1] in tens_digits else 0) + (dp[1] if s[i] != '0' else 0)
             ans = dp[-1]
