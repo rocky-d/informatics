c7adceb6230297610fc22d538222c2acb663a7df
Rocky Haotian Du <2712479005@qq.com>
2023-12-27 10:26:56 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1578.py
@@ -0,0 +1,25 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+        ans = 0
+        i = 0
+        while i + 1 < n:
+            if colors[i] == colors[i + 1]:
+                total_time, max_time = neededTime[i], neededTime[i]
+                i += 1
+                while i + 1 < n and colors[i] == colors[i + 1]:
+                    total_time += neededTime[i]
+                    max_time = max(max_time, neededTime[i])
+                    i += 1
+                total_time += neededTime[i]
+                ans += total_time - max(max_time, neededTime[i])
+            i += 1
+        return ans
+
+
+eg_colors = 'bbbaaa'
+eg_neededTime = [4, 9, 3, 8, 8, 9]
+print(Solution().minCost(colors = eg_colors, neededTime = eg_neededTime))
