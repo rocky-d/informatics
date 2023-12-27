4dcf2384c8625790f215803c0ae47148620e3870
Rocky Haotian Du <2712479005@qq.com>
2023-12-27 16:31:40 +0800

    Updated

--- cpprun.cmd
+++ cpprun.cmd
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 set "script_name=crun"
 
 if ""=="%~1" (
-    echo !script_name! ^>^> One parameter missing. :(
+    echo %script_name% ^>^> One parameter missing. :(
     exit /b 1
 )
 
@@ -16,7 +16,7 @@ for %%I in ("%~1") do cd "%%~dpI"
 for %%i in ("%~1") do g++ "%%~nxi" -o "%output_dir%\%output_name%.exe"
 
 if errorlevel 1 (
-    echo !script_name! ^>^> Fail to compile. :(
+    echo %script_name% ^>^> Fail to compile. :(
     exit /b 1
 )
 
