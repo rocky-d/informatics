fbb7b65845a6faab7b1f9abb2cf76410cd8fe126
Rocky Haotian Du <2712479005@qq.com>
2023-12-27 16:30:23 +0800

    Updated

--- crun.cmd
+++ crun.cmd
@@ -13,7 +13,7 @@ set "output_name=main"
 set "output_dir=%~dp0\code\c\out"
 
 for %%I in ("%~1") do cd "%%~dpI"
-for %%i in ("%~1") do gcc "%%~nxi" -o "%output_dir%\!output_name!.exe"
+for %%i in ("%~1") do gcc "%%~nxi" -o "%output_dir%\%output_name%.exe"
 
 if errorlevel 1 (
     echo !script_name! ^>^> Fail to compile. :(
