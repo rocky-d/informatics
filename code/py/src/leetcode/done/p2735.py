bbf58770e2490a9527f813abc07910c41f8ea0cb
Rocky Haotian Du <2712479005@qq.com>
2023-12-28 13:29:37 +0800

    Updated

--- code/py/src/leetcode/done/p2735.py
+++ code/py/src/leetcode/done/p2735.py
@@ -4,23 +4,10 @@ from rockyutil.leetcode import *
 class Solution:
     def minCost(self, nums: List[int], x: int) -> int:
         n = len(nums)
-        costs, max_move_cost = [], 0
+        ans = 1_000_000_000_000
+        dp = nums.copy()
         for i in range(n):
-            costs.append(0)
-            cost, move_cost = 1_000_000_000 + n * x, 0
-            for j in range(i, n):
-                if cost > nums[j] + move_cost:
-                    cost = nums[j] + move_cost
-                    costs[-1], max_move_cost = nums[j], max(max_move_cost, move_cost)
-                move_cost += x
-                if cost <= move_cost:
-                    break
-            else:
-                for j in range(0, i):
-                    if cost > nums[j] + move_cost:
-                        cost = nums[j] + move_cost
-                        costs[-1], max_move_cost = nums[j], max(max_move_cost, move_cost)
-                    move_cost += x
-                    if cost <= move_cost:
-                        break
-        return sum(costs) + max_move_cost
+            for j in range(n):
+                dp[j] = min(dp[j], nums[(i + j) % n])
+            ans = min(ans, i * x + sum(dp))
+        return ans
