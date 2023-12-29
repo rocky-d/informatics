b6b041c5e679f9493677f42a5b90fdfbe3ed2e7c
Rocky Haotian Du <2712479005@qq.com>
2023-12-29 15:17:57 +0800

    Updated

--- code/py/src/leetcode/done/p1335.py
+++ code/py/src/leetcode/done/p1335.py
@@ -8,7 +8,7 @@ class Solution:
         dp[0][0] = max(jobDifficulty[:1])
         for i in range(1, n):
             dp[i][0] = max(jobDifficulty[:i + 1])
-            for j in range(min(i, d - 1), 0, -1):
+            for j in range(1, min(i + 1, d)):
                 today = jobDifficulty[i]
                 until_today = dp[i - 1][j - 1] + today
                 for k in range(1, i - j + 1):
@@ -19,11 +19,11 @@ class Solution:
 
 
 eg_jobDifficulty = [6, 5, 4, 3, 2, 1]
-eg_d = 4
+eg_d = 3
 print(Solution().minDifficulty(jobDifficulty = eg_jobDifficulty, d = eg_d))
 
 #      j=0           j=1                                                           j=2
 # i=0  max(6)        -1                                                            -1
 # i=1  max(6,5)      min(dp[0][0]+max(5))                                          -1
 # i=2  max(6,5,4)    min(dp[1][0]+max(4), dp[0][0]+max(5,4))                       min(dp[1][1]+max(4))
-# i=3  max(6,5,4,3)  min(dp[2][0]+max(3), dp[1][0]+max(4,3), dp[0][0]+max(5,4,3))  min()
+# i=3  max(6,5,4,3)  min(dp[2][0]+max(3), dp[1][0]+max(4,3), dp[0][0]+max(5,4,3))  min(dp[2][1]+max(3), dp[1][1]+max(4,3))
