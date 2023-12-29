47963144ccdb057cbc43f271936d36dc6464fd68
Rocky Haotian Du <2712479005@qq.com>
2023-12-29 15:37:09 +0800

    Updated

--- code/py/src/leetcode/done/pandas/p180.py
+++ code/py/src/leetcode/done/pandas/p180.py
@@ -3,4 +3,4 @@ from rockyutil.leetcode import *
 
 def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
     logs['var'] = logs.num.rolling(3).var()
-    return pd.DataFrame({'ConsecutiveNums': logs.query('var == 0').num.unique()})
+    return pd.DataFrame({'ConsecutiveNums': logs[0 == logs['var']].num.unique()})
