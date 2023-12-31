fc9e59e3d4b4a99cb15ad7479b2a8d7d981146d8
Rocky Haotian Du <2712479005@qq.com>
2023-12-31 11:26:02 +0800

    Updated

--- code/py/src/leetcode/done/p1624.py
+++ code/py/src/leetcode/done/p1624.py
@@ -1,17 +1,6 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        n = len(s)
-        first, last = dict(), dict()
-        for i, char in zip(range(n), s):
-            if char not in first.keys():
-                first[char] = i
-        for i, char in zip(range(n - 2, -2, -1), s[::-1]):
-            if char not in last.keys():
-                last[char] = i
-        ans = -1
-        for char in first.keys():
-            ans = max(ans, last[char] - first[char])
-        return ans
+        return max(s.rfind(char) - s.find(char) - 1 for char in set(s))
 
 
 eg_s = 'abc'
