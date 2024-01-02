1c49e257679f1660367b8f9d4d426f25b883b19f
Rocky Haotian Du <2712479005@qq.com>
2024-01-02 10:59:59 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2610.py
@@ -0,0 +1,21 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        nums_times = dict()
+        for num in nums:
+            nums_times[num] = 1 + nums_times.get(num, 0)
+        nums_times = sorted(nums_times.items(), key = lambda item: item[1], reverse = True)
+        len_nums_times = len(nums_times)
+        ans = []
+        i = 0
+        for times in range(nums_times[0][1], 0, -1):
+            while i < len_nums_times and times == nums_times[i][1]:
+                i += 1
+            ans.append([num for num, times in nums_times[:i]])
+        return ans
+
+
+eg_nums = [4, 5, 3, 3, 3]
+print(Solution().findMatrix(nums = eg_nums))
