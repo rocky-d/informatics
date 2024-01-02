f87134afe808f70239d66d0c18a9da1cb7a6a6b4
Rocky Haotian Du <2712479005@qq.com>
2024-01-02 14:11:40 +0800

    Updated

--- code/py/src/rockyutil/systemoperating/移动臂调度算法/main.py
+++ code/py/src/rockyutil/systemoperating/移动臂调度算法/main.py
@@ -108,12 +108,12 @@ if __name__ == '__main__':
     movingArm = MovingArm(
         total_cylinders = int(input('磁盘柱面数 > ')),
         # cylinders = [11, 9, 17, 11, 22, 9, 11],
-        cylinders = [86, 147, 91, 177, 94, 150, 102, 175, 130],
-        # cylinders = random_cylinders(
-        #     max_cylinder = int(input('最大调用柱面 > ')),
-        #     len_cylinders = int(input('柱面随机调用次数 > ')),
-        #     show_details = bool(input('直接按下回车键以忽略/输入任何内容以展示 柱面调用顺序 > '))
-        # ),
+        # cylinders = [86, 147, 91, 177, 94, 150, 102, 175, 130],
+        cylinders = random_cylinders(
+            max_cylinder = int(input('最大调用柱面 > ')),
+            len_cylinders = int(input('柱面随机调用次数 > ')),
+            show_details = bool(input('直接按下回车键以忽略/输入任何内容以展示 柱面调用顺序 > '))
+        ),
         start = int(input('移动臂起始所在柱面 > ')),
         decreasing = bool(input('移动臂正在向 内/外（最大号柱面/1号柱面） 扫描（直接按下回车以选择向内，输入任何内容以选择向外） > ')),
         show_details = bool(input('直接按下回车键以忽略/输入任何内容以展示 移动臂移动过程 > '))
