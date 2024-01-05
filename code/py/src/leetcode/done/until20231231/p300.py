59b15d1e6270c9dc75d5ffa42f8216f33f7dbd87
Rocky Haotian Du <2712479005@qq.com>
2024-01-05 08:29:21 +0800

    Updated

--- code/py/src/leetcode/done/until20231231/p300.py
+++ code/py/src/leetcode/done/until20231231/p300.py
@@ -3,15 +3,15 @@ from rockyutil.leetcode import *
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        max_dp = 0
+        ans = 0
         dp = []
         for i, nums_i in enumerate(nums):
             dp.append(1)
             for j, nums_j in enumerate(nums[:i]):
                 if nums_j < nums_i:
                     dp[-1] = max(dp[-1], dp[j] + 1)
-            max_dp = max(max_dp, dp[-1])
-        return max_dp
+            ans = max(ans, dp[-1])
+        return ans
 
 
 sol = Solution()
