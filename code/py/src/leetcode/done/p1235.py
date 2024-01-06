f780f65f634e381c5819829bb5277235a21d9616
Rocky Haotian Du <2712479005@qq.com>
2024-01-06 14:37:48 +0800

    Updated

--- code/py/src/leetcode/done/p1235.py
+++ code/py/src/leetcode/done/p1235.py
@@ -5,7 +5,7 @@ class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         n = len(profit)
         jobs = sorted(zip(startTime, endTime, profit), key = lambda x: x[1])
-        dp = [0 for _ in range(n + 1)]
+        dp = [0 for _ in range(1 + n)]
         dp[1] = jobs[0][2]
         for i in range(1, n):
             dp[i + 1] = max(dp[i], dp[bisect_right(jobs, jobs[i][0], key = lambda x: x[1])] + jobs[i][2])
