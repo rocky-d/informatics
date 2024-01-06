9f1088e51c3fb173b673b61c6fe92ab2107a4de1
Rocky Haotian Du <2712479005@qq.com>
2024-01-06 13:31:22 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2807.py
@@ -0,0 +1,10 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        while node.next is not None:
+            node.next = ListNode(val = gcd(node.val, node.next.val), next = node.next)
+            node = node.next.next
+        return head
