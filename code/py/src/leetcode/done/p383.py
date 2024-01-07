aaf6cdaa2ca4903c6824e2315a3a139e065e2c7e
Rocky Haotian Du <2712479005@qq.com>
2024-01-07 10:52:33 +0800

    Updated

---
+++ code/py/src/leetcode/done/p383.py
@@ -0,0 +1,13 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        rn_counter, m_counter = Counter(ransomNote), Counter(magazine)
+        for char, count in rn_counter.items():
+            if m_counter.get(char, 0) < count:
+                ans = False
+                break
+        else:
+            ans = True
+        return ans
