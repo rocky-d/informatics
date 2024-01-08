629a2855db67062862391c6e87b5d4f6e5f683d1
Rocky Haotian Du <2712479005@qq.com>
2024-01-08 11:01:09 +0800

    Updated

--- code/py/src/leetcode/done/p447.py
+++ code/py/src/leetcode/done/p447.py
@@ -7,10 +7,10 @@ class Solution:
         ans = 0
         distances = []
         for i in range(n):
-            distances.append([])
+            distances.append([0])
             counter = defaultdict(lambda: 0)
             for j in range(i):
-                counter[distances[j][i - j - 1]] += 1
+                counter[distances[j][i - j]] += 1
             for j in range(i + 1, n):
                 distances[-1].append((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2)
                 counter[distances[-1][-1]] += 1
