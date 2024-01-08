4aaf538854ccd9ba7a73a0a6376840a4b3ca86a1
Rocky Haotian Du <2712479005@qq.com>
2024-01-08 12:54:39 +0800

    Updated

--- code/py/src/leetcode/done/concurrency/p1226.py
+++ code/py/src/leetcode/done/concurrency/p1226.py
@@ -25,3 +25,72 @@ class DiningPhilosophers:
         self.forks[right_fork].release()
         self.forks[left_fork].release()
         self.dining.release()
+
+
+eg_n = 1
+instance = DiningPhilosophers()
+
+for _ in range(eg_n):
+    threads = [
+        threading.Thread(
+            target = instance.wantsToEat,
+            args = (
+                0,
+                lambda: print([0, 1, 1]),
+                lambda: print([0, 2, 1]),
+                lambda: print([0, 0, 3]),
+                lambda: print([0, 1, 2]),
+                lambda: print([0, 2, 2]),
+            ),
+        ),
+        threading.Thread(
+            target = instance.wantsToEat,
+            args = (
+                1,
+                lambda: print([1, 1, 1]),
+                lambda: print([1, 2, 1]),
+                lambda: print([1, 0, 3]),
+                lambda: print([1, 1, 2]),
+                lambda: print([1, 2, 2]),
+            ),
+        ),
+        threading.Thread(
+            target = instance.wantsToEat,
+            args = (
+                2,
+                lambda: print([2, 1, 1]),
+                lambda: print([2, 2, 1]),
+                lambda: print([2, 0, 3]),
+                lambda: print([2, 1, 2]),
+                lambda: print([2, 2, 2]),
+            ),
+        ),
+        threading.Thread(
+            target = instance.wantsToEat,
+            args = (
+                3,
+                lambda: print([3, 1, 1]),
+                lambda: print([3, 2, 1]),
+                lambda: print([3, 0, 3]),
+                lambda: print([3, 1, 2]),
+                lambda: print([3, 2, 2]),
+            ),
+        ),
+        threading.Thread(
+            target = instance.wantsToEat,
+            args = (
+                4,
+                lambda: print([4, 1, 1]),
+                lambda: print([4, 2, 1]),
+                lambda: print([4, 0, 3]),
+                lambda: print([4, 1, 2]),
+                lambda: print([4, 2, 2]),
+            ),
+        ),
+    ]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
