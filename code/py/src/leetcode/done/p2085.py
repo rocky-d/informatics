9bb9e5369b8a2e43056ddb9589c69a7bcbab2d87
Rocky Haotian Du <2712479005@qq.com>
2024-01-12 23:30:29 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2085.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        return len(set(word for word, count in Counter(words1).items() if 1 == count) & set(word for word, count in Counter(words2).items() if 1 == count))
