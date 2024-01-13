5dc4ad24b1c1d83d3054fa3fbe817b7f31db047f
Rocky Haotian Du <2712479005@qq.com>
2024-01-13 12:15:55 +0800

    Updated

--- code/py/src/leetcode/done/p2182.py
+++ code/py/src/leetcode/done/p2182.py
@@ -11,13 +11,13 @@ class Solution:
             char, count = chr(-item[0]), -item[1]
             if 0 < len(ans) and ans[-1] == char:
                 if 0 < len(s_counter):
-                    item1 = heappop(s_counter)
-                    char1, count1 = chr(-item1[0]), -item1[1]
-                    repeats1 = 1
-                    ans += repeats1 * char1
-                    last1 = count1 - repeats1
-                    if 0 < last1:
-                        heappush(s_counter, (-ord(char1), -last1))
+                    item_ = heappop(s_counter)
+                    char_, count_ = chr(-item_[0]), -item_[1]
+                    repeats_ = 1
+                    ans += repeats_ * char_
+                    last_ = count_ - repeats_
+                    if 0 < last_:
+                        heappush(s_counter, (-ord(char_), -last_))
                 else:
                     break
             repeats = min(repeatLimit, count)
