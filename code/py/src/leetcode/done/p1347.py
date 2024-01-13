2c3b27635de93411cdb9a190220203d832d7e265
Rocky Haotian Du <2712479005@qq.com>
2024-01-13 11:34:33 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1347.py
@@ -0,0 +1,10 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        ans = 0
+        s_counter, t_counter = Counter(s), Counter(t)
+        for char, count in s_counter.items():
+            ans += max(0, count - t_counter.get(char, 0))
+        return ans
