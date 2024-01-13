5e70d06253971447128dbd108770ddb1589c75b5
Rocky Haotian Du <2712479005@qq.com>
2024-01-13 11:24:31 +0800

    Updated

--- code/py/src/leetcode/done/p1704.py
+++ code/py/src/leetcode/done/p1704.py
@@ -1,9 +1,4 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         s_upper, half_len_s = s.upper(), len(s) // 2
-        a, b = s_upper[:half_len_s], s_upper[half_len_s:]
-        a_count, b_count = 0, 0
-        for char in 'AEIOU':
-            a_count += a.count(char)
-            b_count += b.count(char)
-        return a_count == b_count
+        return sum(s_upper[:half_len_s].count(char) for char in 'AEIOU') == sum(s_upper[half_len_s:].count(char) for char in 'AEIOU')
