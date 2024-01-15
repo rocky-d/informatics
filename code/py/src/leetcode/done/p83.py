59a878a598c889e415f9453c977fdda78e6a22f3
Rocky Haotian Du <2712479005@qq.com>
2024-01-15 18:15:53 +0800

    Updated

--- code/py/src/leetcode/done/p83.py
+++ code/py/src/leetcode/done/p83.py
@@ -3,9 +3,9 @@ from rockyutil.leetcode import *
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = node = ListNode(val = 101, next = head)
+        node = head
         while node is not None:
             while node.next is not None and node.val == node.next.val:
                 node.next = node.next.next
             node = node.next
-        return dummy.next
+        return head
