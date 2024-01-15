2bfc7d9a8cd401de763717b847fbf0bfe012b407
Rocky Haotian Du <2712479005@qq.com>
2024-01-15 19:23:48 +0800

    Updated

--- code/py/src/leetcode/done/p2225.py
+++ code/py/src/leetcode/done/p2225.py
@@ -4,10 +4,10 @@ from rockyutil.leetcode import *
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         players, losers = set(), dict()
-        for match in matches:
-            players.add(match[0])
-            players.add(match[1])
-            losers[match[1]] = 1 + losers.get(match[1], 0)
+        for match_ in matches:
+            players.add(match_[0])
+            players.add(match_[1])
+            losers[match_[1]] = 1 + losers.get(match_[1], 0)
         return [sorted(players - losers.keys()), sorted(loser for loser, count in losers.items() if 1 == count)]
 
 
