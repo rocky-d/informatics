6e49c430448e8db696ff7bb196b4e28a77f09648
Rocky Haotian Du <2712479005@qq.com>
2024-01-16 11:21:57 +0800

    Updated

--- code/py/src/leetcode/done/p380.py
+++ code/py/src/leetcode/done/p380.py
@@ -3,23 +3,29 @@ from rockyutil.leetcode import *
 
 class RandomizedSet:
     def __init__(self):
-        self._set = set()
+        self._list = list()
+        self._dict = dict()
 
     def insert(self, val: int) -> bool:
-        if val in self._set:
+        if val in self._dict.keys():
             res = False
-        else:  # elif val not in self._set:
+        else:  # elif val not in self._dict.keys():
             res = True
-            self._set.add(val)
+            self._dict[val] = len(self._list)
+            self._list.append(val)
         return res
 
     def remove(self, val: int) -> bool:
-        if val in self._set:
+        if val in self._dict.keys():
             res = True
-            self._set.remove(val)
-        else:  # elif val not in self._set:
+            val_index = self._dict[val]
+            self._list[val_index] = self._list[-1]
+            self._dict[self._list[-1]] = val_index
+            del self._list[-1]
+            del self._dict[val]
+        else:  # elif val not in self._dict.keys():
             res = False
         return res
 
     def getRandom(self) -> int:
-        return choice(tuple(self._set))
+        return choice(self._list)
