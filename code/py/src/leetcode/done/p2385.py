66c35d84d8a850b06d28247cdb49bc317cacc54b
Rocky Haotian Du <2712479005@qq.com>
2024-01-17 13:45:54 +0800

    Updated

--- code/py/src/leetcode/done/p2385.py
+++ code/py/src/leetcode/done/p2385.py
@@ -7,18 +7,18 @@ class Solution:
             if node is None:
                 return 0, None
             nonlocal ans
-            node_left, node_right = dfs(node.left), dfs(node.right)
-            if node_left[1] is not None:
-                ans = max(ans, 1 + node_right[0] + node_left[1])
-                res = 1 + node_right[0], 1 + node_left[1]
-            elif node_right[1] is not None:
-                ans = max(ans, 1 + node_left[0] + node_right[1])
-                res = 1 + node_left[0], 1 + node_right[1]
+            left, right = dfs(node.left), dfs(node.right)
+            if left[1] is not None:
+                ans = max(ans, 1 + right[0] + left[1])
+                res = 1 + right[0], 1 + left[1]
+            elif right[1] is not None:
+                ans = max(ans, 1 + left[0] + right[1])
+                res = 1 + left[0], 1 + right[1]
             elif start == node.val:
-                ans = max(node_left[0], node_right[0])
+                ans = max(left[0], right[0])
                 res = None, 0
             else:
-                res = 1 + max(node_left[0], node_right[0]), None
+                res = 1 + max(left[0], right[0]), None
             return res
 
         ans = 0
