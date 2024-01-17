ec6634b002937ef97befef281bd281edbd90d9f9
Rocky Haotian Du <2712479005@qq.com>
2024-01-17 11:45:52 +0800

    Updated

--- code/py/src/leetcode/done/p2744.py
+++ code/py/src/leetcode/done/p2744.py
@@ -3,9 +3,11 @@ from rockyutil.leetcode import *
 
 class Solution:
     def maximumNumberOfStringPairs(self, words: List[str]) -> int:
-        res = 0
-        words_set = set(words)
+        ans = 0
+        seen = set()
         for word in words:
-            if word[::-1] in words_set and word[::-1] != word:
-                res += 1
-        return res // 2
+            if word[::-1] in seen:
+                ans += 1
+            else:
+                seen.add(word)
+        return ans
