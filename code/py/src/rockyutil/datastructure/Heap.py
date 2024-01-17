b221e5dff2dbf3d62ece458382b3c44e1c64d59d
Rocky Haotian Du <2712479005@qq.com>
2024-01-17 16:45:34 +0800

    Updated

--- code/py/src/rockyutil/datastructure/Heap.py
+++ code/py/src/rockyutil/datastructure/Heap.py
@@ -12,13 +12,11 @@ class Heap(object):
         self._reverse = reverse
         self._origin = list(__iterable)
         if self._key is None:
-            # self._heap = list(zip((Heap._converse(item) for item in self._origin) if self._reverse else (item for item in self._origin), range(len(self._origin))))
             if not self._reverse:
                 self._heap = list(zip((item for item in self._origin), range(len(self._origin))))
             else:
                 self._heap = list(zip((Heap._converse(item) for item in self._origin), range(len(self._origin))))
         else:
-            # self._heap = list(zip((Heap._converse(self._key(item)) for item in self._origin) if self._reverse else (self._key(item) for item in self._origin), range(len(self._origin))))
             if not self._reverse:
                 self._heap = list(zip((self._key(item) for item in self._origin), range(len(self._origin))))
             else:
@@ -26,10 +24,16 @@ class Heap(object):
         heapify(self._heap)
 
     def push(self, item):
-        heappush(self._heap, item)
+        self._origin.append(item)
+        heappush(self._heap, (item, len(self._heap)))
 
     def pop(self):
-        return heappop(self._heap)
+        item, i = heappop(self._heap)
+        self._origin[i] = self._origin.pop(-1)
+        for item_, i_ in self._heap:
+            if i_ == len(self._heap):
+                self._heap[i_] = item_, i
+        return item
 
     def replace(self, item):
         return heapreplace(self._heap, item)
