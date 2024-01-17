6580146f7cff3ba38091a8dfc8e799c18cf8c200
Rocky Haotian Du <2712479005@qq.com>
2024-01-18 00:39:19 +0800

    Updated

--- code/py/src/leetcode/done/p2171.py
+++ code/py/src/leetcode/done/p2171.py
@@ -3,11 +3,8 @@ from rockyutil.leetcode import *
 
 class Solution:
     def minimumRemoval(self, beans: List[int]) -> int:
-        ans = beans_sum = sum(beans)
-        beans.sort()
-        for times, bean in zip(range(len(beans), 0, -1), beans):
-            ans = min(ans, beans_sum - times * bean)
-        return ans
+        beans_sum = sum(beans)
+        return min(beans_sum - times * bean for times, bean in zip(range(len(beans), 0, -1), sorted(beans)))
 
 
 eg_beans = [3, 5, 6, 9]
