a30e540fa42100898996d7def71bfe9b55bfa90b
Rocky Haotian Du <2712479005@qq.com>
2024-01-17 16:36:24 +0800

    Updated

--- code/py/src/rockyutil/datastructure/Heap.py
+++ code/py/src/rockyutil/datastructure/Heap.py
@@ -1,45 +1,44 @@
-from _typeshed import SupportsRichComparison
-from heapq import heappush, heappop, heapreplace, heappushpop
-from typing import Callable, TypeVar, Iterable
-
-_T = TypeVar('_T')
+from heapq import heappush, heappop, heapreplace, heappushpop, heapify
 
 
 class Heap(object):
 
-    def __init__(
-            self,
-            __iterable: Iterable[_T],
-            *,
-            key: Callable[[_T], SupportsRichComparison] | None = None,
-            reverse: bool = False
-    ) -> None:
-        self._heap: list = list()
-        if key is None:
-            if not reverse:
-                ...
-            else:
-                ...
-        else:
-            if not reverse:
-                ...
-            else:
-                ...
+    @staticmethod
+    def _converse(item):
+        return -item
 
-    def push(self, item: _T) -> None:
+    def __init__(self, __iterable, *, key = None, reverse = False):
+        self._key = key
+        self._reverse = reverse
+        self._origin = list(__iterable)
+        if self._key is None:
+            # self._heap = list(zip((Heap._converse(item) for item in self._origin) if self._reverse else (item for item in self._origin), range(len(self._origin))))
+            if not self._reverse:
+                self._heap = list(zip((item for item in self._origin), range(len(self._origin))))
+            else:
+                self._heap = list(zip((Heap._converse(item) for item in self._origin), range(len(self._origin))))
+        else:
+            # self._heap = list(zip((Heap._converse(self._key(item)) for item in self._origin) if self._reverse else (self._key(item) for item in self._origin), range(len(self._origin))))
+            if not self._reverse:
+                self._heap = list(zip((self._key(item) for item in self._origin), range(len(self._origin))))
+            else:
+                self._heap = list(zip((Heap._converse(self._key(item)) for item in self._origin), range(len(self._origin))))
+        heapify(self._heap)
+
+    def push(self, item):
         heappush(self._heap, item)
 
-    def pop(self) -> _T:
+    def pop(self):
         return heappop(self._heap)
 
-    def replace(self, item: _T) -> _T:
+    def replace(self, item):
         return heapreplace(self._heap, item)
 
-    def pushpop(self, item: _T) -> _T:
+    def pushpop(self, item):
         return heappushpop(self._heap, item)
 
-    def peek(self, n: int = 1) -> list[_T]:
-        ...
+    def peek(self, n = 1):
+        return ...
 
 
 if __name__ == '__main__':
