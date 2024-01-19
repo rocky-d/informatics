6ab55740d5fce22779925ba79137ec6969f95da6
Rocky Haotian Du <2712479005@qq.com>
2024-01-19 08:52:45 +0800

    Updated

--- code/py/src/leetcode/done/until20231231/p931.py
+++ code/py/src/leetcode/done/until20231231/p931.py
@@ -4,17 +4,18 @@ from rockyutil.leetcode import *
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
-        n_1 = n - 1
-        first, second = [], matrix[0]
+        dp0, dp1 = [], matrix[0]
         for i in range(1, n):
-            first, second = second, [matrix[i][0] + min(second[0], second[1])]
-            for j in range(1, n_1):
-                second.append(matrix[i][j] + min(first[j - 1], first[j], first[j + 1]))
-            second.append(matrix[i][-1] + min(first[-2], first[-1]))
-        return min(second)
+            dp0, dp1 = dp1, [matrix[i][0] + min(dp1[0], dp1[1])]
+            for j in range(1, n - 1):
+                dp1.append(matrix[i][j] + min(dp0[j - 1], dp0[j], dp0[j + 1]))
+            dp1.append(matrix[i][-1] + min(dp0[-2], dp0[-1]))
+        return min(dp1)
 
 
-sol = Solution()
-
-example_matrix = [[2, 1, 3], [6, 5, 4], [7, 8, 9]]
-print(sol.minFallingPathSum(example_matrix))
+eg_matrix = [
+    [2, 1, 3],
+    [6, 5, 4],
+    [7, 8, 9]
+]
+print(Solution().minFallingPathSum(matrix = eg_matrix))
