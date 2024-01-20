f60244a68db24eac906fe4fe14cdd63ac3cf0b20
Rocky Haotian Du <2712479005@qq.com>
2024-01-20 13:50:06 +0800

    Updated

--- code/py/src/leetcode/done/p389.py
+++ code/py/src/leetcode/done/p389.py
@@ -1,6 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        t_list = list(t)
         for char in s:
-            t_list.remove(char)
-        return t_list[0]
+            t = t.replace(char, '', 1)
+        return t
