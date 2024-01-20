59f61aa9d3d7c64d9b15835dc2947e5a9286848a
Rocky Haotian Du <2712479005@qq.com>
2024-01-20 11:38:42 +0800

    Updated

--- code/py/src/leetcode/done/p2788.py
+++ code/py/src/leetcode/done/p2788.py
@@ -3,4 +3,9 @@ from rockyutil.leetcode import *
 
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
-        return sum([[w for w in word.split(separator) if 0 < len(w)] for word in words], [])
+        return reduce(list.__add__, [[w for w in word.split(separator) if 0 < len(w)] for word in words])
+
+
+eg_words = ['$easy$', '$no$problem$']
+eg_separator = '$'
+print(Solution().splitWordsBySeparator(words = eg_words, separator = eg_separator))
