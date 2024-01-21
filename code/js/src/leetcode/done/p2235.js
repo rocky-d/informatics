dfde256b58ad2764a8007599cfc0ff5ef0ca2327
Rocky Haotian Du <2712479005@qq.com>
2024-01-21 19:52:26 +0800

    Updated

--- code/js/src/leetcode/done/p2235.js
+++ code/js/src/leetcode/done/p2235.js
@@ -7,6 +7,6 @@ var sum = function (num1, num2) {
     return num1 + num2;
 };
 
-eg_num1 = 12
-eg_num2 = 5
-console.log(sum(eg_num1, eg_num2));
+egNum1 = 12
+egNum2 = 5
+console.log(sum(egNum1, egNum2));
--- code/js/src/leetcode/done/p2618.js
+++ code/js/src/leetcode/done/p2618.js
@@ -7,6 +7,6 @@ var checkIfInstanceOf = function (obj, classFunction) {
     return obj === null || obj === undefined || !(classFunction instanceof Function) ? false : Object(obj) instanceof classFunction;
 };
 
-eg_obj = new Date()
-eg_classFunction = Date
-console.log(checkIfInstanceOf(eg_obj, eg_classFunction));
+egObj = new Date()
+egClassFunction = Date
+console.log(checkIfInstanceOf(egObj, egClassFunction));
