358004197cb9a4b463860bdc31893ed4a461241d
Rocky Haotian Du <2712479005@qq.com>
2024-01-21 13:38:13 +0800

    Updated

--- code/py/src/leetcode/done/p410.py
+++ code/py/src/leetcode/done/p410.py
@@ -4,15 +4,14 @@ from rockyutil.leetcode import *
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         m, n = k, len(nums)
-        presum = [0]
-        for num in nums:
-            presum.append(presum[-1] + num)
         dp = [[1_000_000_001 for _ in range(1 + m)] for _ in range(1 + n)]
         dp[0][0] = 0
         for i in range(1, 1 + n):
             for j in range(1, 1 + min(i, m)):
-                for k in range(j - 1, i):
-                    dp[i][j] = min(dp[i][j], max(dp[k][j - 1], presum[i] - presum[k]))
+                sum_ = 0
+                for k in reversed(range(j - 1, i)):
+                    sum_ += nums[k]
+                    dp[i][j] = min(dp[i][j], max(dp[k][j - 1], sum_))
         return dp[-1][-1]
 
 
