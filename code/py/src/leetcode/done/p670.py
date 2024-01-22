36d032b93ff51a7c6af219a3d60676803fa7643b
Rocky Haotian Du <2712479005@qq.com>
2024-01-22 23:43:08 +0800

    Updated

--- code/py/src/leetcode/done/p670.py
+++ code/py/src/leetcode/done/p670.py
@@ -1,17 +1,17 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        num_str = str(num)
-        n = len(num_str)
+        s = str(num)
+        n = len(s)
         for i in range(n - 1):
-            digit_i = int(num_str[i])
+            digit_i = int(s[i])
             switch = digit_i, i
             for j in range(i + 1, n):
-                digit_j = int(num_str[j])
+                digit_j = int(s[j])
                 if switch[0] <= digit_j:
                     switch = digit_j, j
             if digit_i != switch[0]:
                 j = switch[1]
-                ans = int(num_str[:i] + num_str[j] + num_str[i + 1:j] + num_str[i] + num_str[j + 1:])
+                ans = int(s[:i] + s[j] + s[i + 1:j] + s[i] + s[j + 1:])
                 break
         else:
             ans = num
