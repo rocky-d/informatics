737a2cee2de4650bd9f93797998cb8c7307ab4e2
Rocky Haotian Du <2712479005@qq.com>
2024-01-23 00:38:59 +0800

    Updated

--- code/py/src/leetcode/done/p2765.py
+++ code/py/src/leetcode/done/p2765.py
@@ -4,23 +4,22 @@ from rockyutil.leetcode import *
 class Solution:
     def alternatingSubarray(self, nums: List[int]) -> int:
         n = len(nums)
-        ans = -1
+        ans = 1
         i = 0
         while i < n:
             len_ = 1
             gap = 1
             tag = False
-            while i + 1 < n and gap == nums[i + 1] - nums[i]:
+            i += 1
+            while i < n and gap == nums[i] - nums[i - 1]:
                 len_ += 1
                 gap = -gap
-                i += 1
                 tag = True
-            if not tag:
                 i += 1
             ans = max(ans, len_)
-        if 1 == ans:
-            ans = -1
-        return ans
+            if tag:
+                i -= 1
+        return -1 if 1 == ans else ans
 
 
 eg_nums = [2, 3, 4, 3, 4]
