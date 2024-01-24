6f4fa01b4e08483ada0e30483a1f8db1368a34f3
Rocky Haotian Du <2712479005@qq.com>
2024-01-24 13:57:45 +0800

    Updated

--- tsrun.cmd
+++ tsrun.cmd
@@ -18,13 +18,7 @@ set "output_dir=%~dp0code\ts\out"
 for %%i in ("%~1") do (
     cd "%%~dpi"
     npx tsc "%%~nxi" --outFile "%output_dir%\%output_name%.js"
+    node "%output_dir%\%output_name%.js"
 )
-
-if errorlevel 1 (
-    echo %script_name% ^>^> Fail to compile. :(
-    exit /b 1
-)
-
-node "%output_dir%\%output_name%.js"
 
 endlocal
