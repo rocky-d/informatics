16dd572325f05a6207cf6bc0de9bd8b29e40ba64
Rocky Haotian Du <2712479005@qq.com>
2024-01-24 13:06:19 +0800

    Updated

--- code/ts/src/leetcode/done/p2235.ts
+++ code/ts/src/leetcode/done/p2235.ts
@@ -2,6 +2,6 @@ function sum(num1: number, num2: number): number {
     return num1 + num2;
 }
 
-let egNum1: number = 12;
-let egNum2: number = 5;
+let egNum1 = 12;
+let egNum2 = 5;
 console.log(sum(egNum1, egNum2));
--- code/ts/src/leetcode/done/p2618.ts
+++ code/ts/src/leetcode/done/p2618.ts
@@ -2,6 +2,6 @@ function checkIfInstanceOf(obj: any, classFunction: any): boolean {
     return obj === null || obj === undefined || !(classFunction instanceof Function) ? false : Object(obj) instanceof classFunction;
 }
 
-let egObj: Date = new Date();
-let egClassFunction: Function = Date;
+let egObj = new Date();
+let egClassFunction = Date;
 console.log(checkIfInstanceOf(egObj, egClassFunction));
