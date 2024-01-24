78c68862a85066f68664ad9cc2fd16744d8ca6c5
Rocky Haotian Du <2712479005@qq.com>
2024-01-24 15:11:11 +0800

    Updated

--- code/py/src/leetcode/undone/until20231231/p2866.py
+++ code/py/src/leetcode/undone/until20231231/p2866.py
@@ -5,19 +5,19 @@ class Solution:
     def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
         n = len(maxHeights)
         ans = 0
-        prefix, suffix = [0 for _ in range(n)], [0 for _ in range(n)]
+        pre, suf = [0 for _ in range(n)], [0 for _ in range(n)]
         stack1, stack2 = [], []
         for i in range(0, n, +1):
             while 0 < len(stack1) and maxHeights[i] < maxHeights[stack1[-1]]:
                 stack1.pop()
-            prefix[i] = (1 + i) * maxHeights[i] if 0 == len(stack1) else prefix[stack1[-1]] + (i - stack1[-1]) * maxHeights[i]
+            pre[i] = (1 + i) * maxHeights[i] if 0 == len(stack1) else pre[stack1[-1]] + (i - stack1[-1]) * maxHeights[i]
             stack1.append(i)
         for i in range(n - 1, -1, -1):
             while 0 < len(stack2) and maxHeights[i] < maxHeights[stack2[-1]]:
                 stack2.pop()
-            suffix[i] = (n - i) * maxHeights[i] if 0 == len(stack2) else suffix[stack2[-1]] + (stack2[-1] - i) * maxHeights[i]
+            suf[i] = (n - i) * maxHeights[i] if 0 == len(stack2) else suf[stack2[-1]] + (stack2[-1] - i) * maxHeights[i]
             stack2.append(i)
-            ans = max(ans, prefix[i] + suffix[i] - maxHeights[i])
+            ans = max(ans, pre[i] + suf[i] - maxHeights[i])
         return ans
 
 
