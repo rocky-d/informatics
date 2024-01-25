e942334eb6fc7c70d0345df8d862b448c6737b4b
Rocky Haotian Du <2712479005@qq.com>
2024-01-25 22:05:58 +0800

    Updated

--- code/py/src/pta/done/ladder2023s/p1_5.py
+++ code/py/src/pta/done/ladder2023s/p1_5.py
@@ -2,12 +2,12 @@ def main() -> None:
     dice = list(map(int, input().split()))
     n = int(input())
 
-    dice_s = list(0b1 << (die - 1) for die in dice)
+    dice_s = list(0b1 << die for die in dice)
     for _ in range(n):
         for j in range(6):
             for k in range(6, 0, -1):
-                if 0b0 == 0b1 & (dice_s[j] >> (k - 1)):
-                    dice_s[j] |= 0b1 << (k - 1)
+                if 0b0 == 0b1 & (dice_s[j] >> k):
+                    dice_s[j] |= 0b1 << k
                     dice[j] = k
                     break
     print(*dice)
