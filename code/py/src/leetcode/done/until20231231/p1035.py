b319ed617c0eb2fac7640a0aa90d18c085e36423
Rocky Haotian Du <2712479005@qq.com>
2024-01-25 12:28:20 +0800

    Updated

--- code/py/src/leetcode/done/until20231231/p1035.py
+++ code/py/src/leetcode/done/until20231231/p1035.py
@@ -3,9 +3,9 @@ from rockyutil.leetcode import *
 
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        dp1, dp2 = [0] + [0 for _ in nums2], [0] + [0 for _ in nums2]
-        for nums1_i in nums1:
-            for j, nums2_j in enumerate(nums2):
-                dp2[j + 1] = dp1[j] + 1 if nums1_i == nums2_j else max(dp1[j + 1], dp2[j])
-            dp1, dp2 = dp2, dp1
-        return dp1[-1]
+        dp0, dp1 = [0] + [0 for _ in nums2], [0] + [0 for _ in nums2]
+        for char1 in nums1:
+            for j, char2 in enumerate(nums2):
+                dp1[j + 1] = dp0[j] + 1 if char1 == char2 else max(dp0[j + 1], dp1[j])
+            dp0, dp1 = dp1, dp0
+        return dp0[-1]
--- code/py/src/leetcode/done/until20231231/p1143.py
+++ code/py/src/leetcode/done/until20231231/p1143.py
@@ -1,15 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp1, dp2 = [0] + [0 for _ in text2], [0] + [0 for _ in text2]
-        for text1_i in text1:
-            for j, text2_j in enumerate(text2):
-                dp2[j + 1] = dp1[j] + 1 if text1_i == text2_j else max(dp1[j + 1], dp2[j])
-            dp1, dp2 = dp2, dp1
-        return dp1[-1]
+        dp0, dp1 = [0] + [0 for _ in text2], [0] + [0 for _ in text2]
+        for char1 in text1:
+            for j, char2 in enumerate(text2):
+                dp1[j + 1] = dp0[j] + 1 if char1 == char2 else max(dp0[j + 1], dp1[j])
+            dp0, dp1 = dp1, dp0
+        return dp0[-1]
 
 
-sol = Solution()
-
-eg_text1 = "abcde"
-eg_text2 = "ace"
-print(sol.longestCommonSubsequence(text1 = eg_text1, text2 = eg_text2))
+eg_text1 = 'abcde'
+eg_text2 = 'ace'
+print(Solution().longestCommonSubsequence(text1 = eg_text1, text2 = eg_text2))
