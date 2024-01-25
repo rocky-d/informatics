1a07b4b01a7706fb8843823ab1012073fa67718f
Rocky Haotian Du <2712479005@qq.com>
2024-01-25 18:06:31 +0800

    Updated

--- code/py/src/pta/done/ladder2023s/p1_3.py
+++ code/py/src/pta/done/ladder2023s/p1_3.py
@@ -1,11 +1,8 @@
 def main() -> None:
     a, b = map(int, input().split())
-    if 0 < a and 0 < b:
-        ans = f"{max(a, b)} {a + b}\n^_^"
-    elif a < 0 and b < 0:
-        ans = '0 0\n-_-'
-    else:
-        ans = f"{max(a, b)} {max(0, a + b)}\nT_T"
+    x, y = max(0, a, b), max(0, a + b)
+    ans = '^_^' if x < y else 'T_T' if y < x else '-_-'
+    print(x, y)
     print(ans)
 
 
