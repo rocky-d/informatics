99a079bfd76fcef15f1b98d173dff1c084bccae5
Rocky Haotian Du <2712479005@qq.com>
2024-01-25 17:05:31 +0800

    Updated

--- code/py/src/leetcode/undone/p464.py
+++ code/py/src/leetcode/undone/p464.py
@@ -7,17 +7,17 @@ class Solution:
             return False
 
         @cache
-        def dfs(nums: int, total: int) -> bool:
+        def dfs(nums: int, remain: int) -> bool:
             for i in range(maxChoosableInteger):
                 if 0b0 == 0b1 & (nums >> i):
-                    if desiredTotal <= total + i + 1 or not dfs(nums = nums | (1 << i), total = total + i + 1):
+                    if remain - i - 1 <= 0 or not dfs(nums = nums | (1 << i), remain = remain - i - 1):
                         res = True
                         break
             else:
                 res = False
             return res
 
-        return dfs(nums = 0, total = 0)
+        return dfs(nums = 0, remain = desiredTotal)
 
 
 eg_maxChoosableInteger = 22
