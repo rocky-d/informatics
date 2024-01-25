d72626f91f20779b91e511030d2e99e0653f0f5f
Rocky Haotian Du <2712479005@qq.com>
2024-01-25 12:20:43 +0800

    Updated

--- code/py/src/leetcode/done/p2859.py
+++ code/py/src/leetcode/done/p2859.py
@@ -3,8 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
-        ans = 0
-        for i, num in enumerate(nums):
-            if k == bin(i)[2:].count('1'):
-                ans += num
-        return ans
+        return sum(nums[i] for i in filter(lambda i: k == bin(i)[2:].count('1'), range(len(nums))))
