5097d8b2fcaded8f665804300289bb4c1a9dbc70
Rocky Haotian Du <2712479005@qq.com>
2024-01-25 16:34:33 +0800

    Updated

---
+++ code/py/src/leetcode/undone/p464.py
@@ -0,0 +1,20 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        if maxChoosableInteger * (1 + maxChoosableInteger) // 2 < desiredTotal:
+            return False
+
+        @cache
+        def dfs(nums: int, total: int) -> bool:
+            for i in range(maxChoosableInteger):
+                if 0b0 == 0b1 & (nums >> i):
+                    if desiredTotal <= total + i + 1 or not dfs(nums = nums | (1 << i), total = total + i + 1):
+                        res = True
+                        break
+            else:
+                res = False
+            return res
+
+        return dfs(nums = 0, total = 0)
