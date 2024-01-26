7213aceb6b59a2731594dedbab48b651ffff32c2
Rocky Haotian Du <2712479005@qq.com>
2024-01-26 14:09:14 +0800

    Updated

--- code/kt/src/rockyutil/leetcode.kt
+++ code/kt/src/rockyutil/leetcode.kt
@@ -1,5 +1,10 @@
 package rockyutil
 
+// Definition for singly-linked list.
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
 // Definition for a binary tree node.
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
