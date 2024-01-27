8289e390646e653f0e7da9b1b3892ca61e9dfdfc
Rocky Haotian Du <2712479005@qq.com>
2024-01-27 16:22:34 +0800

    Updated

--- code/py/src/leetcode/done/p629.py
+++ code/py/src/leetcode/done/p629.py
@@ -1,16 +1,12 @@
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
-        dp = [1] + [0 for _ in range(k)]
         pre = [0] + [1 for _ in range(1 + k)]
         for i in range(1, n):
-            dp_last = dp
-            dp = [1]
             pre_last = pre
             pre = [0, 1]
             for j in range(1, 1 + k):
-                dp.append(pre_last[1 + j] - pre_last[max(0, j - i)])
-                pre.append(pre[-1] + dp[-1])
-        return dp[-1] % 1_000_000_007
+                pre.append(pre[-1] + pre_last[1 + j] - pre_last[max(0, j - i)])
+        return (pre[-1] - pre[-2]) % 1_000_000_007
 
 
 eg_n = 3
