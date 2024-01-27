720af80f348e184d1a89663dc2e498ed6cee6252
Rocky Haotian Du <2712479005@qq.com>
2024-01-27 15:13:31 +0800

    Updated

--- code/py/src/leetcode/undone/p2861.py
+++ code/py/src/leetcode/undone/p2861.py
@@ -3,4 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def maxNumberOfAlloys(self, n: int, k: int, budget: int, composition: List[List[int]], stock: List[int], cost: List[int]) -> int:
-        return max(bisect_right(tuple(sum(cost[k] * max(0, j * metals[k] - stock[k]) for k in range(n)) for j in range(1, 3000)), budget) for metals in composition)
+        return max(bisect_right(range(1, 1_000_000_000), budget, key = lambda count: sum(cost[j] * max(0, count * composition[i][j] - stock[j]) for j in range(n))) for i in range(k))
