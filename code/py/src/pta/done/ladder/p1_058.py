7c81af8f1bc4c55e6affc6fbc062b06abb6e6839
Rocky Haotian Du <2712479005@qq.com>
2024-01-28 22:08:41 +0800

    Updated

---
+++ code/py/src/pta/done/ladder/p1_058.py
@@ -0,0 +1,27 @@
+def main() -> None:
+    s = input()
+
+    ans = ''
+    n = len(s)
+    i = 0
+    while i < n:
+        add = s[i]
+        cnt = 0
+        while i < n and '6' == s[i]:
+            cnt += 1
+            i += 1
+        if 0 == cnt:
+            i += 1
+        else:
+            if cnt <= 3:
+                add = '6' * cnt
+            elif cnt <= 9:
+                add = '9'
+            else:
+                add = '27'
+        ans += add
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
