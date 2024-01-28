760ded514367934f5594d77d3786c44c9debda4a
Rocky Haotian Du <2712479005@qq.com>
2024-01-28 20:44:58 +0800

    Updated

--- code/py/src/leetcode/done/p365.py
+++ code/py/src/leetcode/done/p365.py
@@ -7,10 +7,8 @@ class Solution:
                 return False
             seen.add((jug1, jug2))
             if targetCapacity in (jug1, jug2, jug1 + jug2):
-                return True
-            transfer1 = min(jug1, jug2Capacity - jug2)
-            transfer2 = min(jug2, jug1Capacity - jug1)
-            if dfs(jug1 = 0, jug2 = jug2):
+                res = True
+            elif dfs(jug1 = 0, jug2 = jug2):
                 res = True
             elif dfs(jug1 = jug1, jug2 = 0):
                 res = True
@@ -18,9 +16,9 @@ class Solution:
                 res = True
             elif dfs(jug1 = jug1, jug2 = jug2Capacity):
                 res = True
-            elif dfs(jug1 = jug1 - transfer1, jug2 = jug2 + transfer1):
+            elif dfs(jug1 = jug1 - min(jug1, jug2Capacity - jug2), jug2 = jug2 + min(jug1, jug2Capacity - jug2)):
                 res = True
-            elif dfs(jug1 = jug1 + transfer2, jug2 = jug2 - transfer2):
+            elif dfs(jug1 = jug1 + min(jug2, jug1Capacity - jug1), jug2 = jug2 - min(jug2, jug1Capacity - jug1)):
                 res = True
             else:
                 res = False
