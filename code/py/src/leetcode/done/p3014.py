17d7177a48c4f9ab51ca117a01b84877d08d1a15
Rocky Haotian Du <2712479005@qq.com>
2024-01-28 12:33:09 +0800

    Updated

---
+++ code/py/src/leetcode/done/p3014.py
@@ -0,0 +1,15 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        ans = 0
+        buttons = 8
+        for count in sorted(Counter(word).values(), reverse = True):
+            ans += count * (buttons // 8)
+            buttons += 1
+        return ans
+
+
+eg_word = 'abcde'
+print(Solution().minimumPushes(word = eg_word))
