fdccd39a669a83e7e213abf3cf5551cb83273f9c
Rocky Haotian Du <2712479005@qq.com>
2024-01-28 20:39:38 +0800

    Updated

---
+++ code/py/src/leetcode/done/p365.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
+        seen = set()
+
+        def dfs(jug1, jug2) -> bool:
+            if (jug1, jug2) in seen:
+                return False
+            seen.add((jug1, jug2))
+            if targetCapacity in (jug1, jug2, jug1 + jug2):
+                return True
+            transfer1 = min(jug1, jug2Capacity - jug2)
+            transfer2 = min(jug2, jug1Capacity - jug1)
+            if dfs(jug1 = 0, jug2 = jug2):
+                res = True
+            elif dfs(jug1 = jug1, jug2 = 0):
+                res = True
+            elif dfs(jug1 = jug1Capacity, jug2 = jug2):
+                res = True
+            elif dfs(jug1 = jug1, jug2 = jug2Capacity):
+                res = True
+            elif dfs(jug1 = jug1 - transfer1, jug2 = jug2 + transfer1):
+                res = True
+            elif dfs(jug1 = jug1 + transfer2, jug2 = jug2 - transfer2):
+                res = True
+            else:
+                res = False
+            return res
+
+        return dfs(jug1 = 0, jug2 = 0)
