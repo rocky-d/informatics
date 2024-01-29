dab9bf0a34bb411f86434f2729d28ded3212b78c
Rocky Haotian Du <2712479005@qq.com>
2024-01-29 13:40:38 +0800

    Updated

--- code/py/src/leetcode/done/p514.py
+++ code/py/src/leetcode/done/p514.py
@@ -1,19 +1,18 @@
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
         ring_len = len(ring)
-        dp = [10_001 for _ in range(ring_len)]
-        dp[0] = 0
+        dp = {0: 0}
         for char in key:
-            dp_last, dp = dp, [10_001 for _ in range(ring_len)]
-            for index, count in enumerate(dp_last):
+            dp_last, dp = dp, {}
+            for index, count in dp_last.items():
                 ring_rotated = ring[index:] + ring[:index]
                 right_steps = ring_rotated.index(char)
                 left_steps = 1 + ring_rotated[::-1].index(char)
                 right_index = (index + right_steps) % ring_len
                 left_index = (index - left_steps + ring_len) % ring_len
-                dp[right_index] = min(dp[right_index], count + right_steps + 1)
-                dp[left_index] = min(dp[left_index], count + left_steps + 1)
-        return min(dp)
+                dp[right_index] = min(dp.get(right_index, 10_001), count + right_steps + 1)
+                dp[left_index] = min(dp.get(left_index, 10_001), count + left_steps + 1)
+        return min(dp.values())
 
 
 eg_ring = 'nyngl'
