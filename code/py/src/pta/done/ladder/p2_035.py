a2da66e55bd13c5a102d9fbbc1f2b679de910061
Rocky Haotian Du <2712479005@qq.com>
2024-01-29 14:41:28 +0800

    Updated

--- code/py/src/pta/done/ladder/p2_035.py
+++ code/py/src/pta/done/ladder/p2_035.py
@@ -1,18 +1,15 @@
 def main() -> None:
     n = int(input())
-    nums = list(map(int, input().split()))
+    nums = map(int, input().split())
 
     tree = [0 for _ in range(n)]
-    p = 0
 
     def fill(index: int) -> None:
         if n < index:
             return
-        nonlocal p
         fill(2 * index)
         fill(2 * index + 1)
-        tree[index - 1] = nums[p]
-        p += 1
+        tree[index - 1] = next(nums)
 
     fill(1)
     print(*tree)
