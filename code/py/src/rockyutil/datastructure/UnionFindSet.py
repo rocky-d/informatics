188298723fb0fc833adfa97c8878fd7d9c0ea81b
Rocky Haotian Du <2712479005@qq.com>
2024-01-29 18:39:36 +0800

    Updated

--- code/py/src/rockyutil/datastructure/UnionFindSet.py
+++ code/py/src/rockyutil/datastructure/UnionFindSet.py
@@ -1,37 +1,37 @@
 class UnionFindSet(object):
 
     def __init__(self, __supports_getitem):
-        self._heads = __supports_getitem
+        self.heads = __supports_getitem
 
     def find1(self, a):
-        if a == self._heads[a]:
+        if a == self.heads[a]:
             return a
-        return self.find1(self._heads[a])
+        return self.find1(self.heads[a])
 
     def find2(self, a):
-        if a == self._heads[a]:
+        if a == self.heads[a]:
             return a
-        self._heads[a] = self.find2(self._heads[a])
-        return self._heads[a]
+        self.heads[a] = self.find2(self.heads[a])
+        return self.heads[a]
 
     def find3(self, a):
-        while a != self._heads[a]:
-            a = self._heads[a]
+        while a != self.heads[a]:
+            a = self.heads[a]
         return a
 
     def find4(self, a):
         a_old = a
         cnt = 0
-        while a != self._heads[a]:
+        while a != self.heads[a]:
             cnt += 1
-            a = self._heads[a]
+            a = self.heads[a]
         a, head = a_old, a
         for _ in range(cnt):
-            self._heads[a] = head
-            a = self._heads[a]
+            self.heads[a] = head
+            a = self.heads[a]
         return head
 
     find = find4
 
     def union(self, a, b):
-        self._heads[a] = self._heads[self.find(a)] = self.find(b)
+        self.heads[a] = self.heads[self.find(a)] = self.find(b)
