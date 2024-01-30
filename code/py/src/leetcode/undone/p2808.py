507c4103a8fbed7e1b5abaf9f41d4233f9e6d711
Rocky Haotian Du <2712479005@qq.com>
2024-01-30 14:05:33 +0800

    Updated

--- code/py/src/leetcode/undone/p2808.py
+++ code/py/src/leetcode/undone/p2808.py
@@ -3,22 +3,16 @@ from rockyutil.leetcode import *
 
 class Solution:
     def minimumSeconds(self, nums: List[int]) -> int:
-        n = len(nums)
-        left, right = floor((n - 1) / 2), ceil((n - 1) / 2)
-        dists, nums_frozenset = {num: 0 for num in nums}, frozenset(nums)
+        ans = n = len(nums)
+        positions = defaultdict(lambda: [])
         for i, num in enumerate(nums):
-            dists_i = {num: n for num in nums}
-            for j in range(1, 1 + left):
-                index = i - j
-                if num != nums[index]:
-                    dists_i[nums[index]] = min(dists_i[nums[index]], j)
-            for j in range(1, 1 + right):
-                index = (i + j) % n
-                if num != nums[index]:
-                    dists_i[nums[index]] = min(dists_i[nums[index]], j)
-            for target in nums_frozenset - frozenset((num,)):
-                dists[target] = max(dists[target], dists_i[target])
-        return min(dists.values())
+            positions[num].append(i)
+        for p in positions.values():
+            res = n + p[0] - p[-1]
+            for i in range(len(p)):
+                res = max(res, p[i] - p[i - 1])
+            ans = min(ans, res // 2)
+        return ans
 
 
 eg_nums = [4, 8, 8, 13]
