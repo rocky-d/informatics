6f2b1106dfdd924ac4f8b0f5d2e147b9f2ec75d7
Rocky Haotian Du <2712479005@qq.com>
2024-01-30 20:08:38 +0800

    Updated

--- code/py/src/rockyutil/datastructure/unionfind.py
+++ code/py/src/rockyutil/datastructure/unionfind.py
@@ -7,23 +7,21 @@ class UnionFindList(object):
     def __len__(self):
         return len(self._heads), len(self._groups)
 
-    def find1(self, a):
-        a_old = a
-        cnt = 0
-        while a != self._heads[a]:
-            cnt += 1
-            a = self._heads[a]
-        a, head = a_old, a
-        for _ in range(cnt):
-            self._heads[a] = head
-            a = self._heads[a]
-        return head
-
     # def find1(self, a):
     #     if a == self._heads[a]:
     #         return a
     #     self._heads[a] = self.find1(self._heads[a])
     #     return self._heads[a]
+    def find1(self, a):
+        a_ = a
+        cnt = 0
+        while a != self._heads[a]:
+            cnt += 1
+            a = self._heads[a]
+        for _ in range(cnt):
+            self._heads[a_] = a
+            a_ = self._heads[a_]
+        return a
 
     def union1(self, a, b):
         a_head, b_head = self.find1(a), self.find1(b)
@@ -37,15 +35,14 @@ class UnionFindList(object):
                 if self._groups is not None:
                     self._groups[a_head] += self._groups.pop(b_head)
 
-    def find2(self, a):
-        while a != self._heads[a]:
-            a = self._heads[a]
-        return a
-
     # def find2(self, a):
     #     if a == self._heads[a]:
     #         return a
     #     return self.find2(self._heads[a])
+    def find2(self, a):
+        while a != self._heads[a]:
+            a = self._heads[a]
+        return a
 
     def union2(self, a, b):
         a_head, b_head = self.find2(a), self.find2(b)
