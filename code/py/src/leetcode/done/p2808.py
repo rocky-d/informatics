6db51a745b963de54c4e207d0d23aa9af46f54d0
Rocky Haotian Du <2712479005@qq.com>
2024-01-30 14:20:05 +0800

    Updated

--- code/py/src/leetcode/done/p2808.py
+++ code/py/src/leetcode/done/p2808.py
@@ -8,10 +8,10 @@ class Solution:
         for i, num in enumerate(nums):
             positions[num].append(i)
         for p in positions.values():
-            res = n + p[0] - p[-1]
-            for i in range(len(p)):
-                res = max(res, p[i] - p[i - 1])
-            ans = min(ans, res // 2)
+            dis = n + p[0] - p[-1]
+            for i in range(1, len(p)):
+                dis = max(dis, p[i] - p[i - 1])
+            ans = min(ans, dis // 2)
         return ans
 
 
