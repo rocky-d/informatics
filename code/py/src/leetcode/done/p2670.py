fc8acc29861f29ba1ef85f4f099bd71fc7f9f590
Rocky Haotian Du <2712479005@qq.com>
2024-01-31 13:12:59 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2670.py
@@ -0,0 +1,20 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        left_diffs = []
+        nums_set = set()
+        for num in nums:
+            nums_set.add(num)
+            left_diffs.append(len(nums_set))
+        right_diffs = []
+        nums_set = set()
+        for num in reversed(nums):
+            right_diffs.insert(0, len(nums_set))
+            nums_set.add(num)
+        return [left_diff - right_diff for left_diff, right_diff in zip(left_diffs, right_diffs)]
+
+
+eg_nums = [3, 2, 3, 4, 2]
+print(Solution().distinctDifferenceArray(nums = eg_nums))
