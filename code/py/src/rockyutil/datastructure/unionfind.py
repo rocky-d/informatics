6325abf3d336100de9337fa09b3409c86942117c
Rocky Haotian Du <2712479005@qq.com>
2024-01-31 16:09:27 +0800

    Updated

--- code/py/src/rockyutil/datastructure/unionfind.py
+++ code/py/src/rockyutil/datastructure/unionfind.py
@@ -8,33 +8,33 @@ class UnionFind(object):
         else:
             self._groups = None
         if recursive:
-            self.find_c = self.find_cr
-            self.find_r = self.find_rr
+            self._find_c = self._find_cr
+            self._find_r = self._find_rr
         else:
-            self.find_c = self.find_ci
-            self.find_r = self.find_ri
+            self._find_c = self._find_ci
+            self._find_r = self._find_ri
         if compressed:
             self._ranks = None
-            self.find = self.find_c
-            self.union = self.union_c
+            self.find = self._find_c
+            self.union = self._union_c
         else:
             if self._generic:
                 self._ranks = {x: 0 for x in self._heads}
             else:
                 self._ranks = [0 for _ in self._heads]
-            self.find = self.find_r
-            self.union = self.union_r
+            self.find = self._find_r
+            self.union = self._union_r
 
     def __len__(self):
         return len(self._heads), len(self._groups)
 
-    def find_cr(self, a):
+    def _find_cr(self, a):
         if a == self._heads[a]:
             return a
-        self._heads[a] = self.find_cr(self._heads[a])
+        self._heads[a] = self._find_cr(self._heads[a])
         return self._heads[a]
 
-    def find_ci(self, a):
+    def _find_ci(self, a):
         a_ = a
         cnt = 0
         while a != self._heads[a]:
@@ -45,8 +45,8 @@ class UnionFind(object):
             a_ = self._heads[a_]
         return a
 
-    def union_c(self, a, b):
-        a_head, b_head = self.find_c(a), self.find_c(b)
+    def _union_c(self, a, b):
+        a_head, b_head = self._find_c(a), self._find_c(b)
         if a_head != b_head:
             if len(self._groups[a_head]) < len(self._groups[b_head]):
                 self._heads[a] = self._heads[a_head] = b_head
@@ -57,18 +57,18 @@ class UnionFind(object):
                 if self._groups is not None:
                     self._groups[a_head] += self._groups.pop(b_head)
 
-    def find_rr(self, a):
+    def _find_rr(self, a):
         if a == self._heads[a]:
             return a
-        return self.find_rr(self._heads[a])
+        return self._find_rr(self._heads[a])
 
-    def find_ri(self, a):
+    def _find_ri(self, a):
         while a != self._heads[a]:
             a = self._heads[a]
         return a
 
-    def union_r(self, a, b):
-        a_head, b_head = self.find_r(a), self.find_r(b)
+    def _union_r(self, a, b):
+        a_head, b_head = self._find_r(a), self._find_r(b)
         if a_head != b_head:
             if self._ranks[a_head] < self._ranks[b_head]:
                 self._heads[a_head] = b_head
@@ -99,5 +99,11 @@ class UnionFindDict(UnionFind):
 
 
 if __name__ == '__main__':
+    import random
+
     ufl = UnionFindList(1_000, grouped = True, recursive = True, compressed = False)
     ufd = UnionFindDict((-i for i in range(1_000)), grouped = True, recursive = True, compressed = False)
+
+    for _ in range(200):
+        x, y = random.randint(a = 0, b = 999), random.randint(a = 0, b = 999)
+        ufl.union
