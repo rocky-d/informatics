e929305ac757f73524073cc1c94226ed46b03768
Rocky Haotian Du <2712479005@qq.com>
2024-02-02 00:24:50 +0800

    Updated

--- code/py/src/leetcode/done/p1686.py
+++ code/py/src/leetcode/done/p1686.py
@@ -3,12 +3,7 @@ from rockyutil.leetcode import *
 
 class Solution:
     def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
-        a_points, b_points = 0, 0
-        a_next = True
-        for i, total_value in sorted(((i, a_value + b_value) for i, (a_value, b_value) in enumerate(zip(aliceValues, bobValues))), key = lambda item: item[1], reverse = True):
-            if a_next:
-                a_points += aliceValues[i]
-            else:
-                b_points += bobValues[i]
-            a_next = not a_next
+        n = len(aliceValues)
+        total_values = sorted(((i, a_value + b_value) for i, (a_value, b_value) in enumerate(zip(aliceValues, bobValues))), key = lambda item: item[1], reverse = True)
+        a_points, b_points = sum(aliceValues[total_values[i][0]] for i in range(0, n, 2)), sum(bobValues[total_values[i][0]] for i in range(1, n, 2))
         return 1 if b_points < a_points else -1 if a_points < b_points else 0
