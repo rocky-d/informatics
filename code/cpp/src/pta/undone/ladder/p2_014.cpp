239ec856945655801c2a40d0a3484a92ad498fe3
Rocky Haotian Du <2712479005@qq.com>
2024-02-01 13:52:29 +0800

    Updated

--- code/cpp/src/pta/undone/ladder/p2_014.cpp
+++ code/cpp/src/pta/undone/ladder/p2_014.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, nums[100000];
+const int N_MAX = 100000;
+
+int n, nums[N_MAX];
 
 int main() {
     cin >> n;
@@ -9,7 +11,7 @@ int main() {
         cin >> nums[i];
     }
 
-    int q_heads[100000], q_tails[100000], queues[100000][100000], queues_len, queues_len_max, waiting;
+    int q_heads[N_MAX], q_tails[N_MAX], queues[N_MAX][N_MAX], queues_len, queues_len_max, waiting;
     queues_len = 0;
     queues_len_max = 0;
     waiting = n;
@@ -20,15 +22,15 @@ int main() {
                 bool loopelse = true;
                 for (int j = 0; j != queues_len; j += 1) {
                     if (waiting == queues[j][q_heads[j]]) {
-                        if (1 == (q_tails[j] - q_heads[j] + 100000) % 100000) {
+                        if (1 == (q_tails[j] - q_heads[j] + N_MAX) % N_MAX) {
                             queues_len -= 1;
-                            for (int k = q_heads[queues_len]; k != q_tails[queues_len]; k = (1 + k) % 100000) {
+                            for (int k = q_heads[queues_len]; k != q_tails[queues_len]; k = (1 + k) % N_MAX) {
                                 queues[j][k] = queues[queues_len][k];
                             }
                             q_heads[j] = q_heads[queues_len];
                             q_tails[j] = q_tails[queues_len];
                         } else {
-                            q_heads[j] = (1 + q_heads[j]) % 100000;
+                            q_heads[j] = (1 + q_heads[j]) % N_MAX;
                         }
                         waiting -= 1;
                         loopelse = false;
@@ -43,7 +45,7 @@ int main() {
             for (int j = 0; j != queues_len; j += 1) {
                 if (nums[i] < queues[j][q_tails[j] - 1]) {
                     queues[j][q_tails[j]] = nums[i];
-                    q_tails[j] = (1 + q_tails[j]) % 100000;
+                    q_tails[j] = (1 + q_tails[j]) % N_MAX;
                     loopelse = false;
                     break;
                 }
@@ -51,7 +53,7 @@ int main() {
                 q_heads[queues_len] = 0;
                 q_tails[queues_len] = 0;
                 queues[queues_len][q_tails[queues_len]] = nums[i];
-                q_tails[queues_len] = (1 + q_tails[queues_len]) % 100000;
+                q_tails[queues_len] = (1 + q_tails[queues_len]) % N_MAX;
                 queues_len += 1;
                 if (queues_len_max < queues_len) {
                     queues_len_max = queues_len;
