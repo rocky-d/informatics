9dc48e1b0004b71654e979606832ba852d9e0f57
Rocky Haotian Du <2712479005@qq.com>
2024-02-01 23:18:25 +0800

    Updated

---
+++ code/py/src/leetcode/done/p46.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(list(permutation) for permutation in permutations(nums))
