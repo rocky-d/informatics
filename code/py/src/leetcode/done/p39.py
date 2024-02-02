d589d3379ab9a0941cdf05b345aa4ac372321b6a
Rocky Haotian Du <2712479005@qq.com>
2024-02-02 20:32:07 +0800

    Updated

--- code/py/src/leetcode/done/p39.py
+++ code/py/src/leetcode/done/p39.py
@@ -3,18 +3,18 @@ from rockyutil.leetcode import *
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        combinations_set = set()
+        combinations = set()
 
-        def dfs(last: int, combinations: Dict[int, int]) -> None:
+        def dfs(last: int, combination: Dict[int, int]) -> None:
             if last < 0:
                 return
             if 0 == last:
-                combinations_set.add(tuple(combinations.items()))
+                combinations.add(tuple(combination.items()))
                 return
             for candidate in candidates:
-                combinations[candidate] += 1
-                dfs(last = last - candidate, combinations = combinations)
-                combinations[candidate] -= 1
+                combination[candidate] += 1
+                dfs(last = last - candidate, combination = combination)
+                combination[candidate] -= 1
 
-        dfs(last = target, combinations = {candidate: 0 for candidate in candidates})
-        return [reduce(lambda x, y: x + y, [[num for _ in range(cnt)] for num, cnt in combination]) for combination in combinations_set]
+        dfs(last = target, combination = {candidate: 0 for candidate in candidates})
+        return [reduce(lambda x, y: x + y, [[num for _ in range(cnt)] for num, cnt in combination]) for combination in combinations]
