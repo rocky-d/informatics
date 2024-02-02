24b7cec587c8f272838432c5e41ae33e6772af35
Rocky Haotian Du <2712479005@qq.com>
2024-02-02 17:22:19 +0800

    Updated

--- code/cpp/src/pta/done/ladder/p3_004.cpp
+++ code/cpp/src/pta/done/ladder/p3_004.cpp
@@ -9,14 +9,15 @@ const int M_MAX = 1 + 1286 + 1;
 const int N_MAX = 1 + 128 + 1;
 const int L_MAX = 1 + 60 + 1;
 
+const int OFFSETS[6][3] = {{-1, 0, 0}, {0, -1, 0}, {0, 0, -1}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
 int m, n, l, t;
 char slices[L_MAX][M_MAX][N_MAX];
 
 int ans, vol;
 bool seen[L_MAX][M_MAX][N_MAX];
-Coord coord, next_coord;
-queue<Coord> queue_;
-const int OFFSETS[6][3] = {{-1, 0, 0}, {0, -1, 0}, {0, 0, -1}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+Coord cod, cod_next;
+queue<Coord> que;
 
 int main() {
     cin >> m >> n >> l >> t;
@@ -32,27 +33,27 @@ int main() {
     ans = 0;
     memset(seen, false, sizeof(seen));
     for (int i = 1; i < 1 + l; i += 1) {
+        cod.x = i;
         for (int j = 1; j < 1 + m; j += 1) {
+            cod.y = j;
             for (int k = 1; k < 1 + n; k += 1) {
-                coord.x = i;
-                coord.y = j;
-                coord.z = k;
-                if ('1' == slices[coord.x][coord.y][coord.z]
-                        && false == seen[coord.x][coord.y][coord.z]) {
-                    queue_.push(coord);
-                    seen[coord.x][coord.y][coord.z] = true;
+                cod.z = k;
+                if ('1' == slices[cod.x][cod.y][cod.z]
+                        && false == seen[cod.x][cod.y][cod.z]) {
+                    que.push(cod);
+                    seen[cod.x][cod.y][cod.z] = true;
                     vol = 1;
-                    while (0 < queue_.size()) {
-                        coord = queue_.front();
-                        queue_.pop();
+                    while (0 < que.size()) {
+                        cod = que.front();
+                        que.pop();
                         for (int offset_i = 0; offset_i < 6; offset_i += 1) {
-                            next_coord.x = coord.x + OFFSETS[offset_i][0];
-                            next_coord.y = coord.y + OFFSETS[offset_i][1];
-                            next_coord.z = coord.z + OFFSETS[offset_i][2];
-                            if ('1' == slices[next_coord.x][next_coord.y][next_coord.z]
-                                    && false == seen[next_coord.x][next_coord.y][next_coord.z]) {
-                                queue_.push(next_coord);
-                                seen[next_coord.x][next_coord.y][next_coord.z] = true;
+                            cod_next.x = cod.x + OFFSETS[offset_i][0];
+                            cod_next.y = cod.y + OFFSETS[offset_i][1];
+                            cod_next.z = cod.z + OFFSETS[offset_i][2];
+                            if ('1' == slices[cod_next.x][cod_next.y][cod_next.z]
+                                    && false == seen[cod_next.x][cod_next.y][cod_next.z]) {
+                                que.push(cod_next);
+                                seen[cod_next.x][cod_next.y][cod_next.z] = true;
                                 vol += 1;
                             }
                         }
