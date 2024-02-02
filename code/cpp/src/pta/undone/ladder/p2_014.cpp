41f669f443dd55241e1b406c93abb3dd3cf2a174
Rocky Haotian Du <2712479005@qq.com>
2024-02-02 17:37:37 +0800

    Updated

--- code/cpp/src/pta/undone/ladder/p2_014.cpp
+++ code/cpp/src/pta/undone/ladder/p2_014.cpp
@@ -6,7 +6,7 @@ const int N_MAX = 100000;
 
 int n, nums[N_MAX];
 
-int q_heads[N_MAX], q_tails[N_MAX], queues[N_MAX][N_MAX], queues_len, queues_len_max, waiting;
+int que_heads[N_MAX], que_tails[N_MAX], ques[N_MAX][N_MAX], ques_len, ques_len_max, waiting;
 bool loopelse1, loopelse2;
 
 int main() {
@@ -15,8 +15,8 @@ int main() {
         cin >> nums[i];
     }
 
-    queues_len = 0;
-    queues_len_max = 0;
+    ques_len = 0;
+    ques_len_max = 0;
     waiting = n;
     for (int i = 0; i < n; i += 1) {
         if (waiting == nums[i]) {
@@ -24,17 +24,17 @@ int main() {
             loopelse1 = true;
             while (1 <= waiting) {
                 loopelse2 = true;
-                for (int j = 0; j < queues_len; j += 1) {
-                    if (waiting == queues[j][q_heads[j]]) {
-                        if (1 == (q_tails[j] - q_heads[j] + N_MAX) % N_MAX) {
-                            queues_len -= 1;
-                            for (int k = q_heads[queues_len]; k != q_tails[queues_len]; k = (1 + k) % N_MAX) {
-                                queues[j][k] = queues[queues_len][k];
+                for (int j = 0; j < ques_len; j += 1) {
+                    if (waiting == ques[j][que_heads[j]]) {
+                        if (1 == (que_tails[j] - que_heads[j] + N_MAX) % N_MAX) {
+                            ques_len -= 1;
+                            for (int k = que_heads[ques_len]; k != que_tails[ques_len]; k = (1 + k) % N_MAX) {
+                                ques[j][k] = ques[ques_len][k];
                             }
-                            q_heads[j] = q_heads[queues_len];
-                            q_tails[j] = q_tails[queues_len];
+                            que_heads[j] = que_heads[ques_len];
+                            que_tails[j] = que_tails[ques_len];
                         } else {
-                            q_heads[j] = (1 + q_heads[j]) % N_MAX;
+                            que_heads[j] = (1 + que_heads[j]) % N_MAX;
                         }
                         waiting -= 1;
                         loopelse2 = false;
@@ -49,25 +49,25 @@ int main() {
             }
         } else {
             loopelse1 = true;
-            for (int j = 0; j < queues_len; j += 1) {
-                if (nums[i] < queues[j][q_tails[j] - 1]) {
-                    queues[j][q_tails[j]] = nums[i];
-                    q_tails[j] = (1 + q_tails[j]) % N_MAX;
+            for (int j = 0; j < ques_len; j += 1) {
+                if (nums[i] < ques[j][que_tails[j] - 1]) {
+                    ques[j][que_tails[j]] = nums[i];
+                    que_tails[j] = (1 + que_tails[j]) % N_MAX;
                     loopelse1 = false;
                     break;
                 }
             } if (loopelse1) {
-                q_heads[queues_len] = 0;
-                q_tails[queues_len] = 0;
-                queues[queues_len][q_tails[queues_len]] = nums[i];
-                q_tails[queues_len] = (1 + q_tails[queues_len]) % N_MAX;
-                queues_len += 1;
-                if (queues_len_max < queues_len) {
-                    queues_len_max = queues_len;
+                que_heads[ques_len] = 0;
+                que_tails[ques_len] = 0;
+                ques[ques_len][que_tails[ques_len]] = nums[i];
+                que_tails[ques_len] = (1 + que_tails[ques_len]) % N_MAX;
+                ques_len += 1;
+                if (ques_len_max < ques_len) {
+                    ques_len_max = ques_len;
                 }
             }
         }
     }
-    cout << 1 + queues_len_max << endl;
+    cout << 1 + ques_len_max << endl;
     return 0;
 }
