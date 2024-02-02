29f31e799058428b64254391b3398d81d0296ee7
Rocky Haotian Du <2712479005@qq.com>
2024-02-02 17:47:15 +0800

    Updated

--- code/cpp/src/pta/done/ladder/p2_035.cpp
+++ code/cpp/src/pta/done/ladder/p2_035.cpp
@@ -4,22 +4,30 @@ using namespace std;
 
 const int N_MAX = 30;
 
-int n, tree[N_MAX];
+int n, postorder[N_MAX];
+
+int levelorder[N_MAX], p;
 
 void dfs(int index) {
     if (n <= index)
         return;
-    dfs(2 * index + 1);
-    dfs(2 * index + 2);
-    cin >> tree[index];
+    dfs(1 + 2 * index);
+    dfs(2 + 2 * index);
+    levelorder[index] = postorder[p];
+    p += 1;
 }
 
 int main() {
     cin >> n;
+    for (int i = 0; i < n; i += 1) {
+        cin >> postorder[i];
+    }
+
+    p = 0;
     dfs(0);
-    cout << tree[0];
+    cout << levelorder[0];
     for (int i = 1; i < n; i += 1) {
-        cout << ' ' << tree[i];
+        cout << ' ' << levelorder[i];
     }
     cout << endl;
     return 0;
