de6221e369b513683a4269595ce49627b05f214b
Rocky Haotian Du <2712479005@qq.com>
2024-02-02 17:09:49 +0800

    Updated

--- code/cpp/src/pta/undone/ladder/p3_004.cpp
+++ code/cpp/src/pta/undone/ladder/p3_004.cpp
@@ -1,89 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int M_MAX = 1286;
-const int N_MAX = 128;
-const int L_MAX = 60;
+typedef struct {
+    int x, y, z;
+} Coord;
+
+const int M_MAX = 1 + 1286 + 1;
+const int N_MAX = 1 + 128 + 1;
+const int L_MAX = 1 + 60 + 1;
 
 int m, n, l, t;
 char slices[L_MAX][M_MAX][N_MAX];
 
+int ans, vol;
+bool seen[L_MAX][M_MAX][N_MAX];
+Coord coord, next_coord;
+queue<Coord> queue_;
+const int OFFSETS[6][3] = {{-1, 0, 0}, {0, -1, 0}, {0, 0, -1}, {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
 int main() {
     cin >> m >> n >> l >> t;
-    for (int i = 0; i < l; i += 1) {
-        for (int j = 0; j < m; j += 1) {
-            for (int k = 0; k < n; k += 1) {
+    memset(slices, '0', sizeof(slices));
+    for (int i = 1; i < 1 + l; i += 1) {
+        for (int j = 1; j < 1 + m; j += 1) {
+            for (int k = 1; k < 1 + n; k += 1) {
                 cin >> slices[i][j][k];
             }
         }
     }
 
-    int ans;
     ans = 0;
-    bool seen[L_MAX][M_MAX][N_MAX];
-    for (int i = 0; i < l; i += 1) {
-        for (int j = 0; j < m; j += 1) {
-            for (int k = 0; k < n; k += 1) {
-                seen[i][j][k] = false;
-            }
-        }
-    }
-    for (int i = 0; i < l; i += 1) {
-        for (int j = 0; j < m; j += 1) {
-            for (int k = 0; k < n; k += 1) {
-                if ('1' == slices[i][j][k] && false == seen[i][j][k]) {
-                    int vol;
-                    vol = 0;
-                    int stack[100][3], stack_len;
-                    stack_len = 0;
-                    stack[stack_len][0] = i;
-                    stack[stack_len][1] = j;
-                    stack[stack_len][2] = k;
-                    stack_len += 1;
-                    while (0 < stack_len) {
-                        stack_len -= 1;
-                        int x, y, z;
-                        x = stack[stack_len][0];
-                        y = stack[stack_len][1];
-                        z = stack[stack_len][2];
-                        if (false == seen[x][y][z]) {
-                            vol += 1;
-                            seen[x][y][z] = true;
-                            if (0 <= x - 1 && '1' == slices[x - 1][y][z] && false == seen[x - 1][y][z]) {
-                                stack[stack_len][0] = x - 1;
-                                stack[stack_len][1] = y;
-                                stack[stack_len][2] = z;
-                                stack_len += 1;
-                            }
-                            if (0 <= y - 1 && '1' == slices[x][y - 1][z] && false == seen[x][y - 1][z]) {
-                                stack[stack_len][0] = x;
-                                stack[stack_len][1] = y - 1;
-                                stack[stack_len][2] = z;
-                                stack_len += 1;
-                            }
-                            if (0 <= z - 1 && '1' == slices[x][y][z - 1] && false == seen[x][y][z - 1]) {
-                                stack[stack_len][0] = x;
-                                stack[stack_len][1] = y;
-                                stack[stack_len][2] = z - 1;
-                                stack_len += 1;
-                            }
-                            if (x + 1 < l && '1' == slices[x + 1][y][z] && false == seen[x + 1][y][z]) {
-                                stack[stack_len][0] = x + 1;
-                                stack[stack_len][1] = y;
-                                stack[stack_len][2] = z;
-                                stack_len += 1;
-                            }
-                            if (y + 1 < m && '1' == slices[x][y + 1][z] && false == seen[x][y + 1][z]) {
-                                stack[stack_len][0] = x;
-                                stack[stack_len][1] = y + 1;
-                                stack[stack_len][2] = z;
-                                stack_len += 1;
-                            }
-                            if (z + 1 < n && '1' == slices[x][y][z + 1] && false == seen[x][y][z + 1]) {
-                                stack[stack_len][0] = x;
-                                stack[stack_len][1] = y;
-                                stack[stack_len][2] = z + 1;
-                                stack_len += 1;
+    memset(seen, false, sizeof(seen));
+    for (int i = 1; i < 1 + l; i += 1) {
+        for (int j = 1; j < 1 + m; j += 1) {
+            for (int k = 1; k < 1 + n; k += 1) {
+                coord.x = i;
+                coord.y = j;
+                coord.z = k;
+                if ('1' == slices[coord.x][coord.y][coord.z]
+                        && false == seen[coord.x][coord.y][coord.z]) {
+                    queue_.push(coord);
+                    seen[coord.x][coord.y][coord.z] = true;
+                    vol = 1;
+                    while (0 < queue_.size()) {
+                        coord = queue_.front();
+                        queue_.pop();
+                        for (int offset_i = 0; offset_i < 6; offset_i += 1) {
+                            next_coord.x = coord.x + OFFSETS[offset_i][0];
+                            next_coord.y = coord.y + OFFSETS[offset_i][1];
+                            next_coord.z = coord.z + OFFSETS[offset_i][2];
+                            if ('1' == slices[next_coord.x][next_coord.y][next_coord.z]
+                                    && false == seen[next_coord.x][next_coord.y][next_coord.z]) {
+                                queue_.push(next_coord);
+                                seen[next_coord.x][next_coord.y][next_coord.z] = true;
+                                vol += 1;
                             }
                         }
                     }
