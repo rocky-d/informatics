2446a00ea3786810c9340b1cf6570c1cf5ede779
Rocky Haotian Du <2712479005@qq.com>
2024-02-03 11:46:31 +0800

    Updated

--- code/py/src/leetcode/done/p1690.py
+++ code/py/src/leetcode/done/p1690.py
@@ -6,14 +6,14 @@ class Solution:
         pre = [0]
         for stone in stones:
             pre.append(pre[-1] + stone)
+        dp = [0 for _ in range(1 + len(stones))]
+        for i in range(1, 1 + len(stones)):
+            dp_last, dp = dp, []
+            for j in range(1 + len(stones) - i):
+                dp.append(max(pre[j + i - 1] - pre[j] - dp_last[j],
+                              pre[j + i] - pre[j + 1] - dp_last[j + 1]))
+        return dp[0]
 
-        @cache
-        def dfs(a, b):
-            if a == b:
-                return 0
-            return max(pre[b - 1] - pre[a] - dfs(a = a, b = b - 1),
-                       pre[b] - pre[a + 1] - dfs(a = a + 1, b = b))
 
-        ans = dfs(a = 0, b = len(stones))
-        dfs.cache_clear()
-        return ans
+eg_stones = [5, 3, 1, 4, 2]
+print(Solution().stoneGameVII(eg_stones))
