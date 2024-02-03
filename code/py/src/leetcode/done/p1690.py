78d14e6081afab2bef7a7fa30f2523d448872a11
Rocky Haotian Du <2712479005@qq.com>
2024-02-03 11:47:27 +0800

    Updated

--- code/py/src/leetcode/done/p1690.py
+++ code/py/src/leetcode/done/p1690.py
@@ -3,15 +3,15 @@ from rockyutil.leetcode import *
 
 class Solution:
     def stoneGameVII(self, stones: List[int]) -> int:
-        pre = [0]
+        pres = [0]
         for stone in stones:
-            pre.append(pre[-1] + stone)
+            pres.append(pres[-1] + stone)
         dp = [0 for _ in range(1 + len(stones))]
         for i in range(1, 1 + len(stones)):
             dp_last, dp = dp, []
             for j in range(1 + len(stones) - i):
-                dp.append(max(pre[j + i - 1] - pre[j] - dp_last[j],
-                              pre[j + i] - pre[j + 1] - dp_last[j + 1]))
+                dp.append(max(pres[j + i - 1] - pres[j] - dp_last[j],
+                              pres[j + i] - pres[j + 1] - dp_last[j + 1]))
         return dp[0]
 
 
