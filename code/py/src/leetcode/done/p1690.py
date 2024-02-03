855b970b383018b6c2f59aa709b9349fbe9d3297
Rocky Haotian Du <2712479005@qq.com>
2024-02-03 11:48:59 +0800

    Updated

--- code/py/src/leetcode/done/p1690.py
+++ code/py/src/leetcode/done/p1690.py
@@ -6,8 +6,8 @@ class Solution:
         pres = [0]
         for stone in stones:
             pres.append(pres[-1] + stone)
-        dp = [0 for _ in range(1 + len(stones))]
-        for i in range(1, 1 + len(stones)):
+        dp = [0 for _ in range(len(stones))]
+        for i in range(2, 1 + len(stones)):
             dp_last, dp = dp, []
             for j in range(1 + len(stones) - i):
                 dp.append(max(pres[j + i - 1] - pres[j] - dp_last[j],
