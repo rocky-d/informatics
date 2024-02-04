0bd9308c688d95d45c59e23c2529df2074dd7f33
Rocky Haotian Du <2712479005@qq.com>
2024-02-04 20:16:34 +0800

    Updated

--- code/py/src/pta/undone/ladder/p3_004.py
+++ code/py/src/pta/undone/ladder/p3_004.py
@@ -13,24 +13,24 @@ def main() -> None:
             for k in range(n):
                 if '1' == slices[i][j][k] and (i, j, k) not in seen:
                     vol = 0
-                    queue = [(i, j, k)]
-                    while 0 < len(queue):
-                        x, y, z = queue.pop(0)
+                    que = [(i, j, k)]
+                    while 0 < len(que):
+                        x, y, z = que.pop(0)
                         if (x, y, z) not in seen:
                             vol += 1
                             seen.add((x, y, z))
                             if 0 <= x - 1 and '1' == slices[x - 1][y][z] and (x - 1, y, z) not in seen:
-                                queue.append((x - 1, y, z))
+                                que.append((x - 1, y, z))
                             if 0 <= y - 1 and '1' == slices[x][y - 1][z] and (x, y - 1, z) not in seen:
-                                queue.append((x, y - 1, z))
+                                que.append((x, y - 1, z))
                             if 0 <= z - 1 and '1' == slices[x][y][z - 1] and (x, y, z - 1) not in seen:
-                                queue.append((x, y, z - 1))
+                                que.append((x, y, z - 1))
                             if x + 1 < l and '1' == slices[x + 1][y][z] and (x + 1, y, z) not in seen:
-                                queue.append((x + 1, y, z))
+                                que.append((x + 1, y, z))
                             if y + 1 < m and '1' == slices[x][y + 1][z] and (x, y + 1, z) not in seen:
-                                queue.append((x, y + 1, z))
+                                que.append((x, y + 1, z))
                             if z + 1 < n and '1' == slices[x][y][z + 1] and (x, y, z + 1) not in seen:
-                                queue.append((x, y, z + 1))
+                                que.append((x, y, z + 1))
                     if t <= vol:
                         ans += vol
     print(ans)
