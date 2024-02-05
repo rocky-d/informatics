51162c85c92cd40a597ad5c46b9346010e2b78ba
Rocky Haotian Du <2712479005@qq.com>
2024-02-05 18:40:56 +0800

    Updated

--- code/py/src/leetcode/done/p1696.py
+++ code/py/src/leetcode/done/p1696.py
@@ -4,14 +4,14 @@ from rockyutil.leetcode import *
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         dp = deque([nums[0]])
-        que = deque([0])
+        que_dec = deque([0])
         for i in range(1, len(nums)):
-            if que[0] < i - k:
-                que.popleft()
-            dp.append(dp[que[0]] + nums[i])
-            while 0 < len(que) and dp[que[-1]] <= dp[-1]:
-                que.pop()
-            que.append(i)
+            if que_dec[0] < i - k:
+                que_dec.popleft()
+            dp.append(dp[que_dec[0]] + nums[i])
+            while 0 < len(que_dec) and dp[que_dec[-1]] <= dp[-1]:
+                que_dec.pop()
+            que_dec.append(i)
         return dp[-1]
 
 
