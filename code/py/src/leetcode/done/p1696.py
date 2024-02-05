c0558920123f8e93597f12e429eb5a59cba64c62
Rocky Haotian Du <2712479005@qq.com>
2024-02-05 10:47:34 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1696.py
@@ -0,0 +1,20 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        dp = deque([nums[0]])
+        que = deque([0])
+        for i in range(1, len(nums)):
+            while que[0] < i - k:
+                que.popleft()
+            dp.append(dp[que[0]] + nums[i])
+            while 0 < len(que) and dp[que[-1]] <= dp[-1]:
+                que.pop()
+            que.append(i)
+        return dp[-1]
+
+
+eg_nums = [10, -5, -2, 4, 0, 3]
+eg_k = 3
+print(Solution().maxResult(eg_nums, eg_k))
