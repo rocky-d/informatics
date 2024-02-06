f1b132140eb44decd4b81bebfc12ef9dfd9e48f8
Rocky Haotian Du <2712479005@qq.com>
2024-02-06 17:00:34 +0800

    Updated

---
+++ code/py/src/leetcode/done/p45.py
@@ -0,0 +1,11 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [i for i in range(n)]
+        for i in range(n - 1):
+            for j in range(i + 1, i + 1 + min(n - i - 1, nums[i])):
+                dp[j] = min(dp[j], 1 + dp[i])
+        return dp[-1]
