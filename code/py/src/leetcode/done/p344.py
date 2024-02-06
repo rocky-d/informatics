c0059169e147553ec9d020a8fd5f21d3935f20f5
Rocky Haotian Du <2712479005@qq.com>
2024-02-06 11:25:27 +0800

    Updated

--- code/py/src/leetcode/done/p344.py
+++ code/py/src/leetcode/done/p344.py
@@ -5,3 +5,8 @@ class Solution:
     def reverseString(self, s: List[str]) -> None:
         for i in range(len(s) // 2):
             s[i], s[-1 - i] = s[-1 - i], s[i]
+
+
+eg_s = ['h', 'e', 'l', 'l', 'o']
+Solution().reverseString(eg_s)
+print(eg_s)
