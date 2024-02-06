63b8341bd33f4e94eebb8893193e8db9f5d79962
Rocky Haotian Du <2712479005@qq.com>
2024-02-06 11:13:27 +0800

    Updated

---
+++ code/py/src/leetcode/done/lcp30.py
@@ -0,0 +1,24 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def magicTower(self, nums: List[int]) -> int:
+        ans = 0
+        hp = 1
+        heap_min = []
+        nums_last = 0
+        for num in nums:
+            hp += num
+            if num < 0:
+                if 0 < hp:
+                    heappush(heap_min, num)
+                else:
+                    ans += 1
+                    val = heappushpop(heap_min, num)
+                    hp -= val
+                    nums_last += val
+        return ans if 0 < hp + nums_last else -1
+
+
+eg_nums = [100, 100, 100, -250, -60, -140, -50, -50, 100, 150]
+print(Solution().magicTower(eg_nums))
