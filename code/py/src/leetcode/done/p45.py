85a3fefbced7d98ec9a65713b51f5208b4902378
Rocky Haotian Du <2712479005@qq.com>
2024-02-06 17:03:39 +0800

    Updated

--- code/py/src/leetcode/done/p45.py
+++ code/py/src/leetcode/done/p45.py
@@ -6,6 +6,6 @@ class Solution:
         n = len(nums)
         dp = [i for i in range(n)]
         for i in range(n - 1):
-            for j in range(i + 1, i + 1 + min(n - i - 1, nums[i])):
+            for j in range(i + 1, min(n, 1 + i + nums[i])):
                 dp[j] = min(dp[j], 1 + dp[i])
         return dp[-1]
