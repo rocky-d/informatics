805c6ace9650f1d3c008b0f509b554f690854a4b
Rocky Haotian Du <2712479005@qq.com>
2024-02-07 11:37:55 +0800

    Updated

---
+++ code/py/src/leetcode/undone/p2641.py
@@ -0,0 +1,31 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs1(root: Optional[TreeNode], depth: int):
+            if root is None:
+                return
+            if len(s) <= depth:
+                s.append(0)
+            s[depth] += root.val
+            dfs1(root.left, depth + 1)
+            dfs1(root.right, depth + 1)
+
+        def dfs2(root: Optional[TreeNode], depth: int):
+            sub = (root.left.val if root.left else 0) + (
+                root.right.val if root.right else 0
+            )
+            depth += 1
+            if root.left:
+                root.left.val = s[depth] - sub
+                dfs2(root.left, depth)
+            if root.right:
+                root.right.val = s[depth] - sub
+                dfs2(root.right, depth)
+
+        s = []
+        dfs1(root, 0)
+        root.val = 0
+        dfs2(root, 0)
+        return root
