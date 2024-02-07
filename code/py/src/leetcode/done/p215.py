c2dee2b73c80aaeee57b5f947adccc7162103d6d
Rocky Haotian Du <2712479005@qq.com>
2024-02-07 11:58:49 +0800

    Updated

--- code/py/src/leetcode/done/p215.py
+++ code/py/src/leetcode/done/p215.py
@@ -3,4 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums, reverse = True)[k - 1]
+        return sorted(nums)[-k]
