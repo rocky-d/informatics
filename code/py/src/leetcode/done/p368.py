c068cec54d5940be450bd0fa5e92f4c13f10f604
Rocky Haotian Du <2712479005@qq.com>
2024-02-09 19:00:30 +0800

    Updated

--- code/py/src/leetcode/done/p368.py
+++ code/py/src/leetcode/done/p368.py
@@ -4,11 +4,11 @@ from rockyutil.leetcode import *
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         ans = []
-        dp = [(1, None)]
+        dp = deque(((1, -1),))
         start = 1, 0
         nums.sort()
         for i in range(1, len(nums)):
-            dp.append((1, None))
+            dp.append((1, -1))
             for j in range(i):
                 if 0 == nums[i] % nums[j]:
                     if 1 + dp[j][0] > dp[-1][0]:
@@ -16,7 +16,7 @@ class Solution:
             if start[0] < dp[-1][0]:
                 start = dp[-1][0], i
         index = start[1]
-        while index is not None:
+        while -1 != index:
             ans.append(nums[index])
             index = dp[index][1]
         return ans
