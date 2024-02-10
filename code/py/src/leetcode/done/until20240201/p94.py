a47a03991391f6a0cdc144f964b670163725234a
Rocky Haotian Du <2712479005@qq.com>
2024-02-10 09:52:17 +0800

    Updated

--- code/py/src/leetcode/done/until20240201/p94.py
+++ code/py/src/leetcode/done/until20240201/p94.py
@@ -3,8 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            res = []
-        else:
-            res = self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
-        return res
+        return [] if root is None else self.inorderTraversal(root = root.left) + [root.val] + self.inorderTraversal(root = root.right)
