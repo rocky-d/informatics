ff11093f1aee187f292de8620e341a06a9ed757b
Rocky Haotian Du <2712479005@qq.com>
2024-02-10 13:21:48 +0800

    Updated

--- code/py/src/pta/undone/p2_001.py
+++ code/py/src/pta/undone/p2_001.py
@@ -12,18 +12,23 @@ def main() -> None:
     for city1, city2, length in freeways:
         graph[city1][city2] = graph[city2][city1] = length
     dists = [[float('inf'), 0, ..., 0] for _ in range(n)]
-    dists[s][0], dists[s][1], dists[s][2], dists[s][3] = 0, nums[s], None, 1
+    dists[s] = [0, nums[s], None, 1]
     unseen = set(i for i in range(n))
     for _ in range(n):
         city = min(unseen, key = lambda i: (dists[i][0], -dists[i][1]))
         unseen.remove(city)
         for nxt in range(n):
             if dists[city][0] + graph[city][nxt] < dists[nxt][0]:
-                dists[nxt][0], dists[nxt][1], dists[nxt][2], dists[nxt][3] = dists[city][0] + graph[city][nxt], dists[city][1] + nums[nxt], city, dists[city][3]
+                dists[nxt][0] = dists[city][0] + graph[city][nxt]
+                dists[nxt][1] = dists[city][1] + nums[nxt]
+                dists[nxt][2] = city
+                dists[nxt][3] = dists[city][3]
             elif dists[city][0] + graph[city][nxt] == dists[nxt][0]:
                 dists[nxt][3] += dists[city][3]
                 if dists[nxt][1] < dists[city][1] + nums[nxt]:
-                    dists[nxt][0], dists[nxt][1], dists[nxt][2] = dists[city][0] + graph[city][nxt], dists[city][1] + nums[nxt], city
+                    dists[nxt][0] = dists[city][0] + graph[city][nxt]
+                    dists[nxt][1] = dists[city][1] + nums[nxt]
+                    dists[nxt][2] = city
     route = deque(maxlen = n)
     city = d
     while city is not None:
