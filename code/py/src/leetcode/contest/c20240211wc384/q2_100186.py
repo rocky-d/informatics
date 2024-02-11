e5ed1dc0273f2adae74124ab95c1e1b7c5b0812f
Rocky Haotian Du <2712479005@qq.com>
2024-02-11 13:38:41 +0800

    Updated

--- code/py/src/leetcode/contest/c20240211wc384/q2_100186.py
+++ code/py/src/leetcode/contest/c20240211wc384/q2_100186.py
@@ -5,12 +5,9 @@ class Solution:
     def countMatchingSubarrays(self, nums: List[int], pattern: List[int]) -> int:
         m, n = len(pattern), len(nums)
         dp = [True for _ in range(n)]
-        f1 = lambda a, b: a < b
-        f2 = lambda a, b: a == b
-        f3 = lambda a, b: a > b
         for i in range(m):
             dp_last, dp = dp, []
-            f = f1 if 1 == pattern[i] else f2 if 0 == pattern[i] else f3
+            f = (lambda a, b: a < b) if 1 == pattern[i] else (lambda a, b: a == b) if 0 == pattern[i] else (lambda a, b: a > b)
             for j in range(n - i - 1):
                 dp.append(True if dp_last[j] and f(nums[j + i], nums[j + i + 1]) else False)
         return dp.count(True)
