e600717ef1e0c4a98facc34369ae26e32d67def3
Rocky Haotian Du <2712479005@qq.com>
2024-02-11 14:18:47 +0800

    Updated

--- code/py/src/leetcode/done/p29.py
+++ code/py/src/leetcode/done/p29.py
@@ -1,3 +1,3 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        return min(2 ** 31 - 1, max(-2 * 31, int(dividend / divisor)))
+        return min(2_147_483_647, max(-2_147_483_648, int(dividend / divisor)))
