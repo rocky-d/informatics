46cec89593318634a4f7d7434645cff88a3c11c3
Rocky Haotian Du <2712479005@qq.com>
2024-02-11 16:58:48 +0800

    Updated

--- code/py/src/leetcode/contest/c20240211wc384/q4_100198.py
+++ code/py/src/leetcode/contest/c20240211wc384/q4_100198.py
@@ -3,23 +3,13 @@ from rockyutil.leetcode import *
 
 class Solution:
     def countMatchingSubarrays(self, nums: List[int], pattern: List[int]) -> int:
-        ans = 0
-        m, n = len(pattern), len(nums)
-        nums_s = ''
-        for i in range(1, n):
-            if nums[i - 1] < nums[i]:
-                nums_s += '3'
-            elif nums[i - 1] == nums[i]:
-                nums_s += '2'
-            else:
-                nums_s += '1'
-        pattern_s = ''
-        for p in pattern:
-            pattern_s += str(p + 2)
-        for i in range(1 + n - m):
-            if nums_s[i:].startswith(pattern_s):
-                ans += 1
-        return ans
+        target = ''
+        for i in range(len(nums) - 1):
+            target += '2' if nums[i] < nums[i + 1] else '1' if nums[i] == nums[i + 1] else '0'
+        patt = ''
+        for num in pattern:
+            patt += str(1 + num)
+        return len(findall(f"(?={patt})", target))
 
 
 eg_nums = [1, 2, 3, 4, 5, 6]
