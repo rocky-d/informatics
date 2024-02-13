b72a352b67f27bded5c0fd2371697239271cc76e
Rocky Haotian Du <2712479005@qq.com>
2024-02-13 14:39:03 +0800

    Updated

--- code/py/src/leetcode/done/p2108.py
+++ code/py/src/leetcode/done/p2108.py
@@ -3,10 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-        for word in words:
-            if word == word[::-1]:
-                ans = word
-                break
-        else:
-            ans = ''
-        return ans
+        return next(word for word in words + [''] if word == word[::-1])
