21911bab07930b20f00f84fcd006f9fda2793c68
Rocky Haotian Du <2712479005@qq.com>
2024-02-15 17:11:57 +0800

    Updated

--- code/py/src/leetcode/undone/until20240201/p354.py
+++ code/py/src/leetcode/undone/until20240201/p354.py
@@ -4,16 +4,15 @@ from rockyutil.leetcode import *
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key = lambda envelope: (envelope[0], -envelope[1]))
-        dp = []
-        for i, envelopes_i in enumerate(envelopes):
-            dp.append(1)
-            for j, envelopes_j in enumerate(envelopes[:i]):
-                if envelopes_j[1] < envelopes_i[1]:
-                    dp[-1] = max(dp[-1], dp[j] + 1)
-        return max(dp)
+        dp = [envelopes[0][1]]
+        for i in range(1, len(envelopes)):
+            height = envelopes[i][1]
+            if dp[-1] < height:  # if 0 == len(dp) or dp[-1] < height:
+                dp.append(height)
+            elif 1 == len(dp) or dp[-2] < height:
+                dp[-1] = height
+        return len(dp)
 
-
-sol = Solution()
 
 eg_envelopes = [[5, 4], [6, 4], [6, 7], [2, 3]]
-print(sol.maxEnvelopes(envelopes = eg_envelopes))
+print(Solution().maxEnvelopes(eg_envelopes))
