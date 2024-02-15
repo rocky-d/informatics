7ab9037055e7b13058a04db11c78b37b04473ea7
Rocky Haotian Du <2712479005@qq.com>
2024-02-15 20:10:30 +0800

    Updated

--- code/py/src/dotcpp/done/p3181.py
+++ code/py/src/dotcpp/done/p3181.py
@@ -5,7 +5,7 @@ def main() -> None:
     s = input()
 
     dp = deque((0, 0, 0), maxlen = 3)
-    ofst = ord('a') + 1
+    ofst = ord('a') - 1
     for char in s:
         dp.append(max(dp[-1], max(dp[-2], dp[-3]) + ord(char) - ofst))
     print(dp[-1])
