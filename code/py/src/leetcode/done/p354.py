3679cb783fb82fdbee30940adbb50c4d2649c012
Rocky Haotian Du <2712479005@qq.com>
2024-02-15 17:18:44 +0800

    Updated

--- code/py/src/leetcode/done/p354.py
+++ code/py/src/leetcode/done/p354.py
@@ -6,11 +6,11 @@ class Solution:
         envelopes.sort(key = lambda envelope: (envelope[0], -envelope[1]))
         dp = [envelopes[0][1]]
         for i in range(1, len(envelopes)):
-            height = envelopes[i][1]
-            if dp[-1] < height:  # if 0 == len(dp) or dp[-1] < height:
-                dp.append(height)
-            elif 1 == len(dp) or dp[-2] < height:
-                dp[-1] = height
+            ht = envelopes[i][1]
+            if dp[-1] < ht:
+                dp.append(ht)
+            else:
+                dp[bisect_left(dp, ht)] = ht
         return len(dp)
 
 
