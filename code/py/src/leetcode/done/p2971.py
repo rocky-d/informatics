dcc57ba1383fa8a4e7746bc0ed6bf1045b1c241f
Rocky Haotian Du <2712479005@qq.com>
2024-02-15 10:56:23 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2971.py
@@ -0,0 +1,16 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        pres = [0]
+        for num in nums:
+            pres.append(pres[-1] + num)
+        for i in range(len(nums) - 1, 1, -1):
+            if pres[i] > nums[i]:
+                ans = pres[1 + i]
+                break
+        else:
+            ans = -1
+        return ans
