5967e1807647038802bae84c13010bbbdaa399ed
Rocky Haotian Du <2712479005@qq.com>
2024-02-16 13:48:47 +0800

    Updated

--- code/py/src/leetcode/done/p1481.py
+++ code/py/src/leetcode/done/p1481.py
@@ -5,7 +5,7 @@ class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         arr_cnter = sorted(Counter(arr).items(), key = lambda item: item[1])
         ans = len(arr_cnter)
-        for num, cnt in arr_cnter:
+        for _, cnt in arr_cnter:
             if k < cnt:
                 break
             else:
