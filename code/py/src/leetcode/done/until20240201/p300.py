05593c4ea402cb36fb43df30c4fafb5a07885f93
Rocky Haotian Du <2712479005@qq.com>
2024-02-16 17:37:19 +0800

    Updated

--- code/py/src/leetcode/done/until20240201/p300.py
+++ code/py/src/leetcode/done/until20240201/p300.py
@@ -5,16 +5,14 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         ans = 0
         dp = []
-        for i, nums_i in enumerate(nums):
+        for i in range(len(nums)):
             dp.append(1)
-            for j, nums_j in enumerate(nums[:i]):
-                if nums_j < nums_i:
+            for j in range(i):
+                if nums[j] < nums[i]:
                     dp[-1] = max(dp[-1], dp[j] + 1)
             ans = max(ans, dp[-1])
         return ans
 
 
-sol = Solution()
-
 eg_nums = [4, 10, 4, 3, 8, 9]
-print(sol.lengthOfLIS(nums = eg_nums))
+print(Solution().lengthOfLIS(eg_nums))
