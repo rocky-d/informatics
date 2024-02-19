5e0759e29a2bf389a4f29cfd2e88baefe6d80693
Rocky Haotian Du <2712479005@qq.com>
2024-02-19 14:12:07 +0800

    Updated

--- code/py/src/leetcode/done/concurrency/p1226.py
+++ code/py/src/leetcode/done/concurrency/p1226.py
@@ -6,7 +6,15 @@ class DiningPhilosophers:
         self.dining = threading.Semaphore(4)
         self.forks = [threading.Semaphore(1) for _ in range(5)]
 
-    def wantsToEat(self, philosopher: int, pickLeftFork: Callable[[], None], pickRightFork: Callable[[], None], eat: Callable[[], None], putLeftFork: Callable[[], None], putRightFork: Callable[[], None]) -> None:
+    def wantsToEat(
+            self,
+            philosopher: int,
+            pickLeftFork: Callable[[], None],
+            pickRightFork: Callable[[], None],
+            eat: Callable[[], None],
+            putLeftFork: Callable[[], None],
+            putRightFork: Callable[[], None],
+    ) -> None:
         left_fork, right_fork = philosopher % 5, (philosopher + 1) % 5
         self.dining.acquire()
         self.forks[left_fork].acquire()
