1fe7b394f96697298770fcf159010d80e5a7a0d4
Rocky Haotian Du <2712479005@qq.com>
2024-02-19 21:46:40 +0800

    Updated

--- code/py/src/leetcode/done/p901.py
+++ code/py/src/leetcode/done/p901.py
@@ -5,11 +5,11 @@ class StockSpanner:
     __slots__ = 'stk_dec'
 
     def __init__(self) -> None:
-        self.stk_dec: Deque[Tuple[int, int]] = deque()
+        self.stk_dec: Deque[Tuple[int, int]] = deque(((100_001, 0),))
 
     def next(self, price: int) -> int:
         cnt = 1
-        while 0 < len(self.stk_dec) and self.stk_dec[-1][0] <= price:
+        while self.stk_dec[-1][0] <= price:
             cnt += self.stk_dec.pop()[1]
         self.stk_dec.append((price, cnt))
         return cnt
