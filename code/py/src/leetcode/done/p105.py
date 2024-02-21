2674d33ef98c42361ad801c323b04734324fed79
Rocky Haotian Du <2712479005@qq.com>
2024-02-22 00:16:14 +0800

    Updated

--- code/py/src/leetcode/done/p105.py
+++ code/py/src/leetcode/done/p105.py
@@ -11,8 +11,8 @@ class Solution:
             in_root = inorder_idxes[preorder[pre_lft]]
             return TreeNode(
                 val = inorder[in_root],
-                left = build(pre_lft = pre_lft + 1, pre_rit = pre_lft + in_root - in_lft, in_lft = in_lft, in_rit = in_root - 1),
-                right = build(pre_lft = pre_lft + in_root - in_lft + 1, pre_rit = pre_rit, in_lft = in_root + 1, in_rit = in_rit),
+                left = build(pre_lft = pre_lft + 1, pre_rit = pre_lft + (in_root - in_lft), in_lft = in_lft, in_rit = in_root - 1),
+                right = build(pre_lft = pre_lft + (in_root - in_lft) + 1, pre_rit = pre_rit, in_lft = in_root + 1, in_rit = in_rit),
             )
 
         return build(pre_lft = 0, pre_rit = len(preorder) - 1, in_lft = 0, in_rit = len(inorder) - 1)
--- code/py/src/leetcode/undone/p106.py
+++ code/py/src/leetcode/undone/p106.py
@@ -3,16 +3,16 @@ from rockyutil.leetcode import *
 
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        idxes = {val: idx for idx, val in enumerate(inorder)}
+        inorder_idxes = {val: idx for idx, val in enumerate(inorder)}
 
-        def func(in_left, in_right):
-            if in_left > in_right:
+        def build(post_lft: int, post_rit: int, in_lft: int, in_rit: int) -> Optional[TreeNode]:
+            if post_rit < post_lft or in_rit < in_lft:
                 return None
-            val = postorder.pop()
-            root = TreeNode(val)
-            index = idxes[val]
-            root.right = func(index + 1, in_right)
-            root.left = func(in_left, index - 1)
-            return root
+            in_root = inorder_idxes[postorder[post_lft]]
+            return TreeNode(
+                val = inorder[in_root],
+                left = build(post_lft = post_lft, post_rit = post_rit - (in_rit - in_root) - 1, in_lft = in_lft, in_rit = in_root - 1),
+                right = build(post_lft = post_rit - (in_rit - in_root), post_rit = post_rit - 1, in_lft = in_root + 1, in_rit = in_rit),
+            )
 
-        return func(0, len(inorder) - 1)
+        return build(post_lft = 0, post_rit = len(postorder) - 1, in_lft = 0, in_rit = len(inorder) - 1)
