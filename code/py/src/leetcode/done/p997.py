5cf31a83fb42b8864b7f01a5bdb755cfbb753884
Rocky Haotian Du <2712479005@qq.com>
2024-02-22 19:39:36 +0800

    Updated

--- code/py/src/leetcode/done/p997.py
+++ code/py/src/leetcode/done/p997.py
@@ -3,8 +3,15 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trusts, trusted = {i: [] for i in range(1, 1 + n)}, {i: [] for i in range(1, 1 + n)}
+        trusting = {i: 0 for i in range(1, 1 + n)}
+        trusted = {i: 0 for i in range(1, 1 + n)}
         for a, b in trust:
-            trusts[a].append(b), trusted[b].append(a)
-        judges = tuple(i for i in range(1, 1 + n) if 0 == len(trusts[i]) and n - 1 == len(trusted[i]))
-        return judges[0] if 1 == len(judges) else -1
+            trusting[a] += 1
+            trusted[b] += 1
+        for i in range(1, 1 + n):
+            if 0 == trusting[i] and n - 1 == trusted[i]:
+                ans = i
+                break
+        else:
+            ans = -1
+        return ans
