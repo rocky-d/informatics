3e8d06a5fd071bd14b09c0e7bcd304fbcdae219e
Rocky Haotian Du <2712479005@qq.com>
2024-02-22 15:57:09 +0800

    Updated

--- code/py/src/pta/undone/ladder/p2_001.py
+++ code/py/src/pta/undone/ladder/p2_001.py
@@ -12,40 +12,40 @@ def main() -> None:
     graph = [{} for _ in range(n)]
     for city1, city2, length in freeways:
         graph[city1][city2] = graph[city2][city1] = length
-    table = [[float('inf'), 0, ..., 0] for _ in range(n)]
-    table[s] = [0, nums[s], None, 1]
+    dists = [[float('inf'), 0, ..., 0] for _ in range(n)]
+    dists[s] = [0, nums[s], None, 1]
     seen = {s}
     heap_min = [(0, -nums[s], None, s)]
     for _ in range(n):
-        table_city_0, _table_city_1, table_city_2, city = heappop(heap_min)
-        while table[city][2] != table_city_2:
-            table_city_0, _table_city_1, table_city_2, city = heappushpop(heap_min, (table[city][0], -table[city][1], table[city][2], city))
+        dists_city_0, _dists_city_1, dists_city_2, city = heappop(heap_min)
+        while dists[city][2] != dists_city_2:
+            dists_city_0, _dists_city_1, dists_city_2, city = heappushpop(heap_min, (dists[city][0], -dists[city][1], dists[city][2], city))
         for nb, length in graph[city].items():
-            table_city_0__length = table_city_0 + length
-            table_city_1__nums_nb = -_table_city_1 + nums[nb]
-            if table_city_0__length < table[nb][0]:
-                table[nb][0] = table_city_0__length
-                table[nb][1] = table_city_1__nums_nb
-                table[nb][2] = city
-                table[nb][3] = table[city][3]
+            dists_city_0__length = dists_city_0 + length
+            dists_city_1__nums_nb = -_dists_city_1 + nums[nb]
+            if dists_city_0__length < dists[nb][0]:
+                dists[nb][0] = dists_city_0__length
+                dists[nb][1] = dists_city_1__nums_nb
+                dists[nb][2] = city
+                dists[nb][3] = dists[city][3]
                 if nb not in seen:
                     seen.add(nb)
-                    heappush(heap_min, (table[nb][0], -table[nb][1], table[nb][2], nb))
-            elif table_city_0__length == table[nb][0]:
-                table[nb][3] += table[city][3]
-                if table[nb][1] < table_city_1__nums_nb:
-                    table[nb][0] = table_city_0__length
-                    table[nb][1] = table_city_1__nums_nb
-                    table[nb][2] = city
+                    heappush(heap_min, (dists[nb][0], -dists[nb][1], dists[nb][2], nb))
+            elif dists_city_0__length == dists[nb][0]:
+                dists[nb][3] += dists[city][3]
+                if dists[nb][1] < dists_city_1__nums_nb:
+                    dists[nb][0] = dists_city_0__length
+                    dists[nb][1] = dists_city_1__nums_nb
+                    dists[nb][2] = city
                     if nb not in seen:
                         seen.add(nb)
-                        heappush(heap_min, (table[nb][0], -table[nb][1], table[nb][2], nb))
+                        heappush(heap_min, (dists[nb][0], -dists[nb][1], dists[nb][2], nb))
     ans_path = deque(maxlen = n)
     city = d
     while city is not None:
         ans_path.appendleft(city)
-        city = table[city][2]
-    print(table[d][3], table[d][1])
+        city = dists[city][2]
+    print(dists[d][3], dists[d][1])
     print(*ans_path)
 
 
