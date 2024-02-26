a820c99a6858748d09dcce75642aa5d512a2c50e
Rocky Haotian Du <2712479005@qq.com>
2024-02-26 10:52:05 +0800

    Updated

--- code/py/src/leetcode/done/until20240201/p938.py
+++ code/py/src/leetcode/done/until20240201/p938.py
@@ -3,13 +3,12 @@ from rockyutil.leetcode import *
 
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        node = root
-        if node is None:
-            res = 0
-        elif node.val < low:
-            res = self.rangeSumBST(node.right, low, high)
-        elif high < node.val:
-            res = self.rangeSumBST(node.left, low, high)
+        if root is None:
+            return 0
+        if root.val < low:
+            res = self.rangeSumBST(root = root.right, low = low, high = high)
+        elif high < root.val:
+            res = self.rangeSumBST(root = root.left, low = low, high = high)
         else:
-            res = node.val + self.rangeSumBST(node.left, low, high) + self.rangeSumBST(node.right, low, high)
+            res = root.val + self.rangeSumBST(root = root.left, low = low, high = high) + self.rangeSumBST(root = root.right, low = low, high = high)
         return res
