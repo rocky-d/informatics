78a5c4adcc79e4fa50dbd9018c9dbf48d0aa67c8
Rocky Haotian Du <2712479005@qq.com>
2024-02-26 11:07:03 +0800

    Updated

---
+++ code/py/src/leetcode/done/p100.py
@@ -0,0 +1,8 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None or q is None:
+            return p is None and q is None
+        return p.val == q.val and self.isSameTree(p = p.left, q = q.left) and self.isSameTree(p = p.right, q = q.right)
