3b11b1382d22821f70271f3da284c3d5972458ea
Rocky Haotian Du <2712479005@qq.com>
2024-02-27 21:39:13 +0800

    Updated

--- code/py/src/leetcode/undone/until20240201/p2397.py
+++ code/py/src/leetcode/undone/until20240201/p2397.py
@@ -3,24 +3,24 @@ from rockyutil.leetcode import *
 
 class Solution:
     def maximumRows(self, matrix: List[List[int]], numSelect: int) -> int:
+        ans = 0
         m, n = len(matrix), len(matrix[0])
 
         def dfs(depth: int, free_cols: list[int]):
+            nonlocal ans
             if 0 == depth:
                 rows_having_1_free_cols = set()
-                for c in free_cols:
-                    for r in range(m):
-                        if 1 == matrix[r][c]:
-                            rows_having_1_free_cols.add(r)
-                nonlocal ans
+                for col in free_cols:
+                    for row in range(m):
+                        if 1 == matrix[row][col]:
+                            rows_having_1_free_cols.add(row)
                 ans = max(ans, m - len(rows_having_1_free_cols))
             else:
                 depth -= 1
-                for c in range(len(free_cols)):
-                    dfs(depth, free_cols[:c] + free_cols[c + 1:])
+                for i in range(len(free_cols)):
+                    dfs(depth = depth, free_cols = free_cols[:i] + free_cols[i + 1:])
 
-        ans = 0
-        dfs(numSelect, list(range(n)))
+        dfs(depth = numSelect, free_cols = list(range(n)))
         return ans
 
 
@@ -28,7 +28,7 @@ eg_matrix = [
     [0, 0, 0],
     [1, 0, 1],
     [0, 1, 1],
-    [0, 0, 1]
+    [0, 0, 1],
 ]
 eg_Select = 2
-print(Solution().maximumRows(matrix = eg_matrix, numSelect = eg_Select))
+print(Solution().maximumRows(eg_matrix, eg_Select))
