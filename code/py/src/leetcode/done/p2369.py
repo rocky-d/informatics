22aca133ed38fdc72310fe43da0dfa0095ec2611
Rocky Haotian Du <2712479005@qq.com>
2024-03-01 13:07:24 +0800

    Updated

--- code/py/src/leetcode/done/p2369.py
+++ code/py/src/leetcode/done/p2369.py
@@ -5,5 +5,5 @@ class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         dp = deque((True, False, nums[0] == nums[1]), maxlen = 3)
         for i in range(2, len(nums)):
-            dp.append(dp[1] and nums[i - 1] == nums[i] or dp[0] and (nums[i - 2] == nums[i - 1] == nums[i] or -1 == nums[i - 2] - nums[i - 1] == nums[i - 1] - nums[i]))
+            dp.append(dp[-2] and nums[i - 1] == nums[i] or dp[-3] and (nums[i - 2] == nums[i - 1] == nums[i] or -1 == nums[i - 2] - nums[i - 1] == nums[i - 1] - nums[i]))
         return dp[-1]
