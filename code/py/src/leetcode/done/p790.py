dc749c54508b4d1354ff36887ee4cd85e31ed2b1
Rocky Haotian Du <2712479005@qq.com>
2024-03-01 13:25:38 +0800

    Updated

--- code/py/src/leetcode/done/p790.py
+++ code/py/src/leetcode/done/p790.py
@@ -1,7 +1,7 @@
 class Solution:
     def numTilings(self, n: int) -> int:
         dp = [(0, 1), (0, 1)]
-        for i in range(2, 1 + n):
+        for _ in range(2, 1 + n):
             dp.append((dp[-2][1] + dp[-1][0], dp[-2][1] + dp[-1][1] + 2 * dp[-1][0]))
         return dp[-1][1] % 1_000_000_007
 
