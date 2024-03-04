8635b916d890111d81b23a08b90bdcbce49f87bd
Rocky Haotian Du <2712479005@qq.com>
2024-03-04 22:02:04 +0800

    Updated

--- code/py/src/leetcode/undone/until20240301/p2132.py
+++ code/py/src/leetcode/undone/until20240301/p2132.py
@@ -4,26 +4,28 @@ from rockyutil.leetcode import *
 class Solution:
     def possibleToStamp(self, grid: List[List[int]], stampHeight: int, stampWidth: int) -> bool:
         m, n = len(grid), len(grid[0])
-
-        pre = [[0 for _ in range(1 + n)] for _ in range(1 + m)]
+        prefs = [[0] + [0 for _ in range(n)]] + [[0] for _ in range(m)]
+        for i, row in enumerate(grid, 1):
+            for j, val in enumerate(row, 1):
+                prefs[i].append(val - prefs[i - 1][j - 1] + prefs[i - 1][j] + prefs[i][j - 1])
+        diffs = [[0 for _ in range(2 + n)] for _ in range(2 + m)]
+        for x1 in range(m - stampHeight + 1):
+            for y1 in range(n - stampWidth + 1):
+                x2, y2 = x1 + stampHeight, y1 + stampWidth
+                if 0 == prefs[x2][y2] + prefs[x1][y1] - prefs[x1][y2] - prefs[x2][y1]:
+                    diffs[x2 + 1][y2 + 1] += 1
+                    diffs[x1 + 1][y1 + 1] += 1
+                    diffs[x1 + 1][y2 + 1] -= 1
+                    diffs[x2 + 1][y1 + 1] -= 1
         for i in range(m):
             for j in range(n):
-                pre[i + 1][j + 1] = grid[i][j] + pre[i + 1][j] + pre[i][j + 1] - pre[i][j]
-
-        diff = [[0 for _ in range(2 + n)] for _ in range(2 + m)]
-        for i in range(m - stampHeight + 1):
-            for j in range(n - stampWidth + 1):
-                x = i + stampHeight
-                y = j + stampWidth
-                if 0 == pre[x][y] - pre[x][j] - pre[i][y] + pre[i][j]:
-                    diff[i + 1][j + 1] += 1
-                    diff[x + 1][y + 1] += 1
-                    diff[i + 1][y + 1] -= 1
-                    diff[x + 1][j + 1] -= 1
-
-        for i in range(m):
-            for j in range(n):
-                diff[i + 1][j + 1] += diff[i][j + 1] + diff[i + 1][j] - diff[i][j]
-                if 0 == grid[i][j] and 0 == diff[i + 1][j + 1]:
-                    return False
-        return True
+                diffs[i + 1][j + 1] += diffs[i][j + 1] + diffs[i + 1][j] - diffs[i][j]
+                if 0 == grid[i][j] and 0 == diffs[i + 1][j + 1]:
+                    ans = False
+                    break
+            else:
+                continue
+            break
+        else:
+            ans = True
+        return ans
