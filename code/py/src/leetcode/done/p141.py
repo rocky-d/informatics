408c29f92644151ea1d50f4853ea7d12d909cdba
Rocky Haotian Du <2712479005@qq.com>
2024-03-06 08:45:03 +0800

    Updated

---
+++ code/py/src/leetcode/done/p141.py
@@ -0,0 +1,16 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast is not None:
+            fast = fast.next
+            if slow is fast:
+                ans = True
+                break
+            if fast is not None:
+                slow, fast = slow.next, fast.next
+        else:
+            ans = False
+        return ans
