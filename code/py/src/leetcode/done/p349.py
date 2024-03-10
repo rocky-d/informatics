2223a830b70fcbaedd7f6b062ec86444449188df
Rocky Haotian Du <2712479005@qq.com>
2024-03-10 16:29:49 +0800

    Updated

---
+++ code/py/src/leetcode/done/p349.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(frozenset(nums1) & frozenset(nums2))
