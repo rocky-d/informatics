a75f4ea97826a1f4f37da0d26cefce6a17069294
Rocky Haotian Du <2712479005@qq.com>
2024-03-11 20:40:35 +0800

    Updated

--- code/py/src/pta/done/ladder/p3_029.py
+++ code/py/src/pta/done/ladder/p3_029.py
@@ -1,29 +1,29 @@
+from collections import deque
+
+
 def main() -> None:
     n = int(input())
     gaps = input().split()
     m = int(input())
-    fragments = [input().split()[1:] for _ in range(m)]
+    fills = [input().split()[1:] for _ in range(m)]
 
-    ans = []
-    seen = set()
+    ans = deque(maxlen = m)
 
-    def dfs(index, seq):
-        if index == len(gaps) - 1:
-            for item in seq:
-                ans.append(item)
+    def dfs(index: int) -> bool:
+        if index == n - 1:
             return True
-        for i, fragment in enumerate(fragments, 1):
-            if i in seen:
-                continue
-            if fragment[0] == gaps[index]:
-                if fragment == gaps[index:index + len(fragment)]:
-                    seen.add(i)
-                    if dfs(index + len(fragment) - 1, seq + [i]):
-                        return True
-                    seen.remove(i)
-        return False
+        for i, fill in enumerate(fills, 1):
+            if fill == gaps[index:index + len(fill)]:
+                ans.append(i)
+                if dfs(index + len(fill) - 1):
+                    res = True
+                    break
+                ans.pop()
+        else:
+            res = False
+        return res
 
-    dfs(0, [])
+    dfs(0)
     print(*ans)
 
 
