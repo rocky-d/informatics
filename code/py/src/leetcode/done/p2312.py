400c7772f8e1c42ec03bba2a53451f0714a5a3cb
Rocky Haotian Du <2712479005@qq.com>
2024-03-15 10:38:09 +0800

    Updated

--- code/py/src/leetcode/done/p2312.py
+++ code/py/src/leetcode/done/p2312.py
@@ -7,11 +7,10 @@ class Solution:
 
         @cache
         def dfs(a: int, b: int) -> int:
-            res = prices_dict.get((a, b), 0)
-            for x in range(1, a):
-                res = max(res, dfs(x, b) + dfs(a - x, b))
-            for y in range(1, b):
-                res = max(res, dfs(a, y) + dfs(a, b - y))
-            return res
+            return max(
+                prices_dict.get((a, b), 0),
+                max((dfs(x, b) + dfs(a - x, b) for x in range(1, a)), default = 0),
+                max((dfs(a, y) + dfs(a, b - y) for y in range(1, b)), default = 0),
+            )
 
         return dfs(a = m, b = n)
