7163a5ddc59852d5e29f2396eea6759e52ae7904
Rocky Haotian Du <2712479005@qq.com>
2024-03-16 13:08:44 +0800

    Updated

---
+++ code/py/src/rockyutil/datastructure/segmenttree.py
@@ -0,0 +1,7 @@
+class SegmentTreeNode(object):
+
+    def __init__(self, segment, val, left, right):
+        self.segment = segment
+        self.val = val
+        self.left = left
+        self.right = right
