f4ca2359c2d98ed1f7d7ba68a034373f884a0ec9
Rocky Haotian Du <2712479005@qq.com>
2024-03-16 13:30:30 +0800

    Updated

--- code/py/src/rockyutil/datastructure/linkednode.py
+++ code/py/src/rockyutil/datastructure/linkednode.py
@@ -1,7 +1,41 @@
+class SinglyListNode(object):
+
+    def __init__(self, val, nxt):
+        self.val = val
+        self.nxt = nxt
+
+
+class DoublyListNode(object):
+
+    def __init__(self, val, lst, nxt):
+        self.val = val
+        self.lst = lst
+        self.nxt = nxt
+
+
+class BinaryTreeNode(object):
+
+    def __init__(self, val, lft, rit):
+        self.val = val
+        self.lft = lft
+        self.rit = rit
+
+
 class SegmentTreeNode(object):
 
-    def __init__(self, segment, val, left, right):
+    def __init__(self, segment, val, lft, rit):
         self.segment = segment
         self.val = val
-        self.left = left
-        self.right = right
+        self.lft = lft
+        self.rit = rit
+
+
+class NaryTreeNode(object):
+
+    def __init__(self, val, nxts):
+        self.val = val
+        self.nxts = nxts
+
+
+if __name__ == '__main__':
+    ...
