bb43e6539abcc6505c1400cb8b3fb971d0c81241
Rocky Haotian Du <2712479005@qq.com>
2024-03-17 11:42:34 +0800

    Updated

---
+++ code/py/src/leetcode/contest/c20240317wc389/q4_100227.py
@@ -0,0 +1,14 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def minimumMoves(self, nums: List[int], k: int, maxChanges: int) -> int:
+        ones_idxes = [idx for idx, num in enumerate(nums) if 1 == num]
+        print(ones_idxes)
+        return ...
+
+
+eg_nums = [1, 1, 0, 0, 0, 1, 1, 0, 0, 1]
+eg_k = 3
+eg_maxChanges = 1
+print(Solution().minimumMoves(eg_nums, eg_k, eg_maxChanges))
