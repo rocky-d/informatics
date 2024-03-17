1be899f3f78b26b6dfe5ac366b7c2fb9088f3942
Rocky Haotian Du <2712479005@qq.com>
2024-03-17 17:08:01 +0800

    Updated

---
+++ code/py/src/leetcode/undone/p57.py
@@ -0,0 +1,27 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ans = []
+        new_lft, new_rit = newInterval
+        idx, n = 0, len(intervals)
+        while idx < n and intervals[idx][1] < new_lft:
+            ans.append(intervals[idx])
+            idx += 1
+        if idx < n:
+            new_lft = min(new_lft, intervals[idx][0])
+        while idx < n and intervals[idx][0] <= new_rit:
+            idx += 1
+        if 0 <= idx - 1 < n:
+            new_rit = max(new_rit, intervals[idx - 1][1])
+        ans.append([new_lft, new_rit])
+        while idx < n:
+            ans.append(intervals[idx])
+            idx += 1
+        return ans
+
+
+eg_intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+eg_newInterval = [4, 8]
+print(Solution().insert(eg_intervals, eg_newInterval))
