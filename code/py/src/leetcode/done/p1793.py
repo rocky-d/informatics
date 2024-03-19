92fb0ef03a968be430447312cb98281e1fef9fd4
Rocky Haotian Du <2712479005@qq.com>
2024-03-19 20:48:23 +0800

    Updated

--- code/py/src/leetcode/done/p1793.py
+++ code/py/src/leetcode/done/p1793.py
@@ -5,7 +5,7 @@ class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
         ans = 0
         n = len(nums)
-        lft, rit = k, k
+        lft, rit = k - 1, k + 1
         for line in range(nums[k], 0, -1):
             while 0 <= lft and line <= nums[lft]:
                 lft -= 1
