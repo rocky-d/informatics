0662bf57ff0f08ea1652f38e5315a093ff3b5cc5
Rocky Haotian Du <2712479005@qq.com>
2024-03-22 15:20:46 +0800

    Updated

--- code/py/src/leetcode/done/until20240301/p102.py
+++ code/py/src/leetcode/done/until20240301/p102.py
@@ -3,17 +3,17 @@ from rockyutil.leetcode import *
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
         ans = []
-        que = deque()
-        if root is not None:
-            que.append((root, 0))
+        que = deque([(root, 0)])
         while 0 < len(que):
-            node, row = que.popleft()
-            if row == len(ans):
+            node, level = que.popleft()
+            if level == len(ans):
                 ans.append([])
             ans[-1].append(node.val)
             if node.left is not None:
-                que.append((node.left, row + 1))
+                que.append((node.left, level + 1))
             if node.right is not None:
-                que.append((node.right, row + 1))
+                que.append((node.right, level + 1))
         return ans
