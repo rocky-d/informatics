a8fc070ce6983ea38e50b5bea134eb0c438acb11
Rocky Haotian Du <2712479005@qq.com>
2024-03-23 14:43:36 +0800

    Updated

---
+++ code/py/src/leetcode/done/p143.py
@@ -0,0 +1,24 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        dque1, dque2 = deque(), deque()
+        node = head
+        while node is not None:
+            dque1.append(node)
+            node = node.next
+        for _ in range(len(dque1) // 2):
+            dque2.append(dque1.pop())
+        dummy = ListNode()
+        node = dummy
+        for node1, node2 in zip(dque1, dque2):
+            node.next = node1
+            node = node.next
+            node.next = node2
+            node = node.next
+        if len(dque1) > len(dque2):
+            node.next = dque1[-1]
+            node = node.next
+        node.next = None
+        return dummy.next
