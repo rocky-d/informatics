52155e085b2034198d8618e2b028511fdc650e41
Rocky Haotian Du <2712479005@qq.com>
2024-03-25 15:28:40 +0800

    Updated

--- code/py/src/leetcode/done/p3092.py
+++ code/py/src/leetcode/done/p3092.py
@@ -5,11 +5,11 @@ class Solution:
     def mostFrequentIDs(self, nums: List[int], freq: List[int]) -> List[int]:
         ans = []
         cnter = Counter()
-        fqs = []
+        heap = []
         for num, fq in zip(nums, freq):
             cnter[num] += fq
-            heappush(fqs, (-cnter[num], num))
-            while -fqs[0][0] != cnter[fqs[0][1]]:
-                heappop(fqs)
-            ans.append(-fqs[0][0])
+            heappush(heap, (-cnter[num], num))
+            while -heap[0][0] != cnter[heap[0][1]]:
+                heappop(heap)
+            ans.append(-heap[0][0])
         return ans
