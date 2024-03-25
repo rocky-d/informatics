b095f594bd7cd5d9138da278fb6bda0c2eba2684
Rocky Haotian Du <2712479005@qq.com>
2024-03-25 13:56:40 +0800

    Updated

---
+++ code/py/src/leetcode/done/p61.py
@@ -0,0 +1,23 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        end = ListNode(next = head)
+        nodes = 0
+        while end.next is not None:
+            end = end.next
+            nodes += 1
+        if 0 == nodes:
+            return None
+        k %= nodes
+        if 0 == k:
+            return head
+        fast, slow = head, head
+        for _ in range(k):
+            fast = fast.next
+        while fast.next is not None:
+            fast, slow = fast.next, slow.next
+        ans, slow.next = slow.next, None
+        end.next = head
+        return ans
