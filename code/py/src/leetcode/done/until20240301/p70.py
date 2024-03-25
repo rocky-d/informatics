8f0e06206b3a80028059e391b19c3a07624b9300
Rocky Haotian Du <2712479005@qq.com>
2024-03-25 14:10:36 +0800

    Updated

--- code/py/src/leetcode/done/until20240301/p70.py
+++ code/py/src/leetcode/done/until20240301/p70.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp0, dp1 = 0, 1
-        for _ in range(n):
+        dp0, dp1 = 1, 1
+        for _ in range(1, n):
             dp0, dp1 = dp1, dp0 + dp1
         return dp1
