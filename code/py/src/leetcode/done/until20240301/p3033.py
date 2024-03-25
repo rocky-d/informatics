ac68ba3f8ef69fcdabacc27b737a04a22f61ae30
Rocky Haotian Du <2712479005@qq.com>
2024-03-25 20:56:51 +0800

    Updated

--- code/py/src/leetcode/done/until20240301/p3033.py
+++ code/py/src/leetcode/done/until20240301/p3033.py
@@ -4,12 +4,11 @@ from rockyutil.leetcode import *
 class Solution:
     def modifiedMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
         m, n = len(matrix), len(matrix[0])
-        col_max = dict()
+        col_max = {}
         for row in range(m):
             for col in range(n):
                 if -1 == matrix[row][col]:
-                    if col in col_max:
-                        matrix[row][col] = col_max[col]
-                    else:
-                        matrix[row][col] = col_max[col] = max(matrix[row_][col] for row_ in range(m))
+                    if col not in col_max:
+                        col_max[col] = max(matrix[row_][col] for row_ in range(m))
+                    matrix[row][col] = col_max[col]
         return matrix
