d3459ce975f43cf75b7a21ab98d3e83c885ed273
Rocky Haotian Du <2712479005@qq.com>
2024-03-26 08:11:24 +0800

    Updated

--- code/py/src/leetcode/done/p3093.py
+++ code/py/src/leetcode/done/p3093.py
@@ -6,12 +6,14 @@ class Solution:
         ans = []
 
         class TrieNode(object):
+            __slots__ = 'val1', 'val2', 'children'
+
             def __init__(self, val1: int, val2: int, children: Dict[str, 'TrieNode']) -> None:
                 self.val1 = val1
                 self.val2 = val2
                 self.children = children
 
-        def add(node: TrieNode, w: int, c: int) -> None:
+        def update(node: TrieNode, w: int, c: int) -> None:
             if word_len < node.val1:
                 node.val1 = word_len
                 node.val2 = w
@@ -20,21 +22,21 @@ class Solution:
             char = word[c]
             if char not in node.children.keys():
                 node.children[char] = TrieNode(val1 = 5001, val2 = 10000, children = {})
-            add(node.children[char], w, c - 1)
+            update(node.children[char], w, c - 1)
 
         root = TrieNode(val1 = 5001, val2 = 10000, children = {})
         for w, word in enumerate(wordsContainer):
             word_len = len(word)
-            add(node = root, w = w, c = word_len - 1)
+            update(node = root, w = w, c = word_len - 1)
 
-        def dfs(node: TrieNode, word: str, c: int) -> int:
+        def query(node: TrieNode, word: str, c: int) -> int:
             if c < 0:
                 return node.val2
             char = word[c]
-            return dfs(node.children[char], word, c - 1) if char in node.children.keys() else node.val2
+            return query(node.children[char], word, c - 1) if char in node.children.keys() else node.val2
 
         for word in wordsQuery:
-            ans.append(dfs(node = root, word = word, c = len(word) - 1))
+            ans.append(query(node = root, word = word, c = len(word) - 1))
         return ans
 
 
