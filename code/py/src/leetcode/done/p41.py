22287d9407afd63cc250abbb946b04a86edda0c3
Rocky Haotian Du <2712479005@qq.com>
2024-03-26 09:31:47 +0800

    Updated

--- code/py/src/leetcode/done/p41.py
+++ code/py/src/leetcode/done/p41.py
@@ -3,11 +3,18 @@ from rockyutil.leetcode import *
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums_set = frozenset(nums)
-        for num in range(1, len(nums) + 1):
-            if num not in nums_set:
-                ans = num
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        for i, num in enumerate(nums, 1):
+            if i != num:
+                ans = i
                 break
         else:
-            ans = len(nums) + 1
+            ans = n + 1
         return ans
+
+
+eg_nums = [3, 4, -1, 1]
+print(Solution().firstMissingPositive(eg_nums))
