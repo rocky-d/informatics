317174bdf1d59d6d2492a97ef325858467b6ef98
Rocky Haotian Du <2712479005@qq.com>
2024-03-26 08:32:58 +0800

    Updated

--- code/py/src/leetcode/done/p3093.py
+++ code/py/src/leetcode/done/p3093.py
@@ -6,12 +6,12 @@ class Solution:
         ans = []
 
         class TrieNode(object):
-            __slots__ = 'val1', 'val2', 'children'
+            __slots__ = 'val1', 'val2', 'nxts'
 
-            def __init__(self, val1: int, val2: int, children: Dict[str, 'TrieNode']) -> None:
+            def __init__(self, val1: int, val2: int, nxts: Dict[str, 'TrieNode']) -> None:
                 self.val1 = val1
                 self.val2 = val2
-                self.children = children
+                self.nxts = nxts
 
         def update(node: TrieNode, w: int, c: int) -> None:
             if word_len < node.val1:
@@ -20,11 +20,11 @@ class Solution:
             if c < 0:
                 return
             char = word[c]
-            if char not in node.children.keys():
-                node.children[char] = TrieNode(val1 = 5001, val2 = 10000, children = {})
-            update(node.children[char], w, c - 1)
+            if char not in node.nxts.keys():
+                node.nxts[char] = TrieNode(val1 = 5001, val2 = 10000, nxts = {})
+            update(node.nxts[char], w, c - 1)
 
-        root = TrieNode(val1 = 5001, val2 = 10000, children = {})
+        root = TrieNode(val1 = 5001, val2 = 10000, nxts = {})
         for w, word in enumerate(wordsContainer):
             word_len = len(word)
             update(node = root, w = w, c = word_len - 1)
@@ -33,7 +33,7 @@ class Solution:
             if c < 0:
                 return node.val2
             char = word[c]
-            return node.val2 if char not in node.children.keys() else query(node.children[char], word, c - 1)
+            return node.val2 if char not in node.nxts.keys() else query(node.nxts[char], word, c - 1)
 
         for word in wordsQuery:
             ans.append(query(node = root, word = word, c = len(word) - 1))
