554ba692445824495f688b8f71b42faca4e23325
Rocky Haotian Du <2712479005@qq.com>
2024-03-26 21:33:16 +0800

    Updated

--- code/py/src/leetcode/undone/until20240301/p787.py
+++ code/py/src/leetcode/undone/until20240301/p787.py
@@ -3,35 +3,38 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = [[] for _ in range(n)]
+        graph = [deque() for _ in range(n)]
         for fr, to, price in flights:
             graph[fr].append((to, price))
-        dists = [inf for _ in range(n)]
-        dists[src] = 0
-        seen = {src}
-        heap_min = [(0, -1, src)]
-        for _ in range(n):
-            spend, trans, fr = heappop(heap_min)
-            while spend != dists[fr]:
-                spend, trans, fr = heappushpop(heap_min, (dists[fr], trans, fr))
-            if k < trans + 1:
-                continue
-            for to, price in graph[fr]:
-                if spend + price < dists[to]:
-                    dists[to] = spend + price
-                    if to not in seen:
-                        seen.add(to)
-                        heappush(heap_min, (dists[to], trans + 1, to))
-        return -1 if isinf(dists[dst]) else dists[dst]
+        seen = set()
+        costs, heap = [inf for _ in range(n)], []
+        costs[src] = 0
+        heappush(heap, (costs[src], src, 1))
+        while 0 < len(heap):
+            cost, node, cnt = heappop(heap)
+            while (node in seen or k + 2 < cnt) and 0 < len(heap):
+                cost, node, cnt = heappop(heap)
+            seen.add(node)
+            if node == dst:
+                ans = cost
+                break
+            for nxt, price in graph[node]:
+                if cost + price < costs[nxt]:
+                    costs[nxt] = cost + price
+                    heappush(heap, (costs[nxt], nxt, cnt + 1))
+        else:
+            ans = -1
+        return ans
 
 
-eg_n = 3
+eg_n = 4
 eg_flights = [
-    [0, 1, 100],
-    [1, 2, 100],
-    [0, 2, 500],
+    [0, 1, 1],
+    [0, 2, 5],
+    [1, 2, 1],
+    [2, 3, 1],
 ]
 eg_src = 0
-eg_dst = 2
+eg_dst = 3
 eg_k = 1
 print(Solution().findCheapestPrice(eg_n, eg_flights, eg_src, eg_dst, eg_k))
