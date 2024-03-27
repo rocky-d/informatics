d3c31c290d87484f27bc9ff3e11306e364b5160c
Rocky Haotian Du <2712479005@qq.com>
2024-03-27 20:35:36 +0800

    Updated

--- code/py/src/leetcode/done/p2580.py
+++ code/py/src/leetcode/done/p2580.py
@@ -3,10 +3,9 @@ from rockyutil.leetcode import *
 
 class Solution:
     def countWays(self, ranges: List[List[int]]) -> int:
-        ranges.sort()
         groups = 0
         rit_max = -1
-        for lft, rit in ranges:
+        for lft, rit in sorted(ranges):
             if rit_max < lft:
                 groups += 1
             rit_max = max(rit_max, rit)
