6c88b81c2a2eb4e7d437b03f847cd343389e9a28
Rocky Haotian Du <2712479005@qq.com>
2024-03-28 10:12:08 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2958.py
@@ -0,0 +1,28 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        ans = 1
+        cnter = Counter([nums[0]])
+        lft, rit = 0, 0
+        while True:
+            while k < cnter[nums[rit]]:
+                cnter[nums[lft]] -= 1
+                lft += 1
+            while cnter[nums[rit]] <= k:
+                rit += 1
+                if rit == len(nums):
+                    ans = max(ans, rit - lft)
+                    break
+                cnter[nums[rit]] += 1
+            else:
+                ans = max(ans, rit - lft)
+                continue
+            break
+        return ans
+
+
+eg_nums = [1, 2, 3, 1, 2, 3, 1, 2]
+eg_k = 2
+print(Solution().maxSubarrayLength(eg_nums, eg_k))
