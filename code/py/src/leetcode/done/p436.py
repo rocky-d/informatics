2ce39c0075062d22b6a56db7fcae58edf1af41e9
Rocky Haotian Du <2712479005@qq.com>
2024-03-28 11:35:35 +0800

    Updated

---
+++ code/py/src/leetcode/done/p436.py
@@ -0,0 +1,10 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        ans = []
+        items = sorted(((lft, i) for i, (lft, rit) in enumerate(intervals)), key = lambda item: item[0]) + [(1_000_001, -1)]
+        for i, (lft, rit) in enumerate(intervals):
+            ans.append(items[bisect_left(items, rit, key = lambda interval: interval[0])][1])
+        return ans
