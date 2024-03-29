0750bf163bc6d7dfd1de928b7c6371509f1f5f28
Rocky Haotian Du <2712479005@qq.com>
2024-03-29 12:34:37 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2962.py
@@ -0,0 +1,27 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        ans = 0
+        n = len(nums)
+        maxi, cnt = max(nums), 0
+        lft, rit = 0, 0
+        if maxi == nums[rit]:
+            cnt += 1
+        while True:
+            while k <= cnt:
+                if maxi == nums[lft]:
+                    cnt -= 1
+                lft += 1
+            while cnt < k:
+                ans += lft
+                rit += 1
+                if rit == n:
+                    break
+                if maxi == nums[rit]:
+                    cnt += 1
+            else:
+                continue
+            break
+        return ans
