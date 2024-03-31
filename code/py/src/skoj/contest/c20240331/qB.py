6ff1d0a7e666a46452aaf0eab9719f2449b68321
Rocky Haotian Du <2712479005@qq.com>
2024-03-31 15:03:29 +0800

    Updated

--- code/py/src/skoj/contest/c20240331/qB.py
+++ code/py/src/skoj/contest/c20240331/qB.py
@@ -4,9 +4,9 @@ def main() -> None:
     if n > m:
         n, m = m, n
     if m - n in (0, 1):
-        print('Alice')
-    else:
         print('Brown')
+    else:
+        print('Alice')
 
 
 if __name__ == '__main__':
