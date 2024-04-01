10a5376e016066c5380ee68e2ed053a54beee983
Rocky Haotian Du <2712479005@qq.com>
2024-04-01 09:11:30 +0800

    Updated

--- code/py/src/leetcode/done/p58.py
+++ code/py/src/leetcode/done/p58.py
@@ -1,3 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+        ans = 0
+        idx = len(s) - 1
+        while ' ' == s[idx]:
+            idx -= 1
+        while 0 <= idx and ' ' != s[idx]:
+            idx -= 1
+            ans += 1
+        return ans
