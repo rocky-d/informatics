4463ff02c1d4735b954fd26c16c22199349e90cc
Rocky Haotian Du <2712479005@qq.com>
2024-04-02 08:42:56 +0800

    Updated

--- code/py/src/leetcode/done/p894.py
+++ code/py/src/leetcode/done/p894.py
@@ -5,7 +5,7 @@ class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
         if 0b0 == 0b1 & n:
             return []
-        dp = deque([[], [TreeNode(val = 0, left = None, right = None)]])
+        dp = [[], [TreeNode(val = 0, left = None, right = None)]]
         for nodes in range(3, 1 + n, 2):
             dp.append([]), dp.append([])
             for nodes_lft, nodes_rit in zip(range(1, nodes - 1, +2), range(nodes - 2, 0, -2)):
