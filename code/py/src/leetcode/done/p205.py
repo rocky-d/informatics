75706847064767e62c90c1e360d7d100f1864d1b
Rocky Haotian Du <2712479005@qq.com>
2024-04-02 08:10:13 +0800

    Updated

---
+++ code/py/src/leetcode/done/p205.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dct, vals = dict(), set()
+        for si, ti in zip(s, t):
+            if si in dct.keys():
+                if dct[si] == ti:
+                    ans = False
+                    break
+            else:
+                if ti in vals:
+                    ans = False
+                    break
+                else:
+                    dct[si] = ti
+                    vals.add(ti)
+        else:
+            ans = True
+        return ans
