95bebe053fc3427f889065f9bb1b2c4f147d01f9
Rocky Haotian Du <2712479005@qq.com>
2024-04-06 13:40:52 +0800

    Updated

---
+++ code/py/src/leetcode/undone/p1483.py
@@ -0,0 +1,22 @@
+from rockyutil.leetcode import *
+
+
+class TreeAncestor:
+    def __init__(self, n: int, parent: List[int]) -> None:
+        m = n.bit_length() - 1
+        ancestors = [[p] + [-1 for _ in range(m)] for p in parent]
+        for i in range(m):
+            for node in range(n):
+                if -1 != ancestors[node][i]:
+                    ancestors[node][i + 1] = ancestors[ancestors[node][i]][i]
+        self.ancestors = ancestors
+
+    def getKthAncestor(self, node: int, k: int) -> int:
+        ancestors = self.ancestors
+        for i in range(k.bit_length()):
+            if 0b1 == 0b1 & k:
+                node = ancestors[node][i]
+                if -1 == node:
+                    break
+            k >>= 1
+        return node
