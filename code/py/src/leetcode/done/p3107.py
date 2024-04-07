19f5bfa3659277ddbf89062347903107a51ddee5
Rocky Haotian Du <2712479005@qq.com>
2024-04-07 16:15:15 +0800

    Updated

--- code/py/src/leetcode/done/p3107.py
+++ code/py/src/leetcode/done/p3107.py
@@ -4,8 +4,8 @@ from rockyutil.leetcode import *
 class Solution:
     def minOperationsToMakeMedianK(self, nums: List[int], k: int) -> int:
         ans = 0
+        heapify(nums)
         heap_max, heap_min = [], nums
-        heapify(heap_min)
         for _ in range(len(heap_min) // 2):
             heappush(heap_max, -heappop(heap_min))
         while k != heap_min[0]:
