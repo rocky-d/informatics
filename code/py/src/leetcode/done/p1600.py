991a6367e7eabe7df73dd2b5df6eee516d4fd609
Rocky Haotian Du <2712479005@qq.com>
2024-04-07 14:42:19 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1600.py
@@ -0,0 +1,33 @@
+from rockyutil.leetcode import *
+
+
+class ThroneInheritance:
+    class Person(object):
+        def __init__(self, name: str, children: List['ThroneInheritance.Person']) -> None:
+            self.name = name
+            self.children = children
+
+    def __init__(self, kingName: str) -> None:
+        king = ThroneInheritance.Person(name = kingName, children = [])
+        self.king = king
+        self.people = {kingName: king}
+
+    def birth(self, parentName: str, childName: str) -> None:
+        person = ThroneInheritance.Person(name = childName, children = [])
+        self.people[parentName].children.append(person)
+        self.people[childName] = person
+
+    def death(self, name: str) -> None:
+        del self.people[name]
+
+    def getInheritanceOrder(self) -> List[str]:
+        res = []
+
+        def dfs(person: ThroneInheritance.Person) -> None:
+            if person.name in self.people.keys():
+                res.append(person.name)
+            for child in person.children:
+                dfs(child)
+
+        dfs(person = self.king)
+        return res
