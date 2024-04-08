43302166e5ebaae1f3a8c298b27de081bfa6c42a
Rocky Haotian Du <2712479005@qq.com>
2024-04-08 14:35:18 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1700.py
@@ -0,0 +1,19 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        students, sandwiches = deque(students), deque(sandwiches)
+        while 0 < len(sandwiches):
+            for _ in range(len(students)):
+                if students[0] == sandwiches[0]:
+                    students.popleft(), sandwiches.popleft()
+                    break
+                else:
+                    students.rotate(-1)
+            else:
+                ans = len(students)
+                break
+        else:
+            ans = 0
+        return ans
