95372646a5430be2bb5ffec43fe9106c1e3064f0
Rocky Haotian Du <2712479005@qq.com>
2024-04-08 15:20:17 +0800

    Updated

--- code/py/src/leetcode/done/p2009.py
+++ code/py/src/leetcode/done/p2009.py
@@ -4,8 +4,9 @@ from rockyutil.leetcode import *
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         n = len(nums)
+        n_1 = n - 1
         nums = sorted(frozenset(nums))
         interval = 0, 0
         for i in range(len(nums)):
-            interval = max(interval, (i, bisect_right(nums, nums[i] + n - 1)), key = lambda item: item[1] - item[0])
+            interval = max(interval, (i, bisect_right(nums, nums[i] + n_1)), key = lambda item: item[1] - item[0])
         return n - (interval[1] - interval[0])
