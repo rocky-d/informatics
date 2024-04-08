2debb8270ac3af8e920403b58b94a1bf5bac8ceb
Rocky Haotian Du <2712479005@qq.com>
2024-04-08 16:14:44 +0800

    Updated

--- code/py/src/leetcode/done/p2009.py
+++ code/py/src/leetcode/done/p2009.py
@@ -7,6 +7,10 @@ class Solution:
         n_1 = n - 1
         nums = sorted(frozenset(nums))
         interval = 0, 0
-        for i in range(len(nums)):
-            interval = max(interval, (i, bisect_right(nums, nums[i] + n_1)), key = lambda item: item[1] - item[0])
+        lo, nums_len = 0, len(nums)
+        for i in range(nums_len):
+            lo = bisect_right(nums, nums[i] + n_1, lo = lo)
+            interval = max(interval, (i, lo), key = lambda item: item[1] - item[0])
+            if lo == nums_len:
+                break
         return n - (interval[1] - interval[0])
