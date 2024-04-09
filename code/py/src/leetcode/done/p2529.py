3497aff5fa7d6260ffd03b919674a67b055fd6a3
Rocky Haotian Du <2712479005@qq.com>
2024-04-09 08:55:55 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2529.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        return max(bisect_left(nums, 0), len(nums) - bisect_right(nums, 0))
