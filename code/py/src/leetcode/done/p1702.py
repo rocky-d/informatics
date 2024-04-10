d4a337d9c83bb5c8124be7d2aba9fca14d56765a
Rocky Haotian Du <2712479005@qq.com>
2024-04-10 09:22:31 +0800

    Updated

--- code/py/src/leetcode/done/p1702.py
+++ code/py/src/leetcode/done/p1702.py
@@ -3,12 +3,12 @@ from rockyutil.leetcode import *
 
 class Solution:
     def maximumBinaryString(self, binary: str) -> str:
-        if '0' not in binary:
-            return binary
         binary_groupby = groupby(binary)
         c, s = next(binary_groupby)
+        any_zero = '0' == c
         idx = len(list(s)) - 1
         for c, s in binary_groupby:
             if '0' == c:
+                any_zero = True
                 idx += len(list(s))
-        return '1' * idx + '0' + '1' * (len(binary) - idx - 1)
+        return '1' * idx + '0' + '1' * (len(binary) - idx - 1) if any_zero else binary
