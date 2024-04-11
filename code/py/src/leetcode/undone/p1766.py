ad5849380f48320ddc806fdd3e7457defcd3f2e1
Rocky Haotian Du <2712479005@qq.com>
2024-04-11 15:04:22 +0800

    Updated

--- code/py/src/leetcode/undone/p1766.py
+++ code/py/src/leetcode/undone/p1766.py
@@ -4,7 +4,7 @@ from rockyutil.leetcode import *
 class Solution:
     def getCoprimes(self, nums: List[int], edges: List[List[int]]) -> List[int]:
         ans = [-1 for _ in nums]
-        valid_pairs = set((i, j) for i in range(1, 51) for j in range(i, 51) if 1 == gcd(i, j))
+        valid_pairs = set((i, j) for i in range(1, 51) for j in range(1, 51) if 1 == gcd(i, j))
         graph = [[] for _ in nums]
         for u, v in edges:
             graph[u].append(v), graph[v].append(u)
@@ -12,8 +12,7 @@ class Solution:
 
         def dfs(node: int, lst: int) -> None:
             for ancestor in ancestors:
-                pair = (nums[node], nums[ancestor]) if nums[node] < nums[ancestor] else (nums[ancestor], nums[node])
-                if pair in valid_pairs:
+                if (nums[node], nums[ancestor]) in valid_pairs:
                     ans[node] = ancestor
                     break
             ancestors.appendleft(node)
