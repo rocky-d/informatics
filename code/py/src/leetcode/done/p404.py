9b30f7dffceb4a6e0eb562fe47490bbce63e58a2
Rocky Haotian Du <2712479005@qq.com>
2024-04-14 15:35:20 +0800

    Updated

---
+++ code/py/src/leetcode/done/p404.py
@@ -0,0 +1,8 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return (root.left.val if root.left is not None and root.left.left is None and root.left.right is None else self.sumOfLeftLeaves(root.left)) + self.sumOfLeftLeaves(root.right)
