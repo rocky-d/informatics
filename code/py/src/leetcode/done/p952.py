cc71800dca8532359569fd5f3efb5d594a661ee4
Rocky Haotian Du <2712479005@qq.com>
2024-04-17 13:39:49 +0800

    Updated

---
+++ code/py/src/leetcode/done/p952.py
@@ -0,0 +1,40 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def largestComponentSize(self, nums: List[int]) -> int:
+        heads = {}
+
+        def find(x: int) -> int:
+            if x == heads[x]:
+                return x
+            heads[x] = find(heads[x])
+            return heads[x]
+
+        def union(a: int, b: int) -> None:
+            a_head, b_head = find(x = a), find(x = b)
+            if a_head != b_head:
+                heads[a] = heads[a_head] = b_head
+
+        for num in nums:
+            a = num
+            if a not in heads.keys():
+                heads[a] = a
+            for factor in range(2, isqrt(num) + 1):
+                times = 0
+                while 0 == num % factor:
+                    num //= factor
+                    times += 1
+                if 1 <= times:
+                    if factor not in heads.keys():
+                        heads[factor] = factor
+                    union(a = a, b = factor)
+            if 1 < num:
+                if num not in heads.keys():
+                    heads[num] = num
+                union(a = a, b = num)
+        return max(Counter(find(x = num) for num in nums).values())
+
+
+eg_nums = [4, 6, 15, 35]
+print(Solution().largestComponentSize(eg_nums))
