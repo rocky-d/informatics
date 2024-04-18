ef1c353eb0702bf565b83814a9311bf44dbb7ee4
Rocky Haotian Du <2712479005@qq.com>
2024-04-18 13:55:46 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2007.py
@@ -0,0 +1,21 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        ans = []
+        cnter = Counter(changed)
+        for num in sorted(changed):
+            num2 = num + num
+            while 0 < cnter[num]:
+                cnter[num] -= 1
+                if 0 < cnter[num2]:
+                    cnter[num2] -= 1
+                    ans.append(num)
+                else:
+                    ans = []
+                    break
+            else:
+                continue
+            break
+        return ans
