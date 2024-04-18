afe20214f243924d1ad07dc70657975630bdd8a2
Rocky Haotian Du <2712479005@qq.com>
2024-04-18 14:10:26 +0800

    Updated

---
+++ code/py/src/leetcode/done/p416.py
@@ -0,0 +1,14 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if 0b1 == 0b1 & total:
+            return False
+        total //= 2
+        dp = [0 for _ in range(1 + total)]
+        for num in nums:
+            for vol in range(total, num - 1, -1):
+                dp[vol] = max(dp[vol], dp[vol - num] + num)
+        return total == dp[-1]
