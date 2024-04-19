a60dafb8b804c52c0846e7587359b30f589d2863
Rocky Haotian Du <2712479005@qq.com>
2024-04-19 13:45:27 +0800

    Updated

--- code/py/src/rockyutil/algorithm/primefactor.py
+++ code/py/src/rockyutil/algorithm/primefactor.py
@@ -1,17 +1,16 @@
+from itertools import chain
 from math import isqrt
 
 
 def prime_factors(num):
-    for factor in range(2, isqrt(num) + 1):
+    for factor in chain(range(2, isqrt(num) + 1), [num]):
         times = 0
         while 0 == num % factor:
             num //= factor
             times += 1
         if 1 <= times:
             yield factor, times
-    if 1 < num:
-        yield num, 1
 
 
 if __name__ == '__main__':
-    print(*prime_factors(49), sep = '\n')
+    print(*prime_factors(13), sep = '\n')
