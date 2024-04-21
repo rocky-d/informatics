94aac1df1564db1be7a8e3d9c217e080d9084a43
Rocky Haotian Du <2712479005@qq.com>
2024-04-21 14:53:09 +0800

    Updated

--- code/py/src/leetcode/done/until20240401/p39.py
+++ code/py/src/leetcode/done/until20240401/p39.py
@@ -5,20 +5,21 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         combination = deque()
+        candidates.sort()
         n = len(candidates)
 
         def dfs(vol: int, idx: int) -> None:
             if 0 == vol:
                 ans.append(list(combination))
                 return
-            candidate = candidates[idx]
-            if 0 <= vol - candidate:
-                combination.append(candidate)
-                dfs(vol - candidate, idx)
-                combination.pop()
-            idx += 1
-            if idx < n:
-                dfs(vol, idx)
+            for i in range(idx, n):
+                candidate = candidates[i]
+                if 0 <= vol - candidate:
+                    combination.append(candidate)
+                    dfs(vol - candidate, i)
+                    combination.pop()
+                else:
+                    break
 
         dfs(vol = target, idx = 0)
         return ans
