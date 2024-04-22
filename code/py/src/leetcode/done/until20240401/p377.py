515436a8df2f13a7deb4102b523efe1cca2906e1
Rocky Haotian Du <2712479005@qq.com>
2024-04-22 11:41:00 +0800

    Updated

--- code/py/src/leetcode/done/until20240401/p377.py
+++ code/py/src/leetcode/done/until20240401/p377.py
@@ -3,16 +3,17 @@ from rockyutil.leetcode import *
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [1] + [0 for _i in range(target)]
+        nums.sort()
+        dp = [1] + [0 for _ in range(target)]
         for i in range(1, 1 + target):
             for num in nums:
                 if num <= i:
                     dp[i] += dp[i - num]
+                else:
+                    break
         return dp[-1]
 
 
-sol = Solution()
-
 eg_nums = [1, 2, 3]
 eg_target = 4
-print(sol.combinationSum4(nums = eg_nums, target = eg_target))
+print(Solution().combinationSum4(eg_nums, eg_target))
