423ad76e3f14762c117e885f20f6baf070d19092
Rocky Haotian Du <2712479005@qq.com>
2024-04-25 09:15:01 +0800

    Updated

--- code/py/src/leetcode/done/p2739.py
+++ code/py/src/leetcode/done/p2739.py
@@ -5,7 +5,7 @@ class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
         return 10 * (mainTank + min(additionalTank, ceil((mainTank - 4) / 4)))
 
-#        (main+x)//5 = x
+#         (main+x)//5 = x
 #     0 < (main+x)-5*x <= 4
 #     0 <   main-4*x   <= 4
 #   4*x <     main     <= 4+4*x
