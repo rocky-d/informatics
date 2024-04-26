46615b3696a40c6a4c53bf7a91807e8a7dacf3f3
Rocky Haotian Du <2712479005@qq.com>
2024-04-26 18:39:13 +0800

    Updated

--- code/py/src/leetcode/done/p658.py
+++ code/py/src/leetcode/done/p658.py
@@ -3,4 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        return sorted(sorted(arr, key = lambda num: (abs(num - x), num))[:k])
+        return sorted(sorted(arr, key = lambda num: (abs(x - num), num))[:k])
