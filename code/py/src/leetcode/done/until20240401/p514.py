4271326be0ed8df652b3f4e3f6c684afb3d41d88
Rocky Haotian Du <2712479005@qq.com>
2024-04-27 12:37:11 +0800

    Updated

--- code/py/src/leetcode/done/until20240401/p514.py
+++ code/py/src/leetcode/done/until20240401/p514.py
@@ -1,20 +1,20 @@
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
-        ring_len = len(ring)
+        n = len(ring)
         dp = {0: 0}
         for char in key:
-            dp_last, dp = dp, {}
-            for index, count in dp_last.items():
-                ring_rotated = ring[index:] + ring[:index]
-                right_steps = ring_rotated.index(char)
-                left_steps = 1 + ring_rotated[::-1].index(char)
-                right_index = (index + right_steps) % ring_len
-                left_index = (index - left_steps + ring_len) % ring_len
-                dp[right_index] = min(dp.get(right_index, 10_001), count + right_steps + 1)
-                dp[left_index] = min(dp.get(left_index, 10_001), count + left_steps + 1)
+            dp_lst, dp = dp, {}
+            for idx, cnt in dp_lst.items():
+                ring_rotated = ring[idx:] + ring[:idx]
+                rit_steps = ring_rotated.index(char)
+                lft_steps = 1 + ring_rotated[::-1].index(char)
+                rit_idx = (idx + rit_steps) % n
+                lft_idx = (idx - lft_steps + n) % n
+                dp[rit_idx] = min(dp.get(rit_idx, 10_001), cnt + rit_steps + 1)
+                dp[lft_idx] = min(dp.get(lft_idx, 10_001), cnt + lft_steps + 1)
         return min(dp.values())
 
 
 eg_ring = 'nyngl'
 eg_key = 'ynl'
-print(Solution().findRotateSteps(ring = eg_ring, key = eg_key))
+print(Solution().findRotateSteps(eg_ring, eg_key))
