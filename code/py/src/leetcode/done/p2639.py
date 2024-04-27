084bcf88891141ae02336116ecf66c3d23c6e82d
Rocky Haotian Du <2712479005@qq.com>
2024-04-27 12:45:43 +0800

    Updated

--- code/py/src/leetcode/done/p2639.py
+++ code/py/src/leetcode/done/p2639.py
@@ -3,8 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
-        ans = [0] * len(grid[0])
-        for row in grid:
-            for i, val in enumerate(row):
-                ans[i] = max(ans[i], len(str(val)))
-        return ans
+        return [max(len(str(grid[row][col])) for row in range(len(grid))) for col in range(len(grid[0]))]
