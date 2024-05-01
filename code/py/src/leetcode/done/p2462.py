adbf1821b8438ae8f7c078d148b70db66438652b
Rocky Haotian Du <2712479005@qq.com>
2024-05-01 19:55:57 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2462.py
@@ -0,0 +1,19 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        if len(costs) < k + candidates + candidates:
+            return sum(nsmallest(k, costs))
+        ans = 0
+        heap_lft, heap_rit = costs[:candidates], costs[-candidates:]
+        heapify(heap_lft), heapify(heap_rit)
+        lft, rit = candidates, -candidates - 1
+        for _ in range(k):
+            if heap_lft[0] <= heap_rit[0]:
+                ans += heapreplace(heap_lft, costs[lft])
+                lft += 1
+            else:
+                ans += heapreplace(heap_rit, costs[rit])
+                rit -= 1
+        return ans
