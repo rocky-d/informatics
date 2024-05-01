bb3acd423f29d4d4ac45ca09a8c7594015b003e3
Rocky Haotian Du <2712479005@qq.com>
2024-05-01 10:23:35 +0800

    Updated

--- code/py/src/leetcode/done/p2000.py
+++ code/py/src/leetcode/done/p2000.py
@@ -1,4 +1,4 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        idx = word.find(ch)
-        return word[:idx + 1][::-1] + word[idx + 1:]
+        idx = word.find(ch) + 1
+        return word[:idx][::-1] + word[idx:]
