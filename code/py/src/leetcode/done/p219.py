eee9c831c682985363321783119d96f054680e8e
Rocky Haotian Du <2712479005@qq.com>
2024-05-02 17:28:19 +0800

    Updated

--- code/py/src/leetcode/done/p219.py
+++ code/py/src/leetcode/done/p219.py
@@ -3,20 +3,20 @@ from rockyutil.leetcode import *
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = set()
+        window = set()
         length = min(len(nums), k + 1)
         for rit in range(length):
-            if nums[rit] in seen:
+            if nums[rit] in window:
                 ans = True
                 break
-            seen.add(nums[rit])
+            window.add(nums[rit])
         else:
             for lft, rit in zip(range(len(nums) - length), range(length, len(nums))):
-                seen.remove(nums[lft])
-                if nums[rit] in seen:
+                window.remove(nums[lft])
+                if nums[rit] in window:
                     ans = True
                     break
-                seen.add(nums[rit])
+                window.add(nums[rit])
             else:
                 ans = False
         return ans
