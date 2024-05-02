0dfdb42cc7cd14895a4134383253ce21b55c1aec
Rocky Haotian Du <2712479005@qq.com>
2024-05-02 18:48:38 +0800

    Updated

---
+++ code/py/src/leetcode/done/p3122.py
@@ -0,0 +1,17 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def minimumOperations(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        cols = [Counter() for _ in range(n)]
+        for i, row in enumerate(grid):
+            for j, val in enumerate(row):
+                cols[j][val] += 1
+        col = cols.pop(0)
+        dp = [m - col[val] for val in range(10)]
+        for col in cols:
+            dp_lst, dp = sorted(enumerate(dp), key = lambda item: item[1]), []
+            for val in range(10):
+                dp.append(m - col[val] + dp_lst[1 if val == dp_lst[0][0] else 0][1])
+        return min(dp)
