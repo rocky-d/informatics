c15d9384529ac5279de2f133d52cad00f22b58da
Rocky Haotian Du <2712479005@qq.com>
2024-05-03 23:23:11 +0800

    Updated

--- code/py/src/rockyutil/algorithm/shortestpath.py
+++ code/py/src/rockyutil/algorithm/shortestpath.py
@@ -7,14 +7,12 @@ def dijkstra(graph, start):
     dsts = [inf] * n
     dsts[start] = 0
     pres = [None] * n
-    seen = [False] * n
     heap = []
     heappush(heap, (dsts[start], start))
     while 0 < len(heap):
         dst, node = heappop(heap)
-        if seen[node] or dsts[node] < dst:
+        if dsts[node] < dst:
             continue
-        seen[node] = True
         for nxt, cost in graph[node]:
             if dst + cost < dsts[nxt]:
                 dsts[nxt], pres[nxt] = dst + cost, node
