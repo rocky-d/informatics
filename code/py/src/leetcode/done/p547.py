004ed106222c58cd3ed9e5dc47f3141f1bdaa2f8
Rocky Haotian Du <2712479005@qq.com>
2024-05-04 14:08:40 +0800

    Updated

---
+++ code/py/src/leetcode/done/p547.py
@@ -0,0 +1,21 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        heads = list(range(n))
+
+        def find(x: int) -> int:
+            if x == heads[x]:
+                return x
+            heads[x] = find(heads[x])
+            return heads[x]
+
+        for u, row in enumerate(isConnected):
+            for v in range(u + 1, n):
+                if 1 == row[v]:
+                    u_head, v_head = find(x = u), find(x = v)
+                    if u_head != v_head:
+                        heads[u] = heads[u_head] = v_head
+        return len(frozenset(find(x = head) for head in heads))
