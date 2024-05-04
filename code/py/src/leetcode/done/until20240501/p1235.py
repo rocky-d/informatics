9f1d7fd93145628fda5e233630a1eb2541b71e87
Rocky Haotian Du <2712479005@qq.com>
2024-05-04 12:21:17 +0800

    Updated

--- code/py/src/leetcode/done/until20240501/p1235.py
+++ code/py/src/leetcode/done/until20240501/p1235.py
@@ -3,14 +3,14 @@ from rockyutil.leetcode import *
 
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted(zip(startTime, endTime, profit), key = lambda x: x[1])
         dp = [0]
+        jobs = sorted(zip(startTime, endTime, profit), key = lambda x: x[1])
         for i, job in enumerate(jobs):
-            dp.append(max(dp[i], dp[bisect_right(jobs, job[0], key = lambda x: x[1])] + job[2]))
+            dp.append(max(dp[-1], dp[bisect_right(jobs, job[0], key = lambda x: x[1])] + job[2]))
         return dp[-1]
 
 
 eg_startTime = [1, 2, 3, 4, 6]
 eg_endTime = [3, 5, 10, 6, 9]
 eg_profit = [20, 20, 100, 70, 60]
-print(Solution().jobScheduling(startTime = eg_startTime, endTime = eg_endTime, profit = eg_profit))
+print(Solution().jobScheduling(eg_startTime, eg_endTime, eg_profit))
