a1b1f9e5b868ab665acd9fb2ea5c50d37276f370
Rocky Haotian Du <2712479005@qq.com>
2024-05-06 08:47:23 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1652.py
@@ -0,0 +1,18 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        if 0 == k:
+            return [0] * n
+        ans = []
+        lft, rit = (1, k) if 0 < k else (n + k , n - 1)
+        cnt = sum(code[lft:rit+1])
+        for _ in range(n):
+            ans.append(cnt)
+            cnt -= code[lft]
+            lft = (lft + 1) % n
+            rit = (rit + 1) % n
+            cnt += code[rit]
+        return ans
--- code/py/src/leetcode/done/until20240501/p2487.py
+++ code/py/src/leetcode/done/until20240501/p2487.py
@@ -3,17 +3,17 @@ from rockyutil.leetcode import *
 
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        decreasing_stack = [ListNode(val = 100_000, next = head)]
+        stk_dec = deque([ListNode(val = 100_000, next = head)])
         node = head
         while node is not None:
             while node.next is not None and node.val < node.next.val:
-                parent = decreasing_stack.pop(-1)
-                parent.next = node.next
-                node = parent
-            decreasing_stack.append(node)
+                pre = stk_dec.pop()
+                pre.next = node.next
+                node = pre
+            stk_dec.append(node)
             node = node.next
-        return decreasing_stack[1]
+        return stk_dec[1]
 
 
 eg_head = link([5, 2, 13, 3, 8])
-print(unlink(Solution().removeNodes(head = eg_head)))
+print(*unlink(Solution().removeNodes(eg_head)))
