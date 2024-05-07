31a22cb4a0ad940b7749d65ad3cb82e8c6ea33e0
Rocky Haotian Du <2712479005@qq.com>
2024-05-07 19:47:40 +0800

    Updated

--- code/py/src/leetcode/done/until20240501/p1463.py
+++ code/py/src/leetcode/done/until20240501/p1463.py
@@ -5,13 +5,13 @@ class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         dp = {(0, n - 1): grid[0][0] + grid[0][-1]}
-        for i in range(1, m):
+        for row in range(1, m):
             dp_lst, dp = dp, defaultdict(lambda: -1)
-            for (j, k), val in dp_lst.items():
+            for (a, b), val in dp_lst.items():
                 for x, y in (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1):
-                    jx, ky = j + x, k + y
-                    if 0 <= jx < n and 0 <= ky < n:
-                        dp[jx, ky] = max(dp[jx, ky], dp_lst[j, k] + (grid[i][jx] if jx == ky else grid[i][jx] + grid[i][ky]))
+                    ax, by = a + x, b + y
+                    if 0 <= ax < n and 0 <= by < n:
+                        dp[ax, by] = max(dp[ax, by], dp_lst[a, b] + (grid[row][ax] if ax == by else grid[row][ax] + grid[row][by]))
         return max(dp.values())
 
 
