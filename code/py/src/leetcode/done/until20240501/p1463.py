17953de08d341eae581df1471038e188c0df4471
Rocky Haotian Du <2712479005@qq.com>
2024-05-07 11:06:46 +0800

    Updated

--- code/py/src/leetcode/done/until20240501/p1463.py
+++ code/py/src/leetcode/done/until20240501/p1463.py
@@ -6,12 +6,12 @@ class Solution:
         m, n = len(grid), len(grid[0])
         dp = {(0, n - 1): grid[0][0] + grid[0][-1]}
         for i in range(1, m):
-            dp_last, dp = dp, defaultdict(lambda: -1)
-            for (j, k), val in dp_last.items():
-                for x, y in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)):
+            dp_lst, dp = dp, defaultdict(lambda: -1)
+            for (j, k), val in dp_lst.items():
+                for x, y in (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1):
                     jx, ky = j + x, k + y
                     if 0 <= jx < n and 0 <= ky < n:
-                        dp[jx, ky] = max(dp[jx, ky], dp_last[j, k] + (grid[i][jx] if jx == ky else grid[i][jx] + grid[i][ky]))
+                        dp[jx, ky] = max(dp[jx, ky], dp_lst[j, k] + (grid[i][jx] if jx == ky else grid[i][jx] + grid[i][ky]))
         return max(dp.values())
 
 
