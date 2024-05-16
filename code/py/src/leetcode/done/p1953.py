0f7642fb5cc80edd49b2cdf7a7d9e589bc945d9c
Rocky Haotian Du <2712479005@qq.com>
2024-05-16 14:31:30 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1953.py
@@ -0,0 +1,8 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def numberOfWeeks(self, milestones: List[int]) -> int:
+        maxm = max(milestones)
+        lst = sum(milestones) - maxm
+        return lst + min(lst + 1, maxm)
