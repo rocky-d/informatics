884ac671110d31602933da748638c2ddffd82eaa
Rocky Haotian Du <2712479005@qq.com>
2024-05-16 14:09:11 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2331.py
@@ -0,0 +1,14 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if 0 == root.val:
+            return False
+        if 1 == root.val:
+            return True
+        if 2 == root.val:
+            res = self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        elif 3 == root.val:
+            res = self.evaluateTree(root.left) and self.evaluateTree(root.right)
+        return res
