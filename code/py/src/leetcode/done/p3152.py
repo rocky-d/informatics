5eb9b0b11c4f9ec6a3a4ffeb6949ff2cb32c16a1
Rocky Haotian Du <2712479005@qq.com>
2024-05-19 14:28:50 +0800

    Updated

---
+++ code/py/src/leetcode/done/p3152.py
@@ -0,0 +1,9 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        prefs = [0]
+        for lst, nxt in pairwise(nums):
+            prefs.append(prefs[-1] + 1 if 0b1 & lst == 0b1 & nxt else prefs[-1])
+        return [prefs[fr] == prefs[to] for fr, to in queries]
