71a5f898bcbc68652eb78041b16ab9eb4158b95b
Rocky Haotian Du <2712479005@qq.com>
2024-05-22 08:52:20 +0800

    Updated

--- code/py/src/leetcode/done/until20240501/p2225.py
+++ code/py/src/leetcode/done/until20240501/p2225.py
@@ -5,10 +5,9 @@ class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         players, losers = set(), Counter()
         for winner, loser in matches:
-            players.add(winner)
-            players.add(loser)
+            players.add(winner), players.add(loser)
             losers[loser] += 1
-        return list(sorted(players - losers.keys()), sorted(loser for loser, count in losers.items() if 1 == count))
+        return [sorted(players - losers.keys()), sorted(loser for loser, cnt in losers.items() if 1 == cnt)]
 
 
 eg_matches = [
@@ -21,6 +20,6 @@ eg_matches = [
     [4, 8],
     [4, 9],
     [10, 4],
-    [10, 9]
+    [10, 9],
 ]
-print(Solution().findWinners(matches = eg_matches))
+print(Solution().findWinners(eg_matches))
