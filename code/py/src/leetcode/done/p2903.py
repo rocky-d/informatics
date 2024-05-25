1cc2ace7d1c09561a24a7010740df5b375a43f25
Rocky Haotian Du <2712479005@qq.com>
2024-05-25 11:10:00 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2903.py
@@ -0,0 +1,17 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
+        n = len(nums)
+        for lft in range(n):
+            for rit in range(lft + indexDifference, n):
+                if valueDifference <= abs(nums[lft] - nums[rit]):
+                    ans = [lft, rit]
+                    break
+            else:
+                continue
+            break
+        else:
+            ans = [-1, -1]
+        return ans
