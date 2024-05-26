8f1e9d7ec90df92efd5007e2a38ea634e69c57bb
Rocky Haotian Du <2712479005@qq.com>
2024-05-26 12:07:03 +0800

    Updated

---
+++ code/py/src/rockyutil/algorithm/factor.py
@@ -0,0 +1,29 @@
+from itertools import chain
+from math import isqrt
+
+
+def factors(num):
+    num_isqrt = isqrt(num)
+    for divisor in range(1, num_isqrt):
+        if 0 == num % divisor:
+            yield divisor
+            yield num // divisor
+    if 0 == num % num_isqrt:
+        yield num_isqrt
+        if num // num_isqrt != num_isqrt:
+            yield num // num_isqrt
+
+
+def prime_factors(num):
+    for factor in chain(range(2, isqrt(num) + 1), [num]):
+        times = 0
+        while 0 == num % factor:
+            num //= factor
+            times += 1
+        if 0 < times:
+            yield factor, times
+
+
+if __name__ == '__main__':
+    print(*factors(num = 48))
+    print(*prime_factors(num = 48))
