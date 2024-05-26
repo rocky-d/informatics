300123ba0722db8c412191f42544d642002af5af
Rocky Haotian Du <2712479005@qq.com>
2024-05-26 14:44:40 +0800

    Updated

---
+++ code/py/src/leetcode/done/p3158.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        return reduce(xor, (num for num, cnt in Counter(nums).items() if 2 == cnt), 0)
---
+++ code/py/src/leetcode/done/p3160.py
@@ -0,0 +1,20 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        ans = []
+        colors = Counter()
+        balls = {}
+        for idx, color in queries:
+            if idx not in balls.keys():
+                balls[idx] = color
+                colors[color] += 1
+            elif balls[idx] != color:
+                colors[balls[idx]] -= 1
+                if 0 == colors[balls[idx]]:
+                    colors.pop(balls[idx])
+                balls[idx] = color
+                colors[color] += 1
+            ans.append(len(colors))
+        return ans
