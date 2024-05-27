89b0ace0317d48adf2e1184af2ee04ce06a25692
Rocky Haotian Du <2712479005@qq.com>
2024-05-27 08:19:44 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2028.py
@@ -0,0 +1,9 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        total = mean * (len(rolls) + n) - sum(rolls)
+        low = total // n
+        upps = total - low * n
+        return [] if low < 1 or 6 == low and 0 < upps or 6 < low else [low] * (n - upps) + [low + 1] * upps
