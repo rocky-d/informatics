a37f819181a71525192ebd14b0272e679c99ff43
Rocky Haotian Du <2712479005@qq.com>
2024-05-30 11:04:49 +0800

    Updated

--- code/py/src/leetcode/done/p1442.py
+++ code/py/src/leetcode/done/p1442.py
@@ -9,6 +9,6 @@ class Solution:
         for i in range(n):
             for j in range(i + 1, n):
                 for k in range(j, n):
-                    if prefs[i] == prefs[k + 1]:  # prefs[i] ^ prefs[j] == prefs[j] ^ prefs[k + 1]
+                    if prefs[i] == prefs[k + 1]:  # if prefs[i] ^ prefs[j] == prefs[j] ^ prefs[k + 1]:
                         ans += 1
         return ans
