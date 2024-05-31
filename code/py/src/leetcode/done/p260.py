631adbc5b3c23e5673cb586fcc9616423ef24551
Rocky Haotian Du <2712479005@qq.com>
2024-05-31 08:28:18 +0800

    Updated

--- code/py/src/leetcode/done/p260.py
+++ code/py/src/leetcode/done/p260.py
@@ -7,5 +7,5 @@ class Solution:
         nums_xor_reduce = reduce(xor, nums)
         lowbit = nums_xor_reduce & -nums_xor_reduce
         for num in nums:
-            ans[0 if 0b0 == num & lowbit else 1] ^= num
+            ans[0 if 0b0 == lowbit & num else 1] ^= num
         return ans
