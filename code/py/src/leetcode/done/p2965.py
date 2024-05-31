d8a9ff61b408b6017c125983cbb7d6d5502b73b8
Rocky Haotian Du <2712479005@qq.com>
2024-05-31 08:27:24 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2965.py
@@ -0,0 +1,11 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        cnter = [-1] + [0] * (n * n)
+        for i in range(n):
+            for j in range(n):
+                cnter[grid[i][j]] += 1
+        return [cnter.index(2), cnter.index(0)]
