122dda57b03c6b59cef85e71997ef209b8b87b1e
Rocky Haotian Du <2712479005@qq.com>
2024-05-31 08:29:01 +0800

    Updated

--- code/py/src/leetcode/done/p260.py
+++ code/py/src/leetcode/done/p260.py
@@ -3,7 +3,7 @@ from rockyutil.leetcode import *
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        ans = [0, 0]
+        ans = [0b0, 0b0]
         nums_xor_reduce = reduce(xor, nums)
         lowbit = nums_xor_reduce & -nums_xor_reduce
         for num in nums:
