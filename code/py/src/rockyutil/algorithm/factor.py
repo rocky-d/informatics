edd1d051b32440d2af013982118e6c4f6f3b504e
Rocky Haotian Du <2712479005@qq.com>
2024-06-01 15:59:58 +0800

    Updated

--- code/py/src/rockyutil/algorithm/factor.py
+++ code/py/src/rockyutil/algorithm/factor.py
@@ -1,4 +1,3 @@
-from itertools import chain
 from math import isqrt
 
 
@@ -15,13 +14,15 @@ def factors(num):
 
 
 def prime_factors(num):
-    for factor in chain(range(2, isqrt(num) + 1), [num]):
+    for factor in range(2, isqrt(num) + 1):
         times = 0
         while 0 == num % factor:
             num //= factor
             times += 1
         if 0 < times:
             yield factor, times
+    if 1 < num:
+        yield num, 1
 
 
 if __name__ == '__main__':
