c83613ffd95b0fbc91152c6e920fb1b191098ea1
Rocky Haotian Du <2712479005@qq.com>
2024-06-02 15:32:14 +0800

    Updated

--- code/py/src/leetcode/done/until20240501/p344.py
+++ code/py/src/leetcode/done/until20240501/p344.py
@@ -3,10 +3,10 @@ from rockyutil.leetcode import *
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        for i in range(len(s) // 2):
-            s[i], s[-1 - i] = s[-1 - i], s[i]
+        half = len(s) // 2
+        for lft, rit in zip(range(0, half, +1), range(-1, -1 - half, -1)):
+            s[lft], s[rit] = s[rit], s[lft]
 
 
 eg_s = ['h', 'e', 'l', 'l', 'o']
-Solution().reverseString(eg_s)
-print(eg_s)
+print(Solution().reverseString(eg_s))
