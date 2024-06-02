d2ced12b423e119f8208c552b33bc893e84fb68e
Rocky Haotian Du <2712479005@qq.com>
2024-06-02 17:31:23 +0800

    Updated

--- code/py/src/leetcode/done/p2928.py
+++ code/py/src/leetcode/done/p2928.py
@@ -3,7 +3,8 @@ from rockyutil.leetcode import *
 
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        return (comb(n + 2, 2)
-                - comb(3, 1) * comb(max(0, n + 2 - 1 * (limit + 1)), 2)
-                + comb(3, 2) * comb(max(0, n + 2 - 2 * (limit + 1)), 2)
-                - comb(3, 3) * comb(max(0, n + 2 - 3 * (limit + 1)), 2))
+        n2, limit1 = n + 2, limit + 1
+        return (comb(n2, 2)
+                - comb(3, 1) * comb(max(0, n2 - 1 * limit1), 2)
+                + comb(3, 2) * comb(max(0, n2 - 2 * limit1), 2)
+                - comb(3, 3) * comb(max(0, n2 - 3 * limit1), 2))
