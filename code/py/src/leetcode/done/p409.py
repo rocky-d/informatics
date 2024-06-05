a0d3e73aa84116b023e754a092f53eb72612f77a
Rocky Haotian Du <2712479005@qq.com>
2024-06-05 08:31:02 +0800

    Updated

---
+++ code/py/src/leetcode/done/p409.py
@@ -0,0 +1,15 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0
+        odd = False
+        for cnt in Counter(s).values():
+            ans += cnt
+            if 0b1 == 0b1 & cnt:
+                ans -= 1
+                odd = True
+        if odd:
+            ans += 1
+        return ans
