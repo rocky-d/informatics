515a68b95224ad14a93010f3ea32402ac9da6a19
Rocky Haotian Du <2712479005@qq.com>
2024-06-08 11:03:22 +0800

    Updated

--- code/py/src/leetcode/done/until20240601/p1442.py
+++ code/py/src/leetcode/done/until20240601/p1442.py
@@ -4,5 +4,5 @@ from rockyutil.leetcode import *
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
         n = len(arr)
-        prefs = list(accumulate(arr, func = xor, initial = 0))
+        prefs = list(accumulate(arr, xor, initial = 0))
         return sum(k - i for i in range(n) for k in range(i + 1, n) if prefs[i] == prefs[k + 1])
