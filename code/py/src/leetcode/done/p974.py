d12f1352babff546d022bc76b319257057a9a4b2
Rocky Haotian Du <2712479005@qq.com>
2024-06-09 11:25:21 +0800

    Updated

---
+++ code/py/src/leetcode/done/p974.py
@@ -0,0 +1,8 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        prefs = list(accumulate(nums, initial = 0))
+        cnter = Counter(pref % k for pref in prefs)
+        return sum(cnter[pref % k] - 1 for pref in prefs) // 2
