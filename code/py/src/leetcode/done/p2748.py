71759e6d7e8f0eff9c39c86c0b9fa595b462a555
Rocky Haotian Du <2712479005@qq.com>
2024-06-20 10:28:37 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2748.py
@@ -0,0 +1,7 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        return sum(1 for i in range(n) for j in range(i + 1, n) if 1 == gcd(int(str(nums[i])[0]), int(str(nums[j])[-1])))
