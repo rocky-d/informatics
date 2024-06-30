b2063b8cef2b866dfb8120c0d929dcda93df8382
Rocky Haotian Du <2712479005@qq.com>
2024-06-30 14:47:48 +0800

    Updated

--- code/py/src/leetcode/done/until20240701/p494.py
+++ code/py/src/leetcode/done/until20240701/p494.py
@@ -7,7 +7,7 @@ class Solution:
         if target < -total or +total < target:
             return 0
         total2 = total << 1
-        dp = [1] + [0 for _ in range(total2)]
+        dp = [1] + [0] * total2
         for num in nums:
             num2 = num << 1
             for vol in range(total2, num2 - 1, -1):
