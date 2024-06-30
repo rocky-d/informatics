b992a4215b075e1247225a642475586eefd0c6da
Rocky Haotian Du <2712479005@qq.com>
2024-06-30 14:46:54 +0800

    Updated

--- code/py/src/leetcode/done/until20240701/p494.py
+++ code/py/src/leetcode/done/until20240701/p494.py
@@ -3,22 +3,13 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        n = len(nums)
-
-        @cache
-        def dfs(idx: int, val: int) -> int:
-            if n == idx:
-                return 1 if target == val else 0
-            num, cnt = nums[idx], 0
-            for i in range(idx, n):
-                if num == nums[i]:
-                    cnt += 1
-                else:
-                    idx = i
-                    break
-            else:
-                idx = n
-            return (0b1 << cnt) * dfs(idx, val) if 0 == num else sum(comb(cnt, i) * dfs(idx, nxt) for i, nxt in enumerate(range(val - cnt * num, val + cnt * num + 1, num + num)))
-
-        return dfs(idx = 0, val = 0)
+        total = sum(nums)
+        if target < -total or +total < target:
+            return 0
+        total2 = total << 1
+        dp = [1] + [0 for _ in range(total2)]
+        for num in nums:
+            num2 = num << 1
+            for vol in range(total2, num2 - 1, -1):
+                dp[vol] += dp[vol - num2]
+        return dp[total + target]
