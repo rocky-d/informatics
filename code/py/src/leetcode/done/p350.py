919ed7e97fdda68f2838631f0950bde9887fab72
Rocky Haotian Du <2712479005@qq.com>
2024-07-02 10:42:22 +0800

    Updated

--- code/py/src/leetcode/done/p350.py
+++ code/py/src/leetcode/done/p350.py
@@ -4,4 +4,4 @@ from rockyutil.leetcode import *
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         cnter1, cnter2 = Counter(nums1), Counter(nums2)
-        return reduce(add, ([num] * min(cnter1[num], cnter2[num]) for num in cnter1.keys() & cnter1.keys()))
+        return reduce(add, ([num] * min(cnter1[num], cnter2[num]) for num in cnter1.keys() & cnter1.keys()), [])
