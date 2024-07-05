7df0274aa7a2ec5b42f463a4052e0724c3fb6275
Rocky Haotian Du <2712479005@qq.com>
2024-07-05 12:30:30 +0800

    Updated

--- code/py/src/leetcode/done/until20240701/p3033.py
+++ code/py/src/leetcode/done/until20240701/p3033.py
@@ -5,10 +5,10 @@ class Solution:
     def modifiedMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
         m, n = len(matrix), len(matrix[0])
         col_max = {}
-        for row in range(m):
-            for col in range(n):
-                if -1 == matrix[row][col]:
-                    if col not in col_max:
-                        col_max[col] = max(matrix[row_][col] for row_ in range(m))
-                    matrix[row][col] = col_max[col]
+        for row in matrix:
+            for j in range(n):
+                if -1 == row[j]:
+                    if j not in col_max.keys():
+                        col_max[j] = max(matrix[i][j] for i in range(m))
+                    row[j] = col_max[j]
         return matrix
