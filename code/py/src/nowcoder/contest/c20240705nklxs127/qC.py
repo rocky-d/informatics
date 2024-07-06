4e0a40662a46520d3163f3071d22db6a7ed6b9c1
Rocky Haotian Du <2712479005@qq.com>
2024-07-06 21:12:08 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240705nklxs127/qC.py
+++ code/py/src/nowcoder/contest/c20240705nklxs127/qC.py
@@ -2,6 +2,7 @@ def main() -> None:
     n, m, q = map(int, input().split())
     uvw = (tuple(map(int, input().split())) for _ in range(m))
     lr = (map(int, input().split()) for _ in range(q))
+
     ans = []
     heads = list(range(1 + n))
 
@@ -11,7 +12,7 @@ def main() -> None:
         heads[x] = find(heads[x])
         return heads[x]
 
-    vals = [-1_000_000_001] * n
+    vals = ['NO ANSWER'] * n
     groups = {x: 1 for x in range(1, 1 + n)}
     for u, v, w in sorted(uvw, key = lambda item: item[-1], reverse = True):
         u_head, v_head = find(u), find(v)
@@ -23,25 +24,8 @@ def main() -> None:
                 heads[v] = heads[v_head] = u_head
                 groups[u_head] += groups.pop(v_head)
             vals[len(groups)] = w
-
-    class SparseTable:
-        def __init__(self, data, func = max):
-            self.func = func
-            self.st = st = [data]
-            i, N = 1, len(st[0])
-            while 2 * i <= N:
-                pre = st[-1]
-                st.append([func(pre[j], pre[j + i]) for j in range(N - 2 * i + 1)])
-                i <<= 1
-
-        def query(self, begin: int, end: int):
-            lg = (end - begin + 1).bit_length() - 1
-            return self.func(self.st[lg][begin], self.st[lg][end - (1 << lg) + 1])
-
-    st = SparseTable(vals)
     for l, r in lr:
-        res = st.query(l, r)
-        ans.append('NO ANSWER' if -1_000_000_001 == res else res)
+        ans.append(vals[r])
     print(*ans, sep = '\n')
 
 
