5a9e8931c9e25f6bbfdb1dab5e57687416f9714e
Rocky Haotian Du <2712479005@qq.com>
2024-07-06 10:57:19 +0800

    Updated

--- code/py/src/leetcode/done/p3101.py
+++ code/py/src/leetcode/done/p3101.py
@@ -4,11 +4,11 @@ from rockyutil.leetcode import *
 class Solution:
     def countAlternatingSubarrays(self, nums: List[int]) -> int:
         ans = 0
-        cnt = 1
+        cnt = 2
         for lst, nxt in pairwise(nums):
             if lst == nxt:
-                ans += cnt * (cnt + 1) // 2
-                cnt = 0
+                ans += comb(cnt, 2)
+                cnt = 1
             cnt += 1
-        ans += cnt * (cnt + 1) // 2
+        ans += comb(cnt, 2)
         return ans
