22b54595fbbe83823ac908fe27ebc5e6add31a7e
Rocky Haotian Du <2712479005@qq.com>
2024-07-06 11:12:12 +0800

    Updated

--- code/py/src/leetcode/done/p2582.py
+++ code/py/src/leetcode/done/p2582.py
@@ -1,5 +1,5 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         n2 = n + n
-        cnt = time % (n2 - 2)
-        return 1 + cnt if cnt < n else n2 - cnt - 1
+        time %= (n2 - 2)
+        return 1 + time if time < n else n2 - time - 1
