aed937a219070d01e3455c8abf019e088a0b3211
Rocky Haotian Du <2712479005@qq.com>
2024-07-12 22:58:09 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240712nkxbys98/qD.py
+++ code/py/src/nowcoder/contest/c20240712nkxbys98/qD.py
@@ -13,9 +13,7 @@ def main() -> None:
     dp = [[0] * n for _ in range(n)]
     for diff in range(1, n):
         for lft, rit in zip(range(0, n - diff), range(diff, n)):
-            for cut in range(lft + 1, rit + 1):
-                if l <= abs((c0s[cut] - c0s[lft]) - (c1s[rit + 1] - c1s[cut])) <= r:
-                    dp[lft][rit] = max(dp[lft][rit], 1 + dp[lft][cut - 1] + dp[cut][rit])
+            dp[lft][rit] = max((1 + dp[lft][cut - 1] + dp[cut][rit] for cut in range(lft + 1, rit + 1) if l <= abs((c0s[cut] - c0s[lft]) - (c1s[rit + 1] - c1s[cut])) <= r), default = 0)
     print(dp[0][-1])
 
 
