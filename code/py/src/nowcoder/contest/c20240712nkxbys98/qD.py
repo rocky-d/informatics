ed4aa59009638a72ddad1c3e4dc0a8a6feda69e7
Rocky Haotian Du <2712479005@qq.com>
2024-07-12 22:49:47 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240712nkxbys98/qD.py
+++ code/py/src/nowcoder/contest/c20240712nkxbys98/qD.py
@@ -1,24 +1,22 @@
-from functools import cache
-from itertools import accumulate
-
-
 def main() -> None:
     n, l, r = map(int, input().split())
     s = input()
 
-    c0s = list(accumulate((1 if '0' == char else 0 for char in s), initial = 0))
-    c1s = list(accumulate((1 if '1' == char else 0 for char in s), initial = 0))
-    seen = {}
-
-    @cache
-    def dfs(lft: int, rit: int) -> int:
-        t = s[lft:rit + 1]
-        if t not in seen.keys():
-            seen[t] = max((1 + dfs(lft, cut - 1) + dfs(cut, rit) for cut in range(lft + 1, rit + 1) if
-                           l <= abs((c0s[cut] - c0s[lft]) - (c1s[rit + 1] - c1s[cut])) <= r), default = 0)
-        return seen[t]
-
-    print(dfs(lft = 0, rit = n - 1))
+    c0s, c1s = [0], [0]
+    for char in s:
+        if '0' == char:
+            c0s.append(c0s[-1] + 1)
+            c1s.append(c1s[-1])
+        else:  # elif '1' == char:
+            c0s.append(c0s[-1])
+            c1s.append(c1s[-1] + 1)
+    dp = [[0] * n for _ in range(n)]
+    for diff in range(1, n):
+        for lft, rit in zip(range(n - diff), range(diff, n)):
+            for cut in range(lft + 1, rit + 1):
+                if l <= abs((c0s[cut] - c0s[lft]) - (c1s[rit + 1] - c1s[cut])) <= r:
+                    dp[lft][rit] = max(dp[lft][rit], 1 + dp[lft][cut - 1] + dp[cut][rit])
+    print(dp[0][-1])
 
 
 if __name__ == '__main__':
