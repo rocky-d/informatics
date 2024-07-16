857aa2e7842ef185006ccbd30b75a4c5d5942f87
Rocky Haotian Du <2712479005@qq.com>
2024-07-16 17:48:14 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240716sqdx1/qA.py
+++ code/py/src/nowcoder/contest/c20240716sqdx1/qA.py
@@ -4,8 +4,8 @@ def main() -> None:
     ans = 0
     m -= 1
     pow2 = [1] + [0] * max(n, m * (n - 1))
-    for j in range(1, len(pow2)):
-        pow2[j] = (pow2[j - 1] << 1) % q
+    for i in range(1, len(pow2)):
+        pow2[i] = (pow2[i - 1] << 1) % q
     combn = 1
     for i in range(1, n + 1):
         combni = combn = combn * (n - i + 1) // i
