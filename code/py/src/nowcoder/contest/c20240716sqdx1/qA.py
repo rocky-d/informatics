249abc8740d6488ad8b21d178f1c4aa2d5de3159
Rocky Haotian Du <2712479005@qq.com>
2024-07-16 17:45:55 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240716sqdx1/qA.py
+++ code/py/src/nowcoder/contest/c20240716sqdx1/qA.py
@@ -1,14 +1,11 @@
-from array import array
-
-
 def main() -> None:
     n, m, q = map(int, input().split())
 
     ans = 0
     m -= 1
-    pow2 = array('I', [1])
-    for _ in range(max(n, m * (n - 1))):
-        pow2.append((pow2[-1] << 1) % q)
+    pow2 = [1] + [0] * max(n, m * (n - 1))
+    for j in range(1, len(pow2)):
+        pow2[j] = (pow2[j - 1] << 1) % q
     combn = 1
     for i in range(1, n + 1):
         combni = combn = combn * (n - i + 1) // i
