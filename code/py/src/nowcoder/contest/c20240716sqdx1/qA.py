5e2112841f85f6b8f00939d01f232364debb4d0c
Rocky Haotian Du <2712479005@qq.com>
2024-07-16 17:27:44 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240716sqdx1/qA.py
+++ code/py/src/nowcoder/contest/c20240716sqdx1/qA.py
@@ -7,13 +7,12 @@ def main() -> None:
     ans = 0
     m -= 1
     pow2 = array('I', [1])
-    for _ in range(m * (n - 1)):
+    for _ in range(max(n, m * (n - 1))):
         pow2.append((pow2[-1] << 1) % q)
-    combn = [1]
+    combn = 1
     for i in range(1, n + 1):
-        combni = combn[-1] * (n - i + 1) // i
-        combn.append(combni)
-        ans += (combni % q) * pow2[m * (n - i)] * pow(pow2[i] - 1, m, q) % q
+        combni = combn = combn * (n - i + 1) // i
+        ans += combni % q * pow(pow2[i] - 1, m, q) % q * pow2[m * (n - i)] % q
         ans %= q
     print(ans)
 
