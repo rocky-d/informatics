d7ce6b44dc334ae4f045535c2b7483a103ae143b
Rocky Haotian Du <2712479005@qq.com>
2024-07-16 16:00:14 +0800

    Updated

--- code/py/src/leetcode/done/p2956.py
+++ code/py/src/leetcode/done/p2956.py
@@ -3,4 +3,5 @@ from rockyutil.leetcode import *
 
 class Solution:
     def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return [sum(1 for num in nums1 if num in nums2), sum(1 for num in nums2 if num in nums1)]
+        set1, set2 = frozenset(nums1), frozenset(nums2)
+        return [sum(1 for num in nums1 if num in set2), sum(1 for num in nums2 if num in set1)]
