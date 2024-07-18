839b074fc025bf666c52efbbc10016090ad0bbfa
Rocky Haotian Du <2712479005@qq.com>
2024-07-18 12:47:36 +0800

    Updated

--- code/py/src/rockyutil/algorithm/topology.py
+++ code/py/src/rockyutil/algorithm/topology.py
@@ -1,25 +1,29 @@
 from collections import deque
 
 
-def topological_sort(graph):
+def topological_sort(graph, directed = True):
     n = len(graph)
+    endpoint = 0 if directed else 1
     ins = [0] * n
     for vs in graph:
         for v in vs:
             ins[v] += 1
-    dque = deque((x for x, cnt in enumerate(ins) if 0 == cnt))
+    dque = deque((x for x, cnt in enumerate(ins) if endpoint == cnt))
     while 0 < len(dque):
         u = dque.popleft()
         yield u
         for v in graph[u]:
             ins[v] -= 1
-            if 0 == ins[v]:
+            if endpoint == ins[v]:
                 dque.append(v)
 
 
 if __name__ == '__main__':
-    print(*topological_sort(graph = [
-        [1, 2],
-        [2],
-        [],
-    ]))
+    print(*topological_sort(
+        graph = [
+            [1, 2],
+            [2],
+            [],
+        ],
+        directed = True,
+    ))
