28482b0f6e7876e26023c1a6c0d54327d118fc36
Rocky Haotian Du <2712479005@qq.com>
2024-07-20 10:04:42 +0800

    Updated

--- code/py/src/leetcode/done/p1605.py
+++ code/py/src/leetcode/done/p1605.py
@@ -5,9 +5,9 @@ class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
         ans = [[0] * len(colSum) for _ in range(len(rowSum))]
         rows, cols = [0] * len(rowSum), [0] * len(colSum)
-        for i, x in enumerate(rowSum):
-            for j, y in enumerate(colSum):
-                ans[i][j] = num = min(x - rows[i], y - cols[j])
+        for i, row in enumerate(rowSum):
+            for j, col in enumerate(colSum):
+                ans[i][j] = num = min(row - rows[i], col - cols[j])
                 rows[i] += num
                 cols[j] += num
         return ans
