3da2d5fcb44773bcfd18fcbe374b7a6bdd3d1aa0
Rocky Haotian Du <2712479005@qq.com>
2024-07-20 10:04:14 +0800

    Updated

--- code/py/src/leetcode/done/p1605.py
+++ code/py/src/leetcode/done/p1605.py
@@ -7,8 +7,7 @@ class Solution:
         rows, cols = [0] * len(rowSum), [0] * len(colSum)
         for i, x in enumerate(rowSum):
             for j, y in enumerate(colSum):
-                num = min(x - rows[i], y - cols[j])
+                ans[i][j] = num = min(x - rows[i], y - cols[j])
                 rows[i] += num
                 cols[j] += num
-                ans[i][j] = num
         return ans
