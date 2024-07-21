bb65a59fdcf1c9ab82af9cbc92b34a3187a86f99
Rocky Haotian Du <2712479005@qq.com>
2024-07-21 22:54:34 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240721nkzs52/qB.py
+++ code/py/src/nowcoder/contest/c20240721nkzs52/qB.py
@@ -3,7 +3,7 @@ def main() -> None:
         a, b, n = map(int, input().split())
         if 0b1 == 0b1 & b:
             b -= 1
-        print('YES' if (n <= a + 3 * min(b // 2, n // 3)) else 'NO')
+        print('YES' if n <= a + 3 * min(b // 2, n // 3) else 'NO')
 
 
 if __name__ == '__main__':
