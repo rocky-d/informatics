14eff7967f6285f059b245ff52a98c99181d3de9
Rocky Haotian Du <2712479005@qq.com>
2024-07-23 09:41:04 +0800

    Updated

--- code/py/src/rockyutil/algorithm/combinations.py
+++ code/py/src/rockyutil/algorithm/combinations.py
@@ -1,17 +1,36 @@
+from itertools import pairwise
 from math import comb
 
 
-def combinations(n, k, mod = None):
-    c = [0] * (1 + k)
-    c[0] = 1
-    for i in range(1 + n):
+def combinations2d(n, k, mod = None):
+    assert 0 <= k <= n
+    c = [[1] + [0] * min(i + 1, k) for i in range(1 + n)]
+    for i, (c_lst, c_nxt) in enumerate(pairwise(c), start = 1):
+        for j in range(min(i, k), 0, -1):
+            c_nxt[j] = c_lst[j - 1] + c_lst[j]
+            if mod is not None:
+                c_nxt[j] %= mod
+    return c
+
+
+def combinations1d(n, k, mod = None):
+    assert 0 <= k <= n
+    c = [1] + [0] * k
+    for i in range(1, 1 + n):
         for j in range(min(i, k), 0, -1):
             c[j] += c[j - 1]
             if mod is not None:
                 c[j] %= mod
-    return c[-1]
+    return c
+
+
+def combinations0d(n, k, mod = None):
+    assert 0 <= k <= n
+    return combinations1d(n, k, mod)[-1]
 
 
 if __name__ == '__main__':
-    print(combinations(100, 24, 10007))
+    print(combinations2d(100, 24, 10007)[-1])
+    print(combinations1d(100, 24, 10007))
+    print(combinations0d(100, 24, 10007))
     print(comb(100, 24) % 10007)
