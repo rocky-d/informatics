3961c084bb9ebe142adf0b53e28fc5acb6497604
Rocky Haotian Du <2712479005@qq.com>
2024-07-26 10:15:36 +0800

    Updated

---
+++ code/py/src/leetcode/done/p1334.py
@@ -0,0 +1,13 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        dp = [[inf] * u + [0] + [inf] * (n - u - 1) for u in range(n)]
+        for u, v, w in edges:
+            dp[u][v] = dp[v][u] = w
+        for k in range(n):
+            for u in range(n):
+                for v in range(n):
+                    dp[u][v] = min(dp[u][v], dp[u][k] + dp[k][v])
+        return max((sum(-1 for v in chain(range(u), range(u + 1, n)) if dp[u][v] <= distanceThreshold), u) for u in range(n))[1]
