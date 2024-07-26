124254224b7fafa1913e1ca4e66138644c35c0f4
Rocky Haotian Du <2712479005@qq.com>
2024-07-26 08:45:39 +0800

    Updated

---
+++ code/py/src/leetcode/done/p2740.py
@@ -0,0 +1,6 @@
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def findValueOfPartition(self, nums: List[int]) -> int:
+        return min(nxt - lst for lst, nxt in pairwise(sorted(nums)))
