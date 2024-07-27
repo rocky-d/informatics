224e149f907bdddda875444db64fae2c39dfb120
Rocky Haotian Du <2712479005@qq.com>
2024-07-27 15:21:30 +0800

    Updated

--- code/py/src/rockyutil/algorithm/binarysearch.py
+++ code/py/src/rockyutil/algorithm/binarysearch.py
@@ -1,6 +1,6 @@
 def func1(lft, rit, check):  # [lft, rit]
     while lft <= rit:
-        mid = lft + rit << 1
+        mid = lft + rit >> 1
         if check(mid):
             lft = mid + 1
         else:
@@ -10,7 +10,7 @@ def func1(lft, rit, check):  # [lft, rit]
 
 def func2(lft, rit, check):  # [lft, rit)
     while lft < rit:
-        mid = lft + rit << 1
+        mid = lft + rit >> 1
         if check(mid):
             lft = mid + 1
         else:
@@ -20,9 +20,18 @@ def func2(lft, rit, check):  # [lft, rit)
 
 def func3(lft, rit, check):  # (lft, rit)
     while lft + 1 < rit:
-        mid = lft + rit << 1
+        mid = lft + rit >> 1
         if check(mid):
             lft = mid
         else:
             rit = mid
     return rit
+
+
+if __name__ == '__main__':
+    nums = [0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9]
+    n = len(nums)
+    check = lambda idx: nums[idx] < 5
+    print(func1(lft = 0, rit = n - 1, check = check))
+    print(func1(lft = 0, rit = n, check = check))
+    print(func1(lft = -1, rit = n, check = check))
