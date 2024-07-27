76ec3a525549ecba442023b7f97910a097d4b4ce
Rocky Haotian Du <2712479005@qq.com>
2024-07-27 21:33:35 +0800

    Updated

--- code/py/src/atcoder/contest/c20240727abc364/qD.py
+++ code/py/src/atcoder/contest/c20240727abc364/qD.py
@@ -1,10 +1,17 @@
+from bisect import bisect
+
+
 def main() -> None:
     n, q = map(int, input().split())
     a = map(int, input().split())
     bk = (map(int, input().split()) for _ in range(q))
 
-    a = list(a)
-    print(*(sorted(abs(ai - bi) for ai in a)[ki - 1] for bi, ki in bk), sep = '\n')
+    ans = []
+    a = sorted(a)
+    for bi, ki in bk:
+        idx = bisect(a, bi)
+        ans.append(sorted(abs(a[i] - bi) for i in range(max(0, idx - ki - 1), min(n - 1, idx + ki + 1) + 1))[ki - 1])
+    print(*ans, sep = '\n')
 
 
 if __name__ == '__main__':
