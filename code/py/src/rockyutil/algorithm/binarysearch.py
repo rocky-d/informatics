da16c648d9136ded43e3a21a9c14b74defcc1d00
Rocky Haotian Du <2712479005@qq.com>
2024-07-27 15:26:28 +0800

    Updated

--- code/py/src/rockyutil/algorithm/binarysearch.py
+++ code/py/src/rockyutil/algorithm/binarysearch.py
@@ -31,7 +31,7 @@ def binary_search_oo(lft, rit, check):  # (lft, rit)
 if __name__ == '__main__':
     nums = [0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9]
     n = len(nums)
-    check = lambda idx: nums[idx] < 5
+    check = lambda idx: nums[idx] <= 5
     print(binary_search_cc(lft = 0, rit = n - 1, check = check))
     print(binary_search_co(lft = 0, rit = n, check = check))
     print(binary_search_oo(lft = -1, rit = n, check = check))
