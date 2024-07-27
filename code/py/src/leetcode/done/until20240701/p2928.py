468ae068be8d5166d3ca478410fa1ce4a971f476
Rocky Haotian Du <2712479005@qq.com>
2024-07-27 09:06:44 +0800

    Updated

--- code/py/src/leetcode/done/until20240701/p2928.py
+++ code/py/src/leetcode/done/until20240701/p2928.py
@@ -1,10 +1,4 @@
-from rockyutil.leetcode import *
-
-
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        n2, limit1 = n + 2, limit + 1
-        return (comb(n2, 2)
-                - comb(3, 1) * comb(max(0, n2 - 1 * limit1), 2)
-                + comb(3, 2) * comb(max(0, n2 - 2 * limit1), 2)
-                - comb(3, 3) * comb(max(0, n2 - 3 * limit1), 2))
+        tmp = limit + limit + 1
+        return sum(i + 1 if i <= limit else max(0, tmp - i) for i in range(n - min(limit, n), n + 1))
