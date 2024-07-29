364f2b1e835e698602bbd9eee9febc1bf25fdd52
Rocky Haotian Du <2712479005@qq.com>
2024-07-29 17:44:09 +0800

    Updated

--- code/py/src/leetcode/done/p3234.py
+++ code/py/src/leetcode/done/p3234.py
@@ -7,9 +7,7 @@ class Solution:
         n = len(s)
         z, zi = [i for i, c in enumerate(s) if '0' == c] + [n], 0
         for lft in range(n):
-            ans += z[zi] - lft
-            for zs, zj in enumerate(range(zi + 1, min(len(z), zi + isqrt(n - lft) + 1)), start = 1):
-                ans += min(max(0, (z[zj] - lft + 1) - (zs + zs * zs)), z[zj] - z[zj - 1])
+            ans += z[zi] - lft + sum(min(max(0, (z[zj] - lft + 1) - (zs + zs * zs)), z[zj] - z[zj - 1]) for zs, zj in enumerate(range(zi + 1, min(len(z), zi + isqrt(n - lft) + 1)), start = 1))
             if '0' == s[lft]:
                 zi += 1
         return ans
@@ -17,15 +15,3 @@ class Solution:
 
 eg_s = '101101'
 print(Solution().numberOfSubstrings(eg_s))
-
-# 11 0111 0111 011 0
-
-'''
-1 <= n <= 40000
-1 <= z+o <= n
-z*z <= o
-
-z+z*z <= n
-z*(z+1) <= n
-z < sqrt(n) < z+1
-'''
