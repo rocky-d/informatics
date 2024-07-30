70205f84ec7b9fa7a2f7e05ce5521b112ae33ebf
Rocky Haotian Du <2712479005@qq.com>
2024-07-30 11:37:50 +0800

    Updated

--- code/py/src/leetcode/done/p875.py
+++ code/py/src/leetcode/done/p875.py
@@ -3,4 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        return bisect_left(range(1_000_000_001), -h, lo = 1, key = lambda x: -sum(ceil(pile / x) for pile in piles))
+        return bisect_left(range(1_000_000_001), -h, lo = 1, key = lambda mid: -sum(ceil(pile / mid) for pile in piles))
