9e4e2187cb7faf615d0caf1b21f37bdfd068c145
Rocky Haotian Du <2712479005@qq.com>
2024-07-30 12:43:25 +0800

    Updated

--- code/py/src/leetcode/done/until20240701/p34.py
+++ code/py/src/leetcode/done/until20240701/p34.py
@@ -3,5 +3,5 @@ from rockyutil.leetcode import *
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left, right = bisect_left(nums, target), bisect_right(nums, target)
-        return [-1, -1] if left == right else [left, right - 1]
+        lft, rit = bisect_left(nums, target), bisect_right(nums, target)
+        return [-1, -1] if lft == rit else [lft, rit - 1]
