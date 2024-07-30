f3d1417319d5752485d5cbbb41f189af3d337e0a
Rocky Haotian Du <2712479005@qq.com>
2024-07-30 17:52:26 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240730sqdx5/qH.py
+++ code/py/src/nowcoder/contest/c20240730sqdx5/qH.py
@@ -5,26 +5,20 @@ def main() -> None:
     n, m = map(int, input().split())
     edges = (map(int, input().split()) for _ in range(m))
 
-    ans = 0
     graph = [None] + [[] for _ in range(n)]
     for u, v in edges:
         graph[u].append(v)
         graph[v].append(u)
 
     @lru_cache(maxsize = None)
-    def dfs(u: int, seen: int, cnt: int) -> None:
-        nonlocal ans
+    def dfs(cnt: int, u: int, seen: int) -> int:
         cnt += 1
-        ans = max(ans, cnt)
-        vs = [v for v in graph[u] if 0b0 == 0b1 << v & seen]
-        for v in vs:
+        v_ls = [v for v in graph[u] if 0b0 == 0b1 << v & seen]
+        for v in v_ls:
             seen |= 0b1 << v
-        for v in vs:
-            dfs(v, seen, cnt)
+        return max(cnt, max((dfs(cnt, v, seen) for v in v_ls), default = cnt))
 
-    for start in range(1, 1 + n):
-        dfs(u = start, seen = 0b1 << start, cnt = 0)
-    print(ans)
+    print(max(dfs(cnt = 0, u = start, seen = 0b1 << start) for start in range(1, 1 + n)))
 
 
 if __name__ == '__main__':
