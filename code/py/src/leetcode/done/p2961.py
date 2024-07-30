ccbbae3d38a09ac9e68ff5552e8d8c31ba4d82e9
Rocky Haotian Du <2712479005@qq.com>
2024-07-30 08:50:53 +0800

    Updated

--- code/py/src/leetcode/done/p2961.py
+++ code/py/src/leetcode/done/p2961.py
@@ -3,4 +3,4 @@ from rockyutil.leetcode import *
 
 class Solution:
     def getGoodIndices(self, variables: List[List[int]], target: int) -> List[int]:
-        return [i for i, (a, b, c, m) in enumerate(variables) if target == pow(pow(a, b) % 10, c) % m]
+        return [i for i, (a, b, c, m) in enumerate(variables) if target == pow(pow(a, b, 10), c, m)]
