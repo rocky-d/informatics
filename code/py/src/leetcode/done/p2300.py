4f724862279000ba5b5e83e28f252389a5b254d2
Rocky Haotian Du <2712479005@qq.com>
2024-07-30 11:34:38 +0800

    Updated

--- code/py/src/leetcode/done/p2300.py
+++ code/py/src/leetcode/done/p2300.py
@@ -4,4 +4,4 @@ from rockyutil.leetcode import *
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         potions = sorted(map(neg, potions))
-        return [bisect_right(range(len(potions)), -success, lo = 0, key = lambda mid: spell * potions[mid]) for spell in spells]
+        return [bisect_right(range(len(potions)), -success, lo = 0, key = lambda mid: potions[mid] * spell) for spell in spells]
