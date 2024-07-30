4a3dba7e2e8fcccc41e159a4d216e5867b1dc612
Rocky Haotian Du <2712479005@qq.com>
2024-07-30 10:58:08 +0800

    Updated

--- code/py/src/leetcode/done/p875.py
+++ code/py/src/leetcode/done/p875.py
@@ -6,7 +6,7 @@ class Solution:
         lo, hi = 0, 1_000_000_001
         while 1 < hi - lo:
             mid = lo + hi >> 1
-            if h < sum(ceil(pile / mid) for pile in piles):
+            if -sum(ceil(pile / mid) for pile in piles) < -h:
                 lo = mid
             else:
                 hi = mid
