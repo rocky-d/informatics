98ce22a002f00168b9f7d62f15a1acdde0ae7429
Rocky Haotian Du <2712479005@qq.com>
2024-07-31 22:36:43 +0800

    Updated

--- code/py/src/rockyutil/datastructure/segmenttree.py
+++ code/py/src/rockyutil/datastructure/segmenttree.py
@@ -8,27 +8,6 @@ class SegmentTreeNode(object):
         self.rit = rit
 
 
-def build(lo, hi):
-    if lo == hi:
-        return SegmentTreeNode(lo = lo, hi = hi, val = nums[lo], lft = None, rit = None)
-    mid = lo + hi >> 1
-    lft, rit = build(lo, mid), build(mid + 1, hi)
-    return SegmentTreeNode(lo = lo, hi = hi, val = max(lft.val, rit.val), lft = lft, rit = rit)
-
-
-def query(lo, hi, node):
-    if node.lo == lo and node.hi == hi:
-        return node.val
-    mid = node.lo + node.hi >> 1
-    if hi <= mid:
-        res = query(lo, hi, node.lft)
-    elif mid + 1 <= lo:
-        res = query(lo, hi, node.rit)
-    else:
-        res = max(query(lo, mid, node.lft), query(mid + 1, hi, node.rit))
-    return res
-
-
 class SegmentTree(object):
 
     def __init__(self, __function, __iterable, *, initial = None):
@@ -60,6 +39,27 @@ class SegmentTree(object):
 
     def query(self, lo, hi):  # [lo, hi)
         return self._query(lo, hi - 1, self._root)
+
+
+def build(lo, hi):
+    if lo == hi:
+        return SegmentTreeNode(lo = lo, hi = hi, val = nums[lo], lft = None, rit = None)
+    mid = lo + hi >> 1
+    lft, rit = build(lo, mid), build(mid + 1, hi)
+    return SegmentTreeNode(lo = lo, hi = hi, val = max(lft.val, rit.val), lft = lft, rit = rit)
+
+
+def query(lo, hi, node):
+    if node.lo == lo and node.hi == hi:
+        return node.val
+    mid = node.lo + node.hi >> 1
+    if hi <= mid:
+        res = query(lo, hi, node.lft)
+    elif mid + 1 <= lo:
+        res = query(lo, hi, node.rit)
+    else:
+        res = max(query(lo, mid, node.lft), query(mid + 1, hi, node.rit))
+    return res
 
 
 if __name__ == '__main__':
