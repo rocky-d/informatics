5fca99959a552f17ee0eca07c3363947e997b101
Rocky Haotian Du <2712479005@qq.com>
2024-08-01 21:41:27 +0800

    Updated

--- code/py/src/rockyutil/datastructure/segmenttree.py
+++ code/py/src/rockyutil/datastructure/segmenttree.py
@@ -25,19 +25,19 @@ class SegmentTree(object):
         lft, rit = self._build(lo, mid), self._build(mid + 1, hi)
         return SegmentTreeNode(val = self._func(lft.val, rit.val), lft = lft, rit = rit)
 
-    def _query(self, node, l, r, lo, hi):  # [lo, hi]
-        if l == lo and hi == r:  # l <= lo <= hi <= r
+    def _query(self, node, lft, rit, lo, hi):  # [lo, hi]
+        if lft == lo and hi == rit:  # lft <= lo <= hi <= rit
             return node.val
-        m0 = l + r >> 1
-        m1 = m0 + 1
-        if hi <= m0:
-            res = self._query(node.lft, l, m0, lo, hi)
-        elif m1 <= lo:
-            res = self._query(node.rit, m1, r, lo, hi)
+        mid0 = lft + rit >> 1
+        mid1 = mid0 + 1
+        if hi <= mid0:
+            res = self._query(node.lft, lft, mid0, lo, hi)
+        elif mid1 <= lo:
+            res = self._query(node.rit, mid1, rit, lo, hi)
         else:
             res = self._func(
-                self._query(node.lft, l, m0, lo, m0),
-                self._query(node.rit, m1, r, m1, hi),
+                self._query(node.lft, lft, mid0, lo, mid0),
+                self._query(node.rit, mid1, rit, mid1, hi),
             )
         return res
 
@@ -53,19 +53,19 @@ def build(lo, hi):
     return SegmentTreeNode(val = add(lft.val, rit.val), lft = lft, rit = rit)
 
 
-def query(node, l, r, lo, hi):
-    if l == lo and hi == r:  # l <= lo <= hi <= r
+def query(node, lft, rit, lo, hi):
+    if lft == lo and hi == rit:  # lft <= lo <= hi <= rit
         return node.val
-    m0 = l + r >> 1
-    m1 = m0 + 1
-    if hi <= m0:
-        res = query(node.lft, l, m0, lo, hi)
-    elif m1 <= lo:
-        res = query(node.rit, m1, r, lo, hi)
+    mid0 = lft + rit >> 1
+    mid1 = mid0 + 1
+    if hi <= mid0:
+        res = query(node.lft, lft, mid0, lo, hi)
+    elif mid1 <= lo:
+        res = query(node.rit, mid1, rit, lo, hi)
     else:
         res = add(
-            query(node.lft, l, m0, lo, m0),
-            query(node.rit, m1, r, m1, hi),
+            query(node.lft, lft, mid0, lo, mid0),
+            query(node.rit, mid1, rit, mid1, hi),
         )
     return res
 
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     nums = [9, 1, 42, 5, 1, 6, 1, 45, 1, 4, 5, 6, 67, 78, 21, 1, 6, 1, 5]
 
     root = build(lo = 0, hi = len(nums) - 1)
-    print(query(node = root, l = 0, r = len(nums) - 1, lo = 3, hi = 5))
+    print(query(node = root, lft = 0, rit = len(nums) - 1, lo = 3, hi = 5))
 
     root = SegmentTree(add, nums)
     print(root.query(lo = 3, hi = 6))
