fc6ffc8520c23db534516f51cc74367027263ea3
Rocky Haotian Du <2712479005@qq.com>
2024-08-03 22:02:10 +0800

    Updated

--- code/py/src/rockyutil/algorithm/trick.py
+++ code/py/src/rockyutil/algorithm/trick.py
@@ -1,11 +1,12 @@
 from math import ceil, floor
 
-a, b = 2, 5
-print((b - a) // 2 == floor((b - a) / 2))
-print((a - b) // 2 == floor((a - b) / 2))
-print(int((b - a) / 2) == floor((b - a) / 2))
-print(int((a - b) / 2) == ceil((a - b) / 2))
-print((a + b) // 2 == (a - b) // 2 + b)
+if __name__ == '__main__':
+    a, b = 2, 5
+    print((b - a) // 2 == floor((b - a) / 2))
+    print((a - b) // 2 == floor((a - b) / 2))
+    print(int((b - a) / 2) == floor((b - a) / 2))
+    print(int((a - b) / 2) == ceil((a - b) / 2))
+    print((a + b) // 2 == (a - b) // 2 + b)
 
-x, n = 10, 3
-print((x + n - 1) // n == ceil(x / n))
+    x, n = 10, 3
+    print((x + n - 1) // n == ceil(x / n))
