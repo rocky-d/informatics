676ba37db175a3221e6ae8323684baec80e35b8f
Rocky Haotian Du <2712479005@qq.com>
2024-08-03 20:41:08 +0800

    Updated

--- code/py/src/rockyutil/algorithm/leapyear.py
+++ code/py/src/rockyutil/algorithm/leapyear.py
@@ -1,6 +1,9 @@
+from calendar import isleap
+
+
 def is_leap_year(year):
     return 0 == year % 4 and 0 != year % 100 or 0 == year % 400
 
 
 if __name__ == '__main__':
-    print(is_leap_year(year = 2100))
+    print(all(isleap(year) == is_leap_year(year) for year in range(-1_000_000, +1_000_001)))
