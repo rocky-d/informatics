4a283a9a4ff17076880440955d5a7d1d3b2d99c9
Rocky Haotian Du <2712479005@qq.com>
2024-08-03 18:47:16 +0800

    Updated

---
+++ code/py/src/codeforces/done/p1996F.py
@@ -0,0 +1,23 @@
+from bisect import bisect_left
+
+
+def main() -> None:
+    n, k = map(int, input().split())
+    a = map(int, input().split())
+    b = map(int, input().split())
+
+    ans = 0
+    a, b = list(a), list(b)
+    least = max(0, bisect_left(range(max(a) + 1), -k, lo = 0, key = lambda mid: -sum(max(0, (ai - mid) // bi + 1) for ai, bi in zip(a, b))) - 1)
+    cnt = 0
+    for ai, bi in zip(a, b):
+        tmp = max(0, (ai - least) // bi + 1)
+        ans += (ai + ai - (tmp - 1) * bi) * tmp // 2
+        cnt += tmp
+    ans -= max(0, cnt - k) * least
+    print(ans)
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        main()
