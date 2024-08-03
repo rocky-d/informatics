4ed5435af99acacde546496612c754d9307fc7db
Rocky Haotian Du <2712479005@qq.com>
2024-08-03 22:17:52 +0800

    Updated

--- code/py/src/rockyutil/algorithm/knapsack.py
+++ code/py/src/rockyutil/algorithm/knapsack.py
@@ -15,6 +15,8 @@ def knapsack_unbounded(volume, items):
 
 
 if __name__ == '__main__':
+    volume = 6
     items = [(1, 2), (2, 3), (3, 5), (4, 7)]
-    print(knapsack_01(volume = 6, items = items))
-    print(knapsack_unbounded(volume = 6, items = items))
+
+    print(knapsack_01(volume, items))
+    print(knapsack_unbounded(volume, items))
