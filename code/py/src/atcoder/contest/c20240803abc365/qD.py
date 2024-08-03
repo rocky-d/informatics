0790d75b823fe2e2c8073a2c9ba5ca1115fd8532
Rocky Haotian Du <2712479005@qq.com>
2024-08-03 21:50:50 +0800

    Updated

--- code/py/src/atcoder/contest/c20240803abc365/qD.py
+++ code/py/src/atcoder/contest/c20240803abc365/qD.py
@@ -2,15 +2,15 @@ def main() -> None:
     n = int(input())
     s = input()
 
-    dp = [0, 0, 0]
+    dp = {'R': 0, 'P': 0, 'S': 0}
     for c in s:
         if 'R' == c:
-            dp[0], dp[1] = max(dp[1], dp[2]), max(dp[0], dp[2]) + 1
+            dp['R'], dp['P'] = max(dp['P'], dp['S']), max(dp['R'], dp['S']) + 1
         elif 'P' == c:
-            dp[1], dp[2] = max(dp[0], dp[2]), max(dp[0], dp[1]) + 1
+            dp['P'], dp['S'] = max(dp['R'], dp['S']), max(dp['R'], dp['P']) + 1
         else:  # elif 'S' == c:
-            dp[2], dp[0] = max(dp[0], dp[1]), max(dp[1], dp[2]) + 1
-    print(max(dp))
+            dp['S'], dp['R'] = max(dp['R'], dp['P']), max(dp['P'], dp['S']) + 1
+    print(max(dp.values()))
 
 
 if __name__ == '__main__':
