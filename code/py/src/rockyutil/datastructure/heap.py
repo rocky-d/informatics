f588a330e8827a9fd971341daafc3ab58dd64f40
Rocky Haotian Du <2712479005@qq.com>
2024-08-03 22:35:15 +0800

    Updated

--- code/py/src/rockyutil/datastructure/heap.py
+++ code/py/src/rockyutil/datastructure/heap.py
@@ -74,7 +74,8 @@ class Heap(object):
 
 
 if __name__ == '__main__':
-    heap = Heap([9, 13, 8, 1, 2, 3, 4, 5], reverse = True)
+    nums = [9, 13, 8, 1, 2, 3, 4, 5]
+    heap = Heap(nums, reverse = True)
 
     print(heap.peekall())
     heap.push(7)
