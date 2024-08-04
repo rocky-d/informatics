4105e2525afbfbe1f8ee435f4585d6ff6be74e01
Rocky Haotian Du <2712479005@qq.com>
2024-08-05 01:04:01 +0800

    Updated

--- code/py/src/pintia/undone/caip/caip2024bk2/qE.py
+++ code/py/src/pintia/undone/caip/caip2024bk2/qE.py
@@ -1,4 +1,6 @@
-from math import inf
+from rockyutil.datastructure.prefsdiffs import Prefs2D
+
+inf = 1_000_000_000
 
 
 def main() -> None:
@@ -16,22 +18,23 @@ def main() -> None:
     # print(*grid, sep = '\n',end = '\n\n')
     while True:
         score = -inf
+        prefs2d = Prefs2D(grid, start = 0)
         for x1 in range_n:
             for y1 in range_n:
                 for x2 in range(x1, n):
                     for y2 in range(y1, n):
-                        val = sum(sum(grid[row][y1:y2 + 1]) for row in range(x1, x2 + 1))
+                        val = prefs2d.sum((x1, y1), (x2, y2))
                         if score < val:
                             score = val
                             x11, y11, x22, y22 = x1, y1, x2, y2
         if score <= 0:
             break
-        total += score
-        operations.append(f"({x11 + 1}, {y11 + 1}) ({x22 + 1}, {y22 + 1}) {score}")
         for i in range(x11, x22 + 1):
-            grid[i] = [-inf] * (y22 - y11 + 1) + grid[i][:y11] + grid[i][y22 + 1:]
+            grid[i] = [-inf] * (y22 + 1 - y11) + grid[i][:y11] + grid[i][y22 + 1:]
         # print(x11, y11, x22, y22)
         # print(*grid, sep = '\n', end = '\n\n')
+        operations.append(f"({x11 + 1}, {y11 + 1}) ({x22 + 1}, {y22 + 1}) {score}")
+        total += score
     print(*operations, sep = '\n')
     print(total)
 
