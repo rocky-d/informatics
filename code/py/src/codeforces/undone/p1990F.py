f2705ce2e2de690248bc58a61709d4452a72599d
Rocky Haotian Du <2712479005@qq.com>
2024-08-04 09:48:37 +0800

    Updated

---
+++ code/py/src/codeforces/undone/p1990F.py
@@ -0,0 +1,9 @@
+def main() -> None:
+    n, q = map(int, input().split())
+    a = map(int, input().split())
+    queries = (map(int, input().split()) for _ in range(q))
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        main()
