7b7526cb0c2ec71bec0890a3a794214cf6e2e250
Rocky Haotian Du <2712479005@qq.com>
2024-08-06 22:12:19 +0800

    Updated

--- code/py/src/nowcoder/contest/c20240806sqdx7/qI.py
+++ code/py/src/nowcoder/contest/c20240806sqdx7/qI.py
@@ -4,14 +4,11 @@ from bisect import bisect_left
 def main() -> None:
     m, k, h = map(int, input().split())
 
-    if h <= m:
-        print(h)
-        return
     if m <= k:
-        print(m)
+        print(min(h, m))
         return
     diff = m - k
-    print(bisect_left(range(h + 1), h, lo = m, key = lambda mid: mid + k * (1 + (mid - m) // diff)))
+    print(bisect_left(range(h + 1), h, lo = 0, key = lambda mid: mid + k * max(0, 1 + (mid - m) // diff)))
 
 
 if __name__ == '__main__':
