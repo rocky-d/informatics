7d507e89c812c5a5612bbcb2795a061d9edda1e7
Rocky Haotian Du <2712479005@qq.com>
2024-08-06 09:29:46 +0800

    Updated

--- code/py/src/leetcode/done/until20240801/p3016.py
+++ code/py/src/leetcode/done/until20240801/p3016.py
@@ -5,8 +5,8 @@ class Solution:
     def minimumPushes(self, word: str) -> int:
         ans = 0
         buttons = 8
-        for count in sorted(Counter(word).values(), reverse = True):
-            ans += count * (buttons // 8)
+        for cnt in sorted(Counter(word).values(), reverse = True):
+            ans += cnt * (buttons // 8)
             buttons += 1
         return ans
 
