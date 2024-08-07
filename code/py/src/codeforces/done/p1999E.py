b2aa5bf21c6f523ad014adbe272d441373774d88
Rocky Haotian Du <2712479005@qq.com>
2024-08-07 21:54:32 +0800

    Updated

--- code/py/src/codeforces/done/p1999E.py
+++ code/py/src/codeforces/done/p1999E.py
@@ -3,19 +3,19 @@ from itertools import accumulate
 
 
 @lru_cache(maxsize = None)
-def func(num: int) -> int:
+def dfs(num: int) -> int:
     if 0 == num:
         return 0
-    return 1 + func(num // 3)
+    return 1 + dfs(num // 3)
 
 
-prefs = list(accumulate((func(num = num) for num in range(200_001)), initial = 0))
+prefs = list(accumulate((dfs(num = num) for num in range(200_001)), initial = 0))
 
 
 def main() -> None:
     l, r = map(int, input().split())
 
-    print(func(num = l) + prefs[r + 1] - prefs[l])
+    print(dfs(num = l) + prefs[r + 1] - prefs[l])
 
 
 if __name__ == '__main__':
