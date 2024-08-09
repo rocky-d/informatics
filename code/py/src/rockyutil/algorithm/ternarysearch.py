83a921caf900c420b903c3d4b89ef79f07b6eb2b
Rocky Haotian Du <2712479005@qq.com>
2024-08-09 21:53:32 +0800

    Updated

--- code/py/src/rockyutil/algorithm/ternarysearch.py
+++ code/py/src/rockyutil/algorithm/ternarysearch.py
@@ -1,3 +1,6 @@
+from bisect import bisect_left, bisect_right
+
+
 # [lo, hi]
 def ternary_search_cc(lo, hi, check):  # [lo, hi)
     hi -= 1
@@ -55,4 +58,34 @@ def ternary_search_oo(lo, hi, check):  # [lo, hi)
 
 
 if __name__ == '__main__':
-    ...
+    nums, target = [0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9], 5
+    lo, hi = 0, len(nums)
+    key = lambda x: x
+    func = lambda mid: key(nums[mid])
+    check = lambda mid1, mid2: +1 if func(mid2) < target else 0 if func(mid1) < target else -1
+    print(
+        bisect_left(nums, target, lo = lo, hi = hi, key = key),
+        bisect_left(range(hi), target, lo = lo, key = func),
+        lo + bisect_left(range(lo, hi), target, key = func),
+    )
+    print(
+        ternary_search_cc(lo, hi, check),
+        ternary_search_co(lo, hi, check),
+        ternary_search_oo(lo, hi, check),
+    )
+
+    nums, target = [0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9], 5
+    lo, hi = 0, len(nums)
+    key = lambda x: x
+    func = lambda mid: key(nums[mid])
+    check = lambda mid1, mid2: +1 if func(mid2) <= target else 0 if func(mid1) <= target else -1
+    print(
+        bisect_right(nums, target, lo = lo, hi = hi, key = key),
+        bisect_right(range(hi), target, lo = lo, key = func),
+        lo + bisect_right(range(lo, hi), target, key = func),
+    )
+    print(
+        ternary_search_cc(lo, hi, check),
+        ternary_search_co(lo, hi, check),
+        ternary_search_oo(lo, hi, check),
+    )
