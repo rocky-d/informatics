38302f24fc532aa3ad064515d83a2a48c83847d3
Rocky Haotian Du <2712479005@qq.com>
2024-08-09 20:54:41 +0800

    Updated

---
+++ code/py/src/codeforces/done/p1999G2.py
@@ -0,0 +1,31 @@
+def check(mid1: int, mid2: int) -> int:
+    print('?', mid1, mid2, flush = True)
+    x = int(input())
+    if x == mid1 * mid2:
+        res = +1
+    elif x == mid1 * (mid2 + 1):
+        res = 0
+    else:  # elif x == (mid1 + 1) * (mid2 + 1):
+        res = -1
+    return res
+
+
+def main() -> None:
+    lo, hi = 1, 1000
+    while 1 < hi - lo:
+        diff = hi - lo
+        mid1, mid2 = lo + diff // 3, lo + (diff << 1) // 3
+        res = check(mid1 = mid1, mid2 = mid2)
+        if +1 == res:
+            lo = mid2
+        elif 0 == res:
+            lo = mid1
+            hi = mid2
+        else:  # elif -1 == res:
+            hi = mid1
+    print('!', hi, flush = True)
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        main()
