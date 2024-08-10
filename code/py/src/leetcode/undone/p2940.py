fe2ad56afcb942ee42830f1cbda185d559769e46
Rocky Haotian Du <2712479005@qq.com>
2024-08-10 09:42:42 +0800

    Updated

---
+++ code/py/src/leetcode/undone/p2940.py
@@ -0,0 +1,24 @@
+from rockyutil.datastructure.segmenttree import SegmentTree
+from rockyutil.leetcode import *
+
+
+class Solution:
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        ans = []
+        n = len(heights)
+        root = SegmentTree(max, heights)
+        for a, b in queries:
+            if a == b:
+                ans.append(b)
+                continue
+            if a > b:
+                a, b = b, a
+            if heights[a] < heights[b]:
+                ans.append(b)
+                continue
+            b += 1
+            res = b + bisect_right(range(b, n), heights[a], key = lambda mid: root.query(b, mid + 1))
+            if res == n:
+                res = -1
+            ans.append(res)
+        return ans
