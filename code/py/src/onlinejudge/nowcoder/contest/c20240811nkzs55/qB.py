dfa42fe6b785d1d075ac0e33ee850a2787d855ad
Rocky Haotian Du <2712479005@qq.com>
2024-08-11 20:58:50 +0800

    Updated

--- code/py/src/onlinejudge/nowcoder/contest/c20240811nkzs55/qB.py
+++ code/py/src/onlinejudge/nowcoder/contest/c20240811nkzs55/qB.py
@@ -5,7 +5,8 @@ def main() -> None:
     n = int(input())
     a = map(int, input().split())
 
-    print(sum(1 for pref in accumulate(a, func = lambda x, y: (x % 10) * (y % 10)) if '6' == str(pref)[-1]))
+    a = (ai % 10 for ai in a)
+    print(sum(1 for pref in accumulate(a, func = lambda x, y: x * y % 10) if 6 == pref))
 
 
 if __name__ == '__main__':
