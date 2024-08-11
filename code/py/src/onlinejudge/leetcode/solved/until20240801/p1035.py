1b9f74191fb829c213592e2d9e6c90baf4c400ed
Rocky Haotian Du <2712479005@qq.com>
2024-08-11 10:16:33 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20240801/p1035.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p1035.py
@@ -3,10 +3,10 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums2)
-        dp = [0] + [0] * n
+        m = len(nums2)
+        dp = [0] + [0] * m
         for num1 in nums1:
-            dp_lst, dp = dp, [0] + [0] * n
+            dp_lst, dp = dp, [0] + [0] * m
             for i, num2 in enumerate(nums2, start = 1):
-                dp[i] = dp_lst[i - 1] + 1 if num1 == num2 else max(dp[i - 1], dp_lst[i])
+                dp[i] = dp_lst[i - 1] + 1 if num1 == num2 else max(dp_lst[i], dp[i - 1])
         return dp[-1]
