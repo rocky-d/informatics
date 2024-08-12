28c234327db791477eefb8bc9122e1d0261bac8d
Rocky Haotian Du <2712479005@qq.com>
2024-08-12 08:34:53 +0800

    Updated

--- code/py/src/onlinejudge/nowcoder/contest/c20240811nkzs55/qA.py
+++ code/py/src/onlinejudge/nowcoder/contest/c20240811nkzs55/qA.py
@@ -1,14 +1,7 @@
 def main() -> None:
     s = input()
 
-    n = len(frozenset(s))
-    if 1 == n:
-        ans = 0
-    elif 2 == n:
-        ans = 1
-    else:  # elif 3 == n:
-        ans = 2
-    print(ans)
+    print(len(frozenset(s)) - 1)
 
 
 if __name__ == '__main__':
