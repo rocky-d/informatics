18866f5dee1ca53becb8e7de0138ec9711f392bf
Rocky Haotian Du <2712479005@qq.com>
2024-08-14 08:31:52 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p3151.py
+++
@@ -1,6 +0,0 @@
-from onlinejudge.leetcode import *
-
-
-class Solution:
-    def isArraySpecial(self, nums: List[int]) -> bool:
-        return all(0b1 == lst ^ nxt for lst, nxt in pairwise(0b1 & num for num in nums))
--- code/py/src/onlinejudge/leetcode/solved/until20240801/p3151.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p3151.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        return all(0b1 & lst != 0b1 & nxt for lst, nxt in pairwise(nums))
+        return all(0b1 == lst ^ nxt for lst, nxt in pairwise(0b1 & num for num in nums))
