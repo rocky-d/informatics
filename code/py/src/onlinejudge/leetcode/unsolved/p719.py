f5aa3f5a37e127f35f6cbc475ddd7c2026930875
Rocky Haotian Du <2712479005@qq.com>
2024-08-14 12:20:22 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/unsolved/p719.py
+++ code/py/src/onlinejudge/leetcode/unsolved/p719.py
@@ -4,4 +4,4 @@ from onlinejudge.leetcode import *
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
         nums.sort()
-        return bisect_left(range(nums[-1] - nums[0] + 1), k, key = lambda mid: sum(i - bisect_left(nums, num - mid, hi = i + 1) for i, num in enumerate(nums)))
+        return bisect_left(range(nums[-1] - nums[0] + 1), k, key = lambda mid: sum(i - bisect_left(nums, num - mid, hi = i) for i, num in enumerate(nums)))
