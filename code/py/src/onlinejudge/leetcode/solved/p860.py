adfdc51182573e9b7f928e678c45c64929474902
Rocky Haotian Du <2712479005@qq.com>
2024-08-15 10:03:18 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p860.py
+++ code/py/src/onlinejudge/leetcode/solved/p860.py
@@ -3,24 +3,23 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        change = {5: 0, 10: 0, 20: 0}
+        fives, tens = 0, 0
         for bill in bills:
             if 5 == bill:
-                change[5] += 1
+                fives += 1
             elif 10 == bill:
-                change[10] += 1
-                if 1 <= change[5]:
-                    change[5] -= 1
+                tens += 1
+                if 1 <= fives:
+                    fives -= 1
                 else:
                     ans = False
                     break
             else:  # elif 20 == bill:
-                change[20] += 1
-                if 1 <= change[10] and 1 <= change[5]:
-                    change[10] -= 1
-                    change[5] -= 1
-                elif 3 <= change[5]:
-                    change[5] -= 3
+                if 1 <= tens and 1 <= fives:
+                    tens -= 1
+                    fives -= 1
+                elif 3 <= fives:
+                    fives -= 3
                 else:
                     ans = False
                     break
