cdaa78b23d431ffcec6d89575732a7217546bb18
Rocky Haotian Du <2712479005@qq.com>
2024-08-16 22:54:32 +0800

    Updated

--- code/py/src/onlinejudge/codeforces/unsolved/p1999F.py
+++ code/py/src/onlinejudge/codeforces/unsolved/p1999F.py
@@ -1,26 +1,18 @@
-from itertools import pairwise
+from collections import Counter
+from math import comb
 
 mod = 1_000_000_007
-
-c = [[1] + [0] * (i + 1) for i in range(200_000)]
-for i, (c_lst, c_nxt) in enumerate(pairwise(c), start = 1):
-    for j in range(i, 0, -1):
-        c_nxt[j] = c_lst[j - 1] + c_lst[j]
-        if mod is not None:
-            c_nxt[j] %= mod
 
 
 def main() -> None:
     n, k = map(int, input().split())
-    a = map(int, input().split())
+    a = input().split()
 
     ans = 0
-    a = list(a)
-    half = k // 2
-    for i in range(half, n - half):
-        if 0 == a[i]:
-            continue
-        ans += (c[i][half] + c[n - i - 1][half]) % mod
+    cnter = Counter(a)
+    cnt0, cnt1 = cnter['0'], cnter['1']
+    for i in range((k >> 1) + 1, k + 1):
+        ans += comb(cnt1, i) * comb(cnt0, k - i) % mod
         ans %= mod
     print(ans)
 
