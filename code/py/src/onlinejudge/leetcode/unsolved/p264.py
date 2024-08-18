ae2f2b6d78eac8712c9ab093e8a240f6577d7745
Rocky Haotian Du <2712479005@qq.com>
2024-08-18 21:40:44 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/unsolved/p264.py
+++ code/py/src/onlinejudge/leetcode/unsolved/p264.py
@@ -1,3 +1,16 @@
+from onlinejudge.leetcode import *
+
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        ...
+        vis = {1}
+        heap = [1]
+        for i in range(1, n):
+            num = heappop(heap)
+            for factor in 2, 3, 5:
+                nxt = num * factor
+                if nxt in vis:
+                    continue
+                vis.add(nxt)
+                heappush(heap, nxt)
+        return heappop(heap)
