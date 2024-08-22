31c1cf9d5cea5e1b2a5503be8b402404a5556c1f
Rocky Haotian Du <2712479005@qq.com>
2024-08-22 20:18:12 +0800

    Updated

---
+++ run.ps1
@@ -0,0 +1,77 @@
+param (
+    [string]$pth
+)
+
+# 判断源文件targetPth是否存在，如果不存在则退出脚本
+if (-not (Test-Path $pth)) {
+    Write-Error "Not existing file path: $pth"
+    exit
+}
+
+# 将targetPth转换为绝对路径
+$targetPth = (Get-Item $pth).FullName
+Write-Output "targetPth: $targetPth"
+
+# 获取源文件所在目录
+$targetDir = [System.IO.Path]::GetDirectoryName($targetPth)
+Write-Output "targetDir: $targetDir"
+
+# 获取源文件后缀名，不包含“.”
+$targetExt = [System.IO.Path]::GetExtension($targetPth).Substring(1)
+Write-Output "targetExt: $targetExt"
+
+# 获取主项目目录
+$mainDir = "C:\rocky_d\code\informatics"
+Write-Output "mainDir: $mainDir"
+
+# 获取子项目目录
+$subDir = "$mainDir\code\$targetExt\src"
+Write-Output "subDir: $subDir"
+
+# 添加临时环境变量PATH变量
+$tmpEnvPath = "$mainDir;$subDir;$targetDir;"
+Write-Output "tmpEnvPath: $tmpEnvPath"
+$env:PATH = "$tmpEnvPath$env:PATH"
+
+# 切换当前工作目录
+Set-Location $targetDir
+Write-Output "Get-Location: $(Get-Location)"
+
+# Clear-Host
+Write-Output "-------------------------"
+
+if ($targetExt -eq "c") {
+    # 运行c源文件
+} elseif ($targetExt -eq "cpp") {
+    # 运行cpp源文件
+} elseif ($targetExt -eq "go") {
+    # 运行go源文件
+} elseif ($targetExt -eq "java") {
+    # 运行java源文件
+} elseif ($targetExt -eq "js") {
+    # 运行js源文件
+} elseif ($targetExt -eq "kt") {
+    # 运行kt源文件
+} elseif ($targetExt -eq "lua") {
+    # 运行lua源文件
+} elseif ($targetExt -eq "py") {
+    # 设置PYTHONPATH环境变量
+    $env:PYTHONPATH = $subDir
+    # 运行py源文件
+    python $targetPth
+    # 删除PYTHONPATH环境变量
+    $env:PYTHONPATH = ""
+} elseif ($targetExt -eq "rs") {
+    # 运行rs源文件
+} elseif ($targetExt -eq "ts") {
+    # 运行ts源文件
+} else {
+    # 不支持的源文件类型
+    Write-Error "Unsupported file type: $targetExt"
+}
+
+# 切换回原目录
+Set-Location $mainDir
+
+# 删除临时环境变量PATH变量
+$env:PATH = $env:PATH.Replace($tmpEnvPath, "")
