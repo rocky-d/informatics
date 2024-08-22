be9a1f5bef1dcde74c058a7016bc9f3a5e0ed4a8
Rocky Haotian Du <2712479005@qq.com>
2024-08-22 15:14:16 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p3133.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        zeros = x.bit_length() - x.bit_count()
+        cyc = 1 << zeros
+        a, b = divmod(n - 1, cyc)
+        a, b = bin(a)[2:], bin(b)[2:]
+        s = a
+        b, i = '0' * (zeros - len(b)) + b, 0
+        for bit in bin(x)[2:]:
+            if '0' == bit:
+                s += b[i]
+                i += 1
+            else:
+                s += '1'
+        return int(s, base = 2)
+
+
+eg_n = 2
+eg_x = 7
+print(Solution().minEnd(eg_n, eg_x))
