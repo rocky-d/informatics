31886753d41090903b5217a332c176718bfa91cf
Rocky Haotian Du <2712479005@qq.com>
2024-08-23 20:55:10 +0800

    Updated

--- code/py/src/onlinejudge/nowcoder/contest/c20240823xbys99/qE.py
+++ code/py/src/onlinejudge/nowcoder/contest/c20240823xbys99/qE.py
@@ -8,12 +8,13 @@ def main() -> None:
     h = map(int, input().split())
     x = map(int, input().split())
 
-    xh = sorted(zip(x, h), key=lambda item:item[0])
+    xh = sorted(zip(x, h), key=lambda item: item[0])
     dp = list(range(n))
     for i in reversed(range(n - 1)):
         idx = bisect_right(xh, (xh[i][0] + xh[i][1], inf)) - 1
+        val = dp[idx]
         for j in range(i, idx):
-            dp[j] = dp[idx]
+            dp[j] = val
     print(sum(sorted((len(list(group)) for _, group in groupby(dp)), reverse=True)[:m]))
 
 
