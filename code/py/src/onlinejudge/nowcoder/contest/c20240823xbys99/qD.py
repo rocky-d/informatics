74ad1b844171b6026da1e61cb18a667d622a8256
Rocky Haotian Du <2712479005@qq.com>
2024-08-24 15:32:18 +0800

    Updated

--- code/py/src/onlinejudge/nowcoder/contest/c20240823xbys99/qD.py
+++ code/py/src/onlinejudge/nowcoder/contest/c20240823xbys99/qD.py
@@ -1,24 +1,22 @@
+primes = []
+tags = [False] * 2 + [True] * 9_999_999
+for num in range(2, 10_000_001):
+    if tags[num]:
+        primes.append(num)
+        for composite in range(num * num, 10_000_001, num):
+            tags[composite] = False
+
+
 def main() -> None:
     n = int(input())
     a = map(int, input().split())
 
-    a = sorted(a)
-
-    def check(mid: int) -> bool:
-        for ai in a:
-            x, y = divmod(mid, ai)
-            if 0 == y:
-                return True
-
-
-    lo, hi = 1, 10_000_000_001
-    while 1 < hi - lo:
-        mid = lo + hi >> 1
-        if check(mid=mid):
-            lo = mid
-        else:
-            hi = mid
-    print(hi)
+    a = frozenset(a)
+    for prime in primes:
+        if prime not in a:
+            ans = prime
+            break
+    print(ans)
 
 
 if __name__ == '__main__':
