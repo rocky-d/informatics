9db6803295569dfc3d91e322f36cda01c47ef480
Rocky Haotian Du <2712479005@qq.com>
2024-08-24 15:41:26 +0800

    Updated

--- run.ps1
+++ run.ps1
@@ -51,63 +51,75 @@ Set-Location $targetDir
 # Clear-Host
 # Write-Output "-------------------------"
 
-if ($targetExt -eq "c") {
-    # 设置输出文件路径
-    $resultPth = "$subOutDir\$targetNam.exe"
-    # 编译源文件
-    gcc -o $resultPth $targetPth
-    # 运行输出文件
-    . $resultPth
-} elseif ($targetExt -eq "cpp") {
-    # 设置输出文件路径
-    $resultPth = "$subOutDir\$targetNam.exe"
-    # 编译源文件
-    g++ -o $resultPth $targetPth
-    # 运行输出文件
-    . $resultPth
-} elseif ($targetExt -eq "go") {
-    # 设置输出文件路径
-    $resultPth = "$subOutDir\$targetNam.exe"
-    # 编译源文件
-    go build -o $resultPth $targetPth
-    # 运行输出文件
-    . $resultPth
-} elseif ($targetExt -eq "java") {
-    # 运行java源文件
-} elseif ($targetExt -eq "js") {
-    # 运行js源文件
-} elseif ($targetExt -eq "kt") {
-    # 运行kt源文件
-} elseif ($targetExt -eq "lua") {
-    # 设置LUA_PATH环境变量
-    $env:LUA_PATH = $subSrcDir
-    # 运行lua源文件
-    lua53 $targetPth
-    # 删除LUA_PATH环境变量
-    $env:LUA_PATH = ""
-} elseif ($targetExt -eq "py") {
-    # 设置PYTHONPATH环境变量
-    $env:PYTHONPATH = $subSrcDir
-    # 运行py源文件
-    python $targetPth
-    # 删除PYTHONPATH环境变量
-    $env:PYTHONPATH = ""
-} elseif ($targetExt -eq "rs") {
-    # 设置输出文件路径
-    $resultPth = "$subOutDir\$targetNam.exe"
-    # 编译源文件
-    rustc -o $resultPth $targetPth
-    # 运行输出文件
-    . $resultPth
-} elseif ($targetExt -eq "ts") {
-    # 运行ts源文件
-} else {
-    # 不支持的源文件类型
-    Write-Error "Unsupported file type: $targetExt"
+try {
+    if ($targetExt -eq "c") {
+        # 设置输出文件路径
+        $resultPth = "$subOutDir\$targetNam.exe"
+        # 编译源文件
+        gcc -o $resultPth $targetPth
+        # 运行输出文件
+        . $resultPth
+    }
+    elseif ($targetExt -eq "cpp") {
+        # 设置输出文件路径
+        $resultPth = "$subOutDir\$targetNam.exe"
+        # 编译源文件
+        g++ -o $resultPth $targetPth
+        # 运行输出文件
+        . $resultPth
+    }
+    elseif ($targetExt -eq "go") {
+        # 设置输出文件路径
+        $resultPth = "$subOutDir\$targetNam.exe"
+        # 编译源文件
+        go build -o $resultPth $targetPth
+        # 运行输出文件
+        . $resultPth
+    }
+    elseif ($targetExt -eq "java") {
+        # 运行java源文件
+    }
+    elseif ($targetExt -eq "js") {
+        # 运行js源文件
+    }
+    elseif ($targetExt -eq "kt") {
+        # 运行kt源文件
+    }
+    elseif ($targetExt -eq "lua") {
+        # 设置LUA_PATH环境变量
+        $env:LUA_PATH = $subSrcDir
+        # 运行lua源文件
+        lua53 $targetPth
+        # 删除LUA_PATH环境变量
+        $env:LUA_PATH = ""
+    }
+    elseif ($targetExt -eq "py") {
+        # 设置PYTHONPATH环境变量
+        $env:PYTHONPATH = $subSrcDir
+        # 运行py源文件
+        python $targetPth
+        # 删除PYTHONPATH环境变量
+        $env:PYTHONPATH = ""
+    }
+    elseif ($targetExt -eq "rs") {
+        # 设置输出文件路径
+        $resultPth = "$subOutDir\$targetNam.exe"
+        # 编译源文件
+        rustc -o $resultPth $targetPth
+        # 运行输出文件
+        . $resultPth
+    }
+    elseif ($targetExt -eq "ts") {
+        # 运行ts源文件
+    }
+    else {
+        # 不支持的源文件类型
+        Write-Error "Unsupported file type: $targetExt"
+    }
 }
-
-# 切换回原目录
-Set-Location $mainDir
-
-# 删除临时环境变量PATH变量
-$env:PATH = $env:PATH.Replace($tmpEnvPath, "")
+finally {
+    # 切换回原目录
+    Set-Location $mainDir
+    # 删除临时环境变量PATH变量
+    $env:PATH = $env:PATH.Replace($tmpEnvPath, "")
+}
