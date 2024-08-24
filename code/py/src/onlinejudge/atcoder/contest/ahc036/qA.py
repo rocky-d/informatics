90e6949e42a7692806434ec763d28a0c1cf02a27
Rocky Haotian Du <2712479005@qq.com>
2024-08-24 23:49:41 +0800

    Updated

---
+++ code/py/src/onlinejudge/atcoder/contest/ahc036/qA.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    n, m, t, la, lb = map(int, input().split())
+    uv = (map(int, input().split()) for _ in range(m))
+
+    t = map(int, input().split())
+    xy = (map(int, input().split()) for _ in range(n))
+
+
+if __name__ == '__main__':
+    main()
