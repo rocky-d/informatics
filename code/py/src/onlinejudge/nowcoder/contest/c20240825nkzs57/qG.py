197f1e8f41e5e930437c561463bcf8b9375f8d45
Rocky Haotian Du <2712479005@qq.com>
2024-08-25 21:14:04 +0800

    Updated

--- code/py/src/onlinejudge/nowcoder/contest/c20240825nkzs57/qG.py
+++ code/py/src/onlinejudge/nowcoder/contest/c20240825nkzs57/qG.py
@@ -4,20 +4,21 @@ def main() -> None:
     if 1 == n and 2 != k or 2 == n and 0 == k:
         print(-1)
         return
+    n1 = n + 1
     if 0 == k:
         ans = []
-        for i in range(1, 1 + n):
+        for i in range(1, n1):
             ans.append(i)
             ans.append(i)
         print(*ans)
         return
-    if n + 1 == k:
-        print(*range(1, 1 + n), *range(1, 1 + n))
+    if n1 == k:
+        print(*range(1, n1), *range(1, n1))
         return
     if n == k:
-        print(*range(1, 1 + n), *range(1, n - 1), n, n - 1)
+        print(*range(1, n1), *range(1, n - 1), n, n - 1)
         return
-    print(k, *range(1, 1 + n), *range(1, k), *range(k + 1, 1 + n))
+    print(k, *range(1, n1), *range(1, k), *range(k + 1, n1))
 
 
 if __name__ == '__main__':
