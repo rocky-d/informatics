87c82eb7ccc3b0c8a947156187dc4196737c1b1c
Rocky Haotian Du <2712479005@qq.com>
2024-08-25 12:32:22 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20240801/p144.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p144.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        return [] if root is None else [root.val] + self.preorderTraversal(root = root.left) + self.preorderTraversal(root = root.right)
+        return [] if root is None else [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
--- code/py/src/onlinejudge/leetcode/solved/until20240801/p145.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p145.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        return [] if root is None else self.postorderTraversal(root = root.left) + self.postorderTraversal(root = root.right) + [root.val]
+        return [] if root is None else self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
