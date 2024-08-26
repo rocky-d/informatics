8fee49696b9ab94f96f9f4c58e90b8dfe7819710
Rocky Haotian Du <2712479005@qq.com>
2024-08-26 09:29:54 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20240801/p590.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p590.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def postorder(self, root: Node) -> List[int]:
-        return [] if root is None else reduce(lambda x, y: x + y, (self.postorder(root = child) for child in root.children), []) + [root.val]
+        return [] if root is None else reduce(add, (self.postorder(child) for child in root.children), []) + [root.val]
