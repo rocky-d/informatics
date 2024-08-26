14bab7ca4dbccd758b8f21ae5d576cde48f83435
Rocky Haotian Du <2712479005@qq.com>
2024-08-26 09:40:48 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20240801/p993.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p993.py
@@ -3,15 +3,16 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        targets = {x: ..., y: ...}
+        targets = {x: None, y: None}
 
         def dfs(node: Optional[TreeNode], depth: int, parent: TreeNode) -> None:
             if node is None:
                 return
             if node.val in targets:
                 targets[node.val] = depth, parent.val
-            dfs(node = node.left, depth = depth + 1, parent = node)
-            dfs(node = node.right, depth = depth + 1, parent = node)
+            depth += 1
+            dfs(node.left, depth, node)
+            dfs(node.right, depth, node)
 
-        dfs(node = root, depth = 0, parent = TreeNode(val = 0, left = root, right = root))
+        dfs(node=root, depth=0, parent=TreeNode(val=0, left=root, right=root))
         return targets[x][0] == targets[y][0] and targets[x][1] != targets[y][1]
