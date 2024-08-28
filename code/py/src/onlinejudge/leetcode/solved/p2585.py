7fe23eb23cffe6d3b897350fe6e4857b2ab2cb55
Rocky Haotian Du <2712479005@qq.com>
2024-08-28 23:27:17 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2585.py
+++ code/py/src/onlinejudge/leetcode/solved/p2585.py
@@ -3,17 +3,14 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
-        dp = [[0, 1]] + [[0, 0] for _ in range(target)]
-        for count, mark in types:
+        dp = [1] + [0] * target
+        for cnt, mark in types:
             for vol in range(target, -1, -1):
                 marks = 0
-                for _ in range(count):
+                for _ in range(cnt):
                     marks += mark
                     if vol < marks:
                         break
-                    if dp[vol][0] < dp[vol - marks][0] + marks:
-                        dp[vol][0], dp[vol][1] = dp[vol - marks][0] + marks, dp[vol - marks][1]
-                    elif dp[vol][0] == dp[vol - marks][0] + marks:
-                        dp[vol][1] += dp[vol - marks][1]
-                        dp[vol][1] %= 1_000_000_007
-        return dp[-1][1]
+                    dp[vol] += dp[vol - marks]
+                    dp[vol] %= 1_000_000_007
+        return dp[-1]
