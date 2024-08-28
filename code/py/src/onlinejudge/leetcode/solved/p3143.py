62ee4581e61be049611efd05bc8267b35894ea01
Rocky Haotian Du <2712479005@qq.com>
2024-08-28 17:10:51 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p3143.py
+++ code/py/src/onlinejudge/leetcode/solved/p3143.py
@@ -5,9 +5,9 @@ class Solution:
     def maxPointsInsideSquare(self, points: List[List[int]], s: str) -> int:
         ans = 0
         vis = set()
-        for side, group in groupby(sorted((max(abs(x), abs(y)), c) for (x, y), c in zip(points, s)), key = lambda item: item[0]):
+        for _, group in groupby(sorted((max(abs(x), abs(y)), c) for (x, y), c in zip(points, s)), key = lambda item: item[0]):
             cnt = 0
-            for side, char in group:
+            for _, char in group:
                 if char in vis:
                     break
                 vis.add(char)
