c6ceb049f3338485bfb1e98563ac757b1b190277
Rocky Haotian Du <2712479005@qq.com>
2024-08-28 23:47:59 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2585.py
+++ code/py/src/onlinejudge/leetcode/solved/p2585.py
@@ -6,13 +6,13 @@ class Solution:
         dp = [1] + [0] * target
         mod = 1_000_000_007
         for cnt, mark in types:
-            if cnt * mark < target:
-                total = cnt * mark
+            total = cnt * mark
+            if total < target:
                 for vol in reversed(range(target + 1)):
                     for marks in range(mark, min(total, vol) + 1, mark):
                         dp[vol] += dp[vol - marks]
                         dp[vol] %= mod
-            else:  # elif cnt * mark >= target:
+            else:  # elif total >= target:
                 for vol in range(mark, target + 1):
                     dp[vol] += dp[vol - mark]
                     dp[vol] %= mod
