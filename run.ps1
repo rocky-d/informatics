7dcfe00c8d914af2d7138a6f6bcbe210587d59d5
Rocky Haotian Du <2712479005@qq.com>
2024-08-28 17:25:09 +0800

    Updated

--- run.ps1
+++ run.ps1
@@ -40,7 +40,7 @@ $subOutDir = "$subDir\out"
 # Write-Output "subOutDir: $subOutDir"
 
 # 添加临时环境变量PATH变量
-$tmpEnvPath = "$mainDir;$subDir;$targetDir;"
+$tmpEnvPath = "$mainDir;$subDir;"
 # Write-Output "tmpEnvPath: $tmpEnvPath"
 $env:PATH = "$tmpEnvPath$env:PATH"
 
