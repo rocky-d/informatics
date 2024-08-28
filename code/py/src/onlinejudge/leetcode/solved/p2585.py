69367fc7f2dbdac001cefc22163ce33450838961
Rocky Haotian Du <2712479005@qq.com>
2024-08-28 23:46:51 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2585.py
+++ code/py/src/onlinejudge/leetcode/solved/p2585.py
@@ -4,15 +4,16 @@ from onlinejudge.leetcode import *
 class Solution:
     def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
         dp = [1] + [0] * target
+        mod = 1_000_000_007
         for cnt, mark in types:
             if cnt * mark < target:
                 total = cnt * mark
                 for vol in reversed(range(target + 1)):
                     for marks in range(mark, min(total, vol) + 1, mark):
                         dp[vol] += dp[vol - marks]
-                        dp[vol] %= 1_000_000_007
+                        dp[vol] %= mod
             else:  # elif cnt * mark >= target:
                 for vol in range(mark, target + 1):
                     dp[vol] += dp[vol - mark]
-                    dp[vol] %= 1_000_000_007
+                    dp[vol] %= mod
         return dp[-1]
