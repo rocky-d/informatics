6e382da2da6b0b1943efd09590f87dde6a6fc335
Rocky Haotian Du <2712479005@qq.com>
2024-08-29 12:52:54 +0800

    Updated

--- code/py/src/library/algorithm/knapsack.py
+++ code/py/src/library/algorithm/knapsack.py
@@ -1,13 +1,35 @@
+from itertools import groupby
+
+
 def knapsack_01(items, volume):
-    dp = [0] * (1 + volume)
+    dp = [0] + [0] * volume
     for weight, value in items:
-        for vol in range(volume, weight - 1, -1):
+        for vol in reversed(range(weight, volume + 1)):
             dp[vol] = max(dp[vol], dp[vol - weight] + value)
     return dp[-1]
 
 
+def knapsack_bounded(items, volume):
+    dp = [0] + [0] * volume
+    for (weight, value), group in groupby(items):
+        count = len(list(group))
+        total = count * weight
+        if total < volume:
+            for vol in reversed(range(volume + 1)):
+                cnt = min(total, vol) // weight
+                for w, v in zip(
+                    range(weight, weight * cnt + 1, weight),
+                    range(value, value * cnt + 1, value),
+                ):
+                    dp[vol] = max(dp[vol], dp[vol - w] + v)
+        else:  # elif total >= volume:
+            for vol in range(weight, volume + 1):
+                dp[vol] = max(dp[vol], dp[vol - weight] + value)
+    return dp[-1]
+
+
 def knapsack_unbounded(items, volume):
-    dp = [0] * (1 + volume)
+    dp = [0] + [0] * volume
     for weight, value in items:
         for vol in range(weight, volume + 1):
             dp[vol] = max(dp[vol], dp[vol - weight] + value)
@@ -15,6 +37,7 @@ def knapsack_unbounded(items, volume):
 
 
 if __name__ == '__main__':
-    items, volume = [(1, 2), (2, 3), (3, 5), (4, 7)], 6
+    items, volume = [(1, 2), (1, 2), (2, 3), (3, 5), (4, 7)], 6
     print(knapsack_01(items, volume))
+    print(knapsack_bounded(items, volume))
     print(knapsack_unbounded(items, volume))
