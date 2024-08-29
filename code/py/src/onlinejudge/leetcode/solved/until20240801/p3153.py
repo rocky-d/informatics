35a339d9f07df3bca7f6e6645f29c15c2efc5925
Rocky Haotian Du <2712479005@qq.com>
2024-08-30 00:07:19 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20240801/p3153.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240801/p3153.py
@@ -5,8 +5,9 @@ class Solution:
     def sumDigitDifferences(self, nums: List[int]) -> int:
         ans = comb(len(nums), 2) * len(str(nums[0]))
         words = list(map(str, nums))
+        cnter = defaultdict(lambda: 0)
         for i in range(len(words[0])):
-            cnter = Counter()
+            cnter.clear()
             for word in words:
                 cnter[word[i]] += 1
             ans -= sum(comb(val, 2) for val in cnter.values())
