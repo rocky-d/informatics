2d051422792d266b825050f4d421067104d5f4fc
Rocky Haotian Du <2712479005@qq.com>
2024-08-29 10:31:45 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p873.py
@@ -0,0 +1,27 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        ans = 2
+        dp = {num: {} for num in arr}
+        for i, num in enumerate(arr):
+            for lst, leng in dp[num].items():
+                nxt = lst + num
+                leng += 1
+                if nxt in dp.keys():
+                    dct = dp[nxt]
+                    dct[num] = max(dct.get(num, 0), leng)
+                else:
+                    ans = max(ans, leng)
+            for j in range(i):
+                nxt = arr[j] + num
+                leng = 2
+                if nxt in dp.keys():
+                    dct = dp[nxt]
+                    dct[num] = max(dct.get(num, 0), leng)
+        return 0 if 2 == ans else ans
+
+
+eg_arr = [2, 4, 8, 10, 14, 18, 32, 50]
+print(Solution().lenLongestFibSubseq(eg_arr))
