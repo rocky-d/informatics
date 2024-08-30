07274537f4e8287583b4542a58f7222d714bb802
Rocky Haotian Du <2712479005@qq.com>
2024-08-30 11:13:18 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p1147.py
@@ -0,0 +1,24 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def longestDecomposition(self, text: str) -> int:
+        n = len(text)
+        mid = n >> 1
+        if 0b1 == 0b1 & n:
+            text = text[:mid] + text[mid] + text[mid:]
+            n += 1
+            mid += 1
+            dp = [1] * mid + [-1]
+        else:
+            dp = [1] * mid + [0]
+        for i in range(1, mid + 1):
+            lft, rit = mid - i, mid + i - 1
+            for j in range(1, i + 1):
+                if text[lft : lft + j] == text[rit - j + 1 : rit + 1]:
+                    dp[lft] = max(dp[lft], dp[lft + j] + 2)
+        return dp[0]
+
+
+eg_text = 'ghiabcdefhelloadamhelloabcdefghi'
+print(Solution().longestDecomposition(eg_text))
