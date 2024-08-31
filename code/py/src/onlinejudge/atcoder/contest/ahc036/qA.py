c36c66708bb325f310f397a6ea28ed9173be7e14
Rocky Haotian Du <2712479005@qq.com>
2024-08-31 11:46:49 +0800

    Updated

--- code/py/src/onlinejudge/atcoder/contest/ahc036/qA.py
+++ code/py/src/onlinejudge/atcoder/contest/ahc036/qA.py
@@ -1,9 +1,49 @@
+from collections import deque
+from itertools import chain, pairwise
+
+
 def main() -> None:
-    n, m, t, la, lb = map(int, input().split())
+    n, m, _, la, lb = map(int, input().split())
     uv = (map(int, input().split()) for _ in range(m))
 
+    graph = [[] for _ in range(n)]
+    for fr, to in uv:
+        graph[fr].append(to)
+        graph[to].append(fr)
+
     t = map(int, input().split())
-    xy = (map(int, input().split()) for _ in range(n))
+    # xy = (map(int, input().split()) for _ in range(n))
+
+    ans = []
+    a = list(range(n)) + [0] * (la - n)
+    ans.append(' '.join(map(str, a)))
+
+    def dfs(u: int) -> bool:
+        if u == to:
+            return True
+        for v in graph[u]:
+            if vis[v]:
+                continue
+            vis[v] = True
+            stk.append(v)
+            if dfs(v):
+                res = True
+                break
+            stk.pop()
+            vis[v] = False
+        else:
+            res = False
+        return res
+
+    for fr, to in pairwise(chain([0], t)):
+        stk = deque()
+        vis = [False] * n
+        vis[fr] = True
+        dfs(u=fr)
+        for x in stk:
+            ans.append(f"s 1 {x} 0")
+            ans.append(f"m {x}")
+    print(*ans, sep='\n')
 
 
 if __name__ == '__main__':
