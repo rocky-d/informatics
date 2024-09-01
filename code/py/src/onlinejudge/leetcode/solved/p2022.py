298cde8f94688d02877be1b1f1180754f6882a09
Rocky Haotian Du <2712479005@qq.com>
2024-09-01 09:18:28 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2022.py
+++ code/py/src/onlinejudge/leetcode/solved/p2022.py
@@ -4,6 +4,4 @@ from onlinejudge.leetcode import *
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         mn = m * n
-        if mn != len(original):
-            return []
-        return [original[i : i + n] for i in range(0, mn, n)]
+        return [original[i : i + n] for i in range(0, mn, n)] if mn == len(original) else []
