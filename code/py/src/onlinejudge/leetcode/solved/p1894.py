84861ac1a891e1630047d225e1223131aa4295a8
Rocky Haotian Du <2712479005@qq.com>
2024-09-02 14:27:47 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p1894.py
+++ code/py/src/onlinejudge/leetcode/solved/p1894.py
@@ -3,10 +3,5 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        k %= sum(chalk)
-        for idx, num in enumerate(chalk):
-            if k < num:
-                ans = idx
-                break
-            k -= num
-        return ans
+        prefs = list(accumulate(chalk))
+        return bisect_right(prefs, k % prefs[-1])
