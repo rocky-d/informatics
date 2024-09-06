49c63f308fe859fb6a02d4f23d8368853ca1b217
Rocky Haotian Du <2712479005@qq.com>
2024-09-06 12:57:43 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p3217.py
+++ code/py/src/onlinejudge/leetcode/solved/p3217.py
@@ -5,8 +5,9 @@ class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = node = ListNode(next=head)
         nums = frozenset(nums)
-        while node is not None and node.next is not None:
-            while node.next is not None and node.next.val in nums:
+        while node.next is not None:
+            if node.next.val in nums:
                 node.next = node.next.next
-            node = node.next
+            else:
+                node = node.next
         return dummy.next
