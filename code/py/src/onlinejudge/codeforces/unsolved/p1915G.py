20b7e799270a080f44cb98984e754618b1493cd5
Rocky Haotian Du <2712479005@qq.com>
2024-09-08 22:46:09 +0800

    Updated

---
+++ code/py/src/onlinejudge/codeforces/unsolved/p1915G.py
@@ -0,0 +1,36 @@
+from heapq import heappop, heappush
+from math import inf
+
+
+def main() -> None:
+    n, m = map(int, input().split())
+    uvw = (map(int, input().split()) for _ in range(m))
+
+    graph = [None] + [[] for _ in range(n)]
+    for u, v, w in uvw:
+        graph[u].append((v, w))
+        graph[v].append((u, w))
+
+    s = map(int, input().split())
+
+    s = [None] + list(s)
+    dsts = [None] + [[inf] * 1001 for _ in range(n)]
+    dsts[1][s[1]] = 0
+    heap = []
+    heappush(heap, (dsts[1][s[1]], 1, s[1]))
+    while 0 < len(heap):
+        u_dst, u, u_s = heappop(heap)
+        if u_dst != dsts[u][u_s]:
+            continue
+        for v, w in graph[u]:
+            v_dst = dsts[u][u_s] + w * u_s
+            v_s = min(u_s, s[v])
+            if v_dst < dsts[v][v_s]:
+                dsts[v][v_s] = v_dst
+                heappush(heap, (v_dst, v, v_s))
+    print(min(dsts[n]))
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        main()
