f7cf9c4606914fb6d0342ce2cff262723921558f
Rocky Haotian Du <2712479005@qq.com>
2024-09-13 09:58:57 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p435.py
@@ -0,0 +1,14 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda interval: (interval[1], interval[0]))
+        dp = [(-inf, 0)]
+        for lo, hi in intervals:
+            insort(dp, (hi, max(dp[-1][1], dp[bisect_right(dp, (lo, inf)) - 1][1] + 1)))
+        return len(intervals) - max(cnt for _, cnt in dp)
+
+
+eg_intervals = [[1, 2], [2, 3]]
+print(Solution().eraseOverlapIntervals(eg_intervals))
