7ef1ea5921fe485b96244cb2c9f1d6d5145ee931
Rocky Haotian Du <2712479005@qq.com>
2024-09-15 11:40:49 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2848.py
+++ code/py/src/onlinejudge/leetcode/solved/p2848.py
@@ -3,8 +3,8 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
-        diffs = [0] + [0] * (max(hi for _, hi in nums) + 1)
+        diffs = [0] + [0] * max(hi for _, hi in nums)
         for lo, hi in nums:
-            diffs[lo] += 1
-            diffs[hi + 1] -= 1
+            diffs[lo - 1] += 1
+            diffs[hi] -= 1
         return sum(1 for pref in accumulate(diffs, initial=0) if 0 < pref)
