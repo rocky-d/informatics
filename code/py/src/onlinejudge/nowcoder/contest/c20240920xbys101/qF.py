027ee06003cf20e7a87773e4ab978efcd95a8dbb
Rocky Haotian Du <2712479005@qq.com>
2024-09-20 21:12:38 +0800

    Updated

--- code/py/src/onlinejudge/nowcoder/contest/c20240920xbys101/qF.py
+++ code/py/src/onlinejudge/nowcoder/contest/c20240920xbys101/qF.py
@@ -1,9 +1,6 @@
 from itertools import accumulate
 
 
-mod = 998244353
-
-
 def main() -> None:
     n, w = map(int, input().split())
     a = map(int, input().split())
@@ -11,25 +8,25 @@ def main() -> None:
 
     ans = 1
     a = [None] + list(a)
+    b = [None] + list(b)
     a_idxes = {}
     for idx in range(1, 1 + n):
         a_idxes[a[idx]] = idx
-    a_set = frozenset(a[1:])
-    b = [None] + list(b)
-    prefs = [0] + list(accumulate(1 if -1 == a[i] else 0 for i in range(1, n + 1)))
-    fill = 0
-    for i in range(1, n + 1):
-        if b[i] in a_set:
-            if a_idxes[b[i]] > i + w:
+    prefs = [0] + list(accumulate(1 if -1 == a[i] else 0 for i in range(1, 1 + n)))
+    cnt = 0
+    for i in range(1, 1 + n):
+        if b[i] in a_idxes.keys():
+            if i + w < a_idxes[b[i]]:
                 ans = 0
                 break
             continue
-        if prefs[min(i + w, n)] - fill <= 0:
+        pref = prefs[min(i + w, n)] - cnt
+        if pref <= 0:
             ans = 0
             break
-        ans *= prefs[min(i + w, n)] - fill
-        ans %= mod
-        fill += 1
+        cnt += 1
+        ans *= pref
+        ans %= 998244353
     print(ans)
 
 
