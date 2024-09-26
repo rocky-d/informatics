8cfb41065541f2ea15637c1e35753d61872cc7be
Rocky Haotian Du <2712479005@qq.com>
2024-09-26 10:37:24 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2535.py
+++ code/py/src/onlinejudge/leetcode/solved/p2535.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def differenceOfSum(self, nums: List[int]) -> int:
-        return abs(sum(nums) - sum(map(int, reduce(add, map(str, nums)))))
+        return abs(sum(nums) - sum(sum(map(int, str(num))) for num in nums))
