563e26e70f817ba1a8b500f03864cd32e798c8d7
Rocky Haotian Du <2712479005@qq.com>
2024-09-28 13:33:26 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p641.py
@@ -0,0 +1,55 @@
+from onlinejudge.leetcode import *
+
+
+class MyCircularDeque:
+    def __init__(self, k: int) -> None:
+        self.que = [None] + [None] * k
+        self.n = len(self.que)
+        self.lft = 0
+        self.rit = 0
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.que[self.rit] = value
+        self.rit += 1
+        self.rit %= self.n
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.lft -= 1
+        self.lft %= self.n
+        self.que[self.lft] = value
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rit -= 1
+        self.rit %= self.n
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.lft += 1
+        self.lft %= self.n
+        return True
+
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.que[(self.rit - 1) % self.n]
+
+    def getRear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.que[self.lft]
+
+    def isEmpty(self) -> bool:
+        return self.lft == self.rit
+
+    def isFull(self) -> bool:
+        return self.lft == (self.rit + 1) % self.n
