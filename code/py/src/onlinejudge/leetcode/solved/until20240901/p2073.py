f1a973182fe56804262274e1b6abf04ba5c152f9
Rocky Haotian Du <2712479005@qq.com>
2024-09-29 22:30:52 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20240901/p2073.py
+++ code/py/src/onlinejudge/leetcode/solved/until20240901/p2073.py
@@ -3,11 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        ans = 0
-        line = tickets[k]
-        for i in range(0, k + 1):
-            ans += min(line, tickets[i])
-        line -= 1
-        for i in range(k + 1, len(tickets)):
-            ans += min(line, tickets[i])
-        return ans
+        return sum(min(tickets[k], tickets[i]) for i in range(0, k + 1)) + sum(min(tickets[k] - 1, tickets[i]) for i in range(k + 1, len(tickets)))
