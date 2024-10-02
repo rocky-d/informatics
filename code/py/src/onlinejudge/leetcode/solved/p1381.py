1a22d778f8e80057235ec12f8d777a82efab7497
Rocky Haotian Du <2712479005@qq.com>
2024-10-02 14:00:14 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p1381.py
+++ code/py/src/onlinejudge/leetcode/solved/p1381.py
@@ -1,23 +1,30 @@
-from onlinejudge.leetcode import *
-
-
 class CustomStack:
     def __init__(self, maxSize: int) -> None:
         self.stk = [None] * maxSize
         self.ptr = 0
+        self.lst = 0
 
     def push(self, x: int) -> None:
         if self.ptr == len(self.stk):
             return
-        self.stk[self.ptr] = x
+        self.stk[self.ptr] = x - self.lst
+        self.lst += self.stk[self.ptr]
         self.ptr += 1
 
     def pop(self) -> int:
         if 0 == self.ptr:
             return -1
+        res = self.lst
         self.ptr -= 1
-        return self.stk[self.ptr]
+        self.lst -= self.stk[self.ptr]
+        return res
 
     def increment(self, k: int, val: int) -> None:
-        for i in range(min(self.ptr, k)):
-            self.stk[i] += val
+        if 0 == self.ptr:
+            return
+        if self.ptr <= k:
+            self.stk[0] += val
+            self.lst += val
+        else:
+            self.stk[0] += val
+            self.stk[k] -= val
