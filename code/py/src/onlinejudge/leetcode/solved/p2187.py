bbf3ddd0862bc424167cb9f951a629e6e28a010d
Rocky Haotian Du <2712479005@qq.com>
2024-10-05 10:36:26 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p2187.py
@@ -0,0 +1,6 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        return 1 + bisect_left(range(1, 1 + totalTrips * min(time)), 1, key=lambda mid: 1 if totalTrips <= sum(mid // t for t in time) else 0)
