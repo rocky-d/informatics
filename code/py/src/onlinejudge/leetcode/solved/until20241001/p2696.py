979429d5d4a18f20cada69054e3bb70e500cc40a
Rocky Haotian Du <2712479005@qq.com>
2024-10-07 10:32:06 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241001/p2696.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241001/p2696.py
@@ -1,24 +1,27 @@
+from onlinejudge.leetcode import *
+
+
 class Solution:
     def minLength(self, s: str) -> int:
         ans = len(s)
-        stack = []
+        stk = deque()
         for char in s:
             if 'A' == char or 'C' == char:
-                stack.append(char)
+                stk.append(char)
                 continue
             elif 'B' == char:
-                if 0 < len(stack) and 'A' == stack[-1]:
-                    stack.pop(-1)
+                if 0 < len(stk) and 'A' == stk[-1]:
+                    stk.pop()
                     ans -= 2
                     continue
             elif 'D' == char:
-                if 0 < len(stack) and 'C' == stack[-1]:
-                    stack.pop(-1)
+                if 0 < len(stk) and 'C' == stk[-1]:
+                    stk.pop()
                     ans -= 2
                     continue
-            stack.clear()
+            stk.clear()
         return ans
 
 
 eg_s = 'ABFCACDB'
-print(Solution().minLength(s = eg_s))
+print(Solution().minLength(eg_s))
