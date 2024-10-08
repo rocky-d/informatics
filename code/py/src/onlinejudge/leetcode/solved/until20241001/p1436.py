461c25806c37b2cdd8bc4ad2cd1e66d06cd0cbe3
Rocky Haotian Du <2712479005@qq.com>
2024-10-08 09:50:32 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241001/p1436.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241001/p1436.py
@@ -3,8 +3,8 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        froms, tos = set(), set()
-        for city_a, city_b in paths:
-            froms.add(city_a)
-            tos.add(city_b)
-        return tuple(tos - froms)[0]
+        a_set, b_set = set(), set()
+        for a, b in paths:
+            a_set.add(a)
+            b_set.add(b)
+        return tuple(b_set - a_set)[0]
