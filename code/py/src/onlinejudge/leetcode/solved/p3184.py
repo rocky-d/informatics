d53a5ed8337ae946d5d10266f8844bb1f6157e39
Rocky Haotian Du <2712479005@qq.com>
2024-10-22 08:36:16 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p3184.py
+++ code/py/src/onlinejudge/leetcode/solved/p3184.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def countCompleteDayPairs(self, hours: List[int]) -> int:
-        return sum(1 for i, j in combinations(range(len(hours)), 2) if 0 == (hours[i] + hours[j]) % 24)
+        return sum(1 for x, y in combinations(hours, 2) if 0 == (x + y) % 24)
