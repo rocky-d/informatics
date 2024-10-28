638f15f9390e7a67b506648c9cdf78c89a42f684
Rocky Haotian Du <2712479005@qq.com>
2024-10-28 08:52:25 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2501.py
+++ code/py/src/onlinejudge/leetcode/solved/p2501.py
@@ -4,14 +4,10 @@ from onlinejudge.leetcode import *
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
         maxm = -1
-        dp = {}
+        dp = defaultdict(lambda: 0)
         for num in sorted(nums, reverse=True):
-            num_sq = num * num
-            if num_sq in dp.keys():
-                dp[num] = dp[num_sq] + 1
-                maxm = max(maxm, dp[num])
-            elif num == isqrt(num) ** 2:
-                dp[num] = 1
+            dp[num] = dp[num * num] + 1
+            maxm = max(maxm, dp[num])
         return -1 if 1 == maxm else maxm
 
 
