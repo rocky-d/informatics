17bbbcd2bf82bf168d745821137dc8710d178216
Rocky Haotian Du <2712479005@qq.com>
2024-10-29 08:18:08 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241001/p2684.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241001/p2684.py
@@ -4,13 +4,14 @@ from onlinejudge.leetcode import *
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [True for _ in grid]
+        dp = [True] * m
         for col in range(1, n):
-            dp_lst, dp = dp, [False for _ in grid]
+            dp_lst, dp = dp, [False] * m
             not_moved = True
             for row in range(m):
                 focus = grid[row][col]
-                for row_lst in map(lambda ofst: row + ofst, (-1, 0, 1)):
+                for row_lst in -1, 0, 1:
+                    row_lst += row
                     if row_lst < 0 or m <= row_lst:
                         continue
                     if dp_lst[row_lst] and grid[row_lst][col - 1] < focus:
