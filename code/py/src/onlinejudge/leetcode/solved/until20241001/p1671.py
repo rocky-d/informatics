8c99e2471f62517be32440e97d6f60af0c59fe9e
Rocky Haotian Du <2712479005@qq.com>
2024-10-30 08:03:58 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241001/p1671.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241001/p1671.py
@@ -3,7 +3,7 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def getLISArray(self, nums: List[int]) -> List[int]:
-        dp = [1 for _ in range(len(nums))]
+        dp = [1] * len(nums)
         for i, nums_i in enumerate(nums):
             for j, nums_j in enumerate(nums[:i]):
                 if nums_j < nums_i:
@@ -11,12 +11,12 @@ class Solution:
         return dp
 
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        max_len = 0
-        for left, right in zip(self.getLISArray(nums), self.getLISArray(nums[::-1])[::-1]):
-            if 1 < left and 1 < right:
-                max_len = max(max_len, left + right)
-        return len(nums) - (max_len - 1)
+        maxm = 0
+        for lft, rit in zip(self.getLISArray(nums), self.getLISArray(nums[::-1])[::-1]):
+            if 1 < lft and 1 < rit:
+                maxm = max(maxm, lft + rit)
+        return len(nums) - (maxm - 1)
 
 
 eg_nums = [2, 1, 1, 5, 6, 2, 3, 1]
-print(Solution().minimumMountainRemovals(nums = eg_nums))
+print(Solution().minimumMountainRemovals(eg_nums))
