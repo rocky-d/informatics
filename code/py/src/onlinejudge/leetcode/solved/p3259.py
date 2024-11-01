63302f76c09a179ff5b21ac54d8427bafc6f74d8
Rocky Haotian Du <2712479005@qq.com>
2024-11-01 08:22:52 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p3259.py
@@ -0,0 +1,13 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def maxEnergyBoost(self, energyDrinkA: List[int], energyDrinkB: List[int]) -> int:
+        dp = [0, 0, 0]
+        for a, b in zip(energyDrinkA, energyDrinkB):
+            dp[0], dp[1], dp[2] = (
+                max(dp[1], dp[2]),
+                max(dp[0], dp[1]) + a,
+                max(dp[0], dp[2]) + b,
+            )
+        return max(dp[1], dp[2])
