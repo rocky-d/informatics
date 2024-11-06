342b3f5f5862bdeb1f62f072b72ba36370972b81
Rocky Haotian Du <2712479005@qq.com>
2024-11-06 08:22:53 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241101/p3254.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241101/p3254.py
@@ -3,7 +3,6 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
         k -= 1
-        prefs = list(accumulate((0 if nums[i - 1] + 1 == nums[i] else 1 for i in range(1, n)), initial = 0))
-        return [nums[i] if 0 == prefs[i] - prefs[i - k] else -1 for i in range(k, n)]
+        prefs = list(accumulate((0 if nums[i - 1] + 1 == nums[i] else 1 for i in range(1, len(nums))), initial=0))
+        return [nums[i] if 0 == prefs[i] - prefs[i - k] else -1 for i in range(k, len(nums))]
