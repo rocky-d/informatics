d08f2d847008f1e08528e2d953902c44a2b0c0a3
Rocky Haotian Du <2712479005@qq.com>
2024-11-14 08:23:32 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2064.py
+++ code/py/src/onlinejudge/leetcode/solved/p2064.py
@@ -3,11 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        lo, hi = 0, max(quantities) + 1
-        while 1 < hi - lo:
-            mid = lo + hi >> 1
-            if n < sum(ceil(quantity / mid) for quantity in quantities):
-                lo = mid
-            else:
-                hi = mid
-        return hi
+        return bisect_left(range(max(quantities) + 1), -n, lo=1, key=lambda mid: -sum(ceil(quantity / mid) for quantity in quantities))
