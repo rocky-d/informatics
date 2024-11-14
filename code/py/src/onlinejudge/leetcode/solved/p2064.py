5c9ad32a8e2396cdf2fff9c304514aa3c5eb8099
Rocky Haotian Du <2712479005@qq.com>
2024-11-14 08:16:33 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2064.py
+++ code/py/src/onlinejudge/leetcode/solved/p2064.py
@@ -3,7 +3,7 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        lo, hi = 0, sum(quantities) + 1
+        lo, hi = 0, max(quantities) + 1
         while 1 < hi - lo:
             mid = lo + hi >> 1
             if n < sum(ceil(quantity / mid) for quantity in quantities):
