1b697def0444bb86fc1565d10b4a81e8f7d87018
Rocky Haotian Du <2712479005@qq.com>
2024-11-18 08:10:30 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241101/p661.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241101/p661.py
@@ -3,16 +3,17 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        m, n = len(img), len(img[0])
         ans = []
+        m, n = len(img), len(img[0])
         for i in range(m):
-            ans.append([])
+            res = []
             for j in range(n):
                 cells, sum_cells = 1, img[i][j]
-                for x_offset, y_offset in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-                    x, y = i + x_offset, j + y_offset
-                    if -1 < x < m and -1 < y < n:
+                for dx, dy in (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1):
+                    x, y = i + dx, j + dy
+                    if 0 <= x < m and 0 <= y < n:
                         cells += 1
                         sum_cells += img[x][y]
-                ans[-1].append(math.floor(sum_cells / cells))
+                res.append(math.floor(sum_cells / cells))
+            ans.append(res)
         return ans
