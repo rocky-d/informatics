3a91590ab6f61dfb7c1d8e3e0d33cdbaf577ab17
Rocky Haotian Du <2712479005@qq.com>
2024-11-23 12:12:38 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p1861.py
+++ code/py/src/onlinejudge/leetcode/solved/p1861.py
@@ -4,7 +4,7 @@ from onlinejudge.leetcode import *
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         m, n = len(box), len(box[0])
-        ans = [[''] * m for _ in range(n)]
+        ans = [[...] * m for _ in range(n)]
         for i, row in zip(reversed(range(m)), box):
             for j, cell in enumerate(row):
                 if '*' == cell:
