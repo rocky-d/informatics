08c76cead3b39d7b01186eb323cc691ab6584685
Rocky Haotian Du <2712479005@qq.com>
2024-11-26 09:45:41 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241101/p2924.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241101/p2924.py
@@ -3,8 +3,8 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        ins = [0 for _ in range(n)]
-        for a, b in edges:
-            ins[b] += 1
-        winners = [i for i, val in enumerate(ins) if 0 == val]
+        cnter = [0] * n
+        for _, b in edges:
+            cnter[b] += 1
+        winners = [i for i, cnt in enumerate(cnter) if 0 == cnt]
         return winners[0] if 1 == len(winners) else -1
