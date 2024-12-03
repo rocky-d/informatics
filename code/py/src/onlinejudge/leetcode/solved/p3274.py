72e02a61bf9c13cf7bef0ea853f11b931d3f85cd
Rocky Haotian Du <2712479005@qq.com>
2024-12-03 09:15:41 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p3274.py
+++ code/py/src/onlinejudge/leetcode/solved/p3274.py
@@ -1,6 +1,3 @@
-from onlinejudge.leetcode import *
-
-
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
         return 0b0 == 0b1 & abs(ord(coordinate1[0]) - ord(coordinate2[0])) + abs(int(coordinate1[1]) - int(coordinate2[1]))
