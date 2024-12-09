d7270442d79210592f3cb14494d8f37f863c1653
Rocky Haotian Du <2712479005@qq.com>
2024-12-09 09:19:49 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241101/p3152.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241101/p3152.py
@@ -3,5 +3,5 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        prefs = list(accumulate((0 if 0b1 == lst ^ nxt else 1 for i, (lst, nxt) in enumerate(pairwise(0b1 & num for num in nums), start = 1)), initial = 0))
+        prefs = list(accumulate((0 if 0b1 == lst ^ nxt else 1 for lst, nxt in pairwise(0b1 & num for num in nums)), initial=0))
         return [prefs[lo] == prefs[hi] for lo, hi in queries]
