71985aaf21a6d6f3c17799751393ddaa454964f7
Rocky Haotian Du <2712479005@qq.com>
2024-12-11 22:29:49 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2563.py
+++ code/py/src/onlinejudge/leetcode/solved/p2563.py
@@ -3,10 +3,10 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        ans = 0
+        cnt = 0
         nums.sort()
         for idx, num in enumerate(nums):
             idx1 = bisect_left(nums, lower - num)
             idx2 = bisect_right(nums, upper - num)
-            ans += idx2 - idx1 if not idx1 <= idx < idx2 else idx2 - idx1 - 1
-        return ans // 2
+            cnt += idx2 - idx1 if not idx1 <= idx < idx2 else idx2 - idx1 - 1
+        return cnt // 2
