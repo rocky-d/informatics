20a31f2867bcefdfa850c974043c6a7a889947dd
Rocky Haotian Du <2712479005@qq.com>
2024-12-12 10:12:38 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p2558.py
@@ -0,0 +1,15 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        heap = [-gift for gift in gifts]
+        heapify(heap)
+        for _ in range(k):
+            heappush(heap, -isqrt(-heappop(heap)))
+        return -sum(heap)
+
+
+eg_gifts = [54, 6, 34, 66, 33, 69, 43, 61, 72]
+eg_k = 7
+print(Solution().pickGifts(eg_gifts, eg_k))
