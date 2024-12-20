f98dfd8180619436fca51846616d4183d8965da9
Rocky Haotian Du <2712479005@qq.com>
2024-12-20 09:05:39 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241101/p2415.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241101/p2415.py
@@ -3,13 +3,14 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(left_node: Optional[TreeNode], right_node: Optional[TreeNode], is_odd_level: bool) -> None:
-            if left_node is None or right_node is None:
+        def dfs(lft: Optional[TreeNode], rit: Optional[TreeNode], reverse: bool) -> None:
+            if lft is None or rit is None:
                 return
-            if is_odd_level:
-                left_node.val, right_node.val = right_node.val, left_node.val
-            dfs(left_node = left_node.left, right_node = right_node.right, is_odd_level = not is_odd_level)
-            dfs(left_node = left_node.right, right_node = right_node.left, is_odd_level = not is_odd_level)
+            if reverse:
+                lft.val, rit.val = rit.val, lft.val
+            reverse = not reverse
+            dfs(lft.left, rit.right, reverse)
+            dfs(lft.right, rit.left, reverse)
 
-        dfs(left_node = root.left, right_node = root.right, is_odd_level = True)
+        dfs(lft=root.left, rit=root.right, reverse=True)
         return root
