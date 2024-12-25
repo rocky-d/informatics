68a63dfc70b8a67bbe66711eaceab4b1704dbf0a
Rocky Haotian Du <2712479005@qq.com>
2024-12-25 09:26:42 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241101/p107.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241101/p107.py
@@ -8,12 +8,12 @@ class Solution:
         if root is not None:
             que.append((root, 0))
         while 0 < len(que):
-            node, row = que.popleft()
-            if row == len(ans):
+            node, level = que.popleft()
+            if level == len(ans):
                 ans.insert(0, [])
             ans[0].append(node.val)
             if node.left is not None:
-                que.append((node.left, row + 1))
+                que.append((node.left, level + 1))
             if node.right is not None:
-                que.append((node.right, row + 1))
+                que.append((node.right, level + 1))
         return ans
