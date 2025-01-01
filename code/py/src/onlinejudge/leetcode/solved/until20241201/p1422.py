4282b0847add82cacee6cc44db97a93eee23836b
Rocky Haotian Du <2712479005@qq.com>
2025-01-01 13:08:05 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20241201/p1422.py
+++ code/py/src/onlinejudge/leetcode/solved/until20241201/p1422.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        left_zeros, right_ones = [0], [0]
+        lft_0, rit_1 = [0], [0]
         for char in s[:-1]:
-            left_zeros.append(1 + left_zeros[-1] if '0' == char else left_zeros[-1])
-        for char in reversed(s[1:]):
-            right_ones.insert(0, 1 + right_ones[0] if '1' == char else right_ones[0])
-        del left_zeros[0], right_ones[-1]
-        ans = 0
-        for i in range(len(s) - 1):
-            ans = max(ans, left_zeros[i] + right_ones[i])
-        return ans
+            lft_0.append(1 + lft_0[-1] if '0' == char else lft_0[-1])
+        for char in s[:0:-1]:
+            rit_1.insert(0, 1 + rit_1[0] if '1' == char else rit_1[0])
+        del lft_0[0], rit_1[-1]
+        return max(lft_0[i] + rit_1[i] for i in range(len(s) - 1))
 
 
 eg_s = '011101'
-print(Solution().maxScore(s = eg_s))
+print(Solution().maxScore(eg_s))
