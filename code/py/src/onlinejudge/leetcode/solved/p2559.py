017ee6ac23e3812167e74214cc85017edcce50ab
Rocky Haotian Du <2712479005@qq.com>
2025-01-02 09:49:15 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p2559.py
+++ code/py/src/onlinejudge/leetcode/solved/p2559.py
@@ -5,4 +5,4 @@ class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
         vowels = frozenset('aeiou')
         prefs = list(accumulate((1 if word[0] in vowels and word[-1] in vowels else 0 for word in words), initial=0))
-        return [prefs[rit + 1] - prefs[lft] for lft, rit in queries]
+        return [prefs[hi + 1] - prefs[lo] for lo, hi in queries]
