24992fa17ca2217d853e27dac568d0d3c605223b
Rocky Haotian Du <2712479005@qq.com>
2025-01-07 08:45:59 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20250101/p3019.py
+++ code/py/src/onlinejudge/leetcode/solved/until20250101/p3019.py
@@ -1,9 +1,6 @@
+from onlinejudge.leetcode import *
+
+
 class Solution:
     def countKeyChanges(self, s: str) -> int:
-        ans = 0
-        char_last = s[0].upper()
-        for char in s.upper()[1:]:
-            if char_last != char:
-                ans += 1
-                char_last = char
-        return ans
+        return sum(1 for lst, nxt in pairwise(s.upper()) if lst != nxt)
