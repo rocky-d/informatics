fff7f330a236e9cd42a6d0fcc9f60e97529b402c
Rocky Haotian Du <2712479005@qq.com>
2025-02-11 13:22:18 +0800

    Updated

---
+++ code/py/src/onlinejudge/leetcode/solved/p1910.py
@@ -0,0 +1,9 @@
+from onlinejudge.leetcode import *
+
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        ls = s.split(sep=part, maxsplit=1)
+        while 2 == len(ls):
+            ls = ''.join(ls).split(sep=part, maxsplit=1)
+        return ls[0]
