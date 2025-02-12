3aa91fcce379139904e112399c4fa8c381772fa7
Rocky Haotian Du <2712479005@qq.com>
2025-02-12 21:41:07 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p1760.py
+++ code/py/src/onlinejudge/leetcode/solved/p1760.py
@@ -3,4 +3,4 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        return bisect_left(range(max(nums) + 1), True, lo=1, key=lambda mid: sum((num + mid - 1) // mid - 1 for num in nums) <= maxOperations)
+        return bisect_left(range(max(nums) + 1), True, lo=1, key=lambda mid: sum((num - 1) // mid for num in nums) <= maxOperations)
