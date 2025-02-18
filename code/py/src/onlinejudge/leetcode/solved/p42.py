d0d20734f1c5e08b422347a8aa5e81a428f4a9be
Rocky Haotian Du <2712479005@qq.com>
2025-02-18 10:33:33 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/p42.py
+++ code/py/src/onlinejudge/leetcode/solved/p42.py
@@ -4,11 +4,12 @@ from onlinejudge.leetcode import *
 class Solution:
     def trap(self, height: List[int]) -> int:
         cnt = 0
-        hl = 0
-        hr = 0
-        r = range(len(height))
-        for l, r in zip(r, reversed(r)):
-            hl = max(hl, height[l])
-            hr = max(hr, height[r])
-            cnt += hl + hr
-        return cnt - hl * len(height) - sum(height)
+        h = 0
+        for hi in height:
+            h = max(h, hi)
+            cnt += h
+        h = 0
+        for hi in reversed(height):
+            h = max(h, hi)
+            cnt += h
+        return cnt - h * len(height) - sum(height)
