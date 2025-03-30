a010d2be3a1b74ea6abf3f2cd0b406a74543c326
Rocky Haotian Du <2712479005@qq.com>
2025-03-30 21:46:22 +0800

    Updated

--- code/py/src/onlinejudge/leetcode/solved/until20250301/p2109.py
+++ code/py/src/onlinejudge/leetcode/solved/until20250301/p2109.py
@@ -3,11 +3,11 @@ from onlinejudge.leetcode import *
 
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        ls = list()
+        ans = ''
         i, n = 0, len(spaces)
         for idx, char in enumerate(s):
             if i < n and idx == spaces[i]:
                 i += 1
-                ls.append(' ')
-            ls.append(char)
-        return ''.join(ls)
+                ans += ' '
+            ans += char
+        return ans
