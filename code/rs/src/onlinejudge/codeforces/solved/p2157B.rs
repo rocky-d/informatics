ff906d27075261080f4f2d43acfc6fd0c10d41af
Rocky Haotian Du <2712479005@qq.com>
2026-01-22 09:07:02 +0800

    Updated

---
+++ code/rs/src/onlinejudge/codeforces/solved/p2157B.rs
@@ -0,0 +1,27 @@
+use std::io::{self, Read};
+
+fn solve<'a>(tokens: &mut impl Iterator<Item = &'a str>) {
+    let n: i32 = tokens.next().unwrap().parse().unwrap();
+    let mut x: i32 = tokens.next().unwrap().parse().unwrap();
+    let mut y: i32 = tokens.next().unwrap().parse().unwrap();
+    let s: &str = tokens.next().unwrap();
+
+    x = n - x.abs();
+    y = n - y.abs();
+    if x < 0 || y < 0 {
+        println!("NO");
+        return;
+    }
+    let f = s.chars().filter(|&c| '4' == c).count() as i32;
+    let ans = if f <= x + y { "YES" } else { "NO" };
+    println!("{}", ans);
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut tokens = buf.split_whitespace();
+    for _ in 0..tokens.next().unwrap().parse().unwrap() {
+        solve(&mut tokens);
+    }
+}
