c517ffb4d5819874fd1f3d615e64067cd2889a17
Rocky Haotian Du <116997913+rocky-d@users.noreply.github.com>
2026-01-27 10:41:06 +0800

    Updated

---
+++ code/rs/src/onlinejudge/codeforces/solved/p2153A.rs
@@ -0,0 +1,22 @@
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+fn solve<'a>(tokens: &mut impl Iterator<Item = &'a str>) {
+    let n = tokens.next().unwrap().parse::<usize>().unwrap();
+    let b = tokens.take(n);
+
+    let mut set = HashSet::new();
+    let ans = b.filter(|&x| set.insert(x)).collect::<Vec<_>>().len();
+    println!("{ans}")
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut tokens = buf.split_whitespace();
+    // let t = 1;
+    let t = tokens.next().unwrap().parse().unwrap();
+    for _ in 0..t {
+        solve(&mut tokens);
+    }
+}
