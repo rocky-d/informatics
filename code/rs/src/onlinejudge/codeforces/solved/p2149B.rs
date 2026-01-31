1d1e229d4dc448d77223d77a16ae8b2b7bc7ef8b
Rocky Haotian Du <116997913+rocky-d@users.noreply.github.com>
2026-01-31 08:29:46 +0800

    Updated

---
+++ code/rs/src/onlinejudge/codeforces/solved/p2149B.rs
@@ -0,0 +1,22 @@
+use std::io::{self, Read};
+
+fn solve<'a>(tokens: &mut impl Iterator<Item = &'a str>) {
+    let n = tokens.next().unwrap().parse::<usize>().unwrap();
+    let a = tokens.take(n).map(|x| x.parse::<i32>().unwrap());
+
+    let mut a = a.collect::<Vec<_>>();
+    a.sort();
+    let ans = (1..n).step_by(2).map(|i| a[i] - a[i - 1]).max().unwrap();
+    println!("{}", ans);
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut tokens = buf.split_whitespace();
+    // let t = 1;
+    let t = tokens.next().unwrap().parse().unwrap();
+    for _ in 0..t {
+        solve(&mut tokens);
+    }
+}
