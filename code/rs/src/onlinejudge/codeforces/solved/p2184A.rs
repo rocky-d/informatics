8904edd300090719679419f8b7adbeb05540a0e7
Rocky Haotian Du <116997913+rocky-d@users.noreply.github.com>
2026-02-16 11:00:14 +0800

    Updated

---
+++ code/rs/src/onlinejudge/codeforces/solved/p2184A.rs
@@ -0,0 +1,23 @@
+use std::io::{self, Read};
+
+fn solve<'a>(tokens: &mut impl Iterator<Item = &'a str>) {
+    let n = tokens.next().unwrap().parse::<usize>().unwrap();
+
+    if n <= 3 {
+        println!("{}", n);
+        return;
+    }
+    let ans = if 0 == n % 2 { 0 } else { 1 };
+    println!("{}", ans);
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut tokens = buf.split_whitespace();
+    // let t = 1;
+    let t = tokens.next().unwrap().parse().unwrap();
+    for _ in 0..t {
+        solve(&mut tokens);
+    }
+}
