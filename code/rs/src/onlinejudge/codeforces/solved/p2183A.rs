e2e88d7c5a7a03adbbd0eb47ba34d3352f01f07f
Rocky Haotian Du <116997913+rocky-d@users.noreply.github.com>
2026-02-20 16:45:29 +0800

    Updated

---
+++ code/rs/src/onlinejudge/codeforces/solved/p2183A.rs
@@ -0,0 +1,24 @@
+use std::io::{self, Read};
+
+fn solve<'a>(tokens: &mut impl Iterator<Item = &'a str>) {
+    let n = tokens.next().unwrap().parse::<usize>().unwrap();
+    let a = tokens.take(n).collect::<Vec<_>>();
+
+    let ans = if Some(&"1") == a.first() || Some(&"1") == a.last() {
+        "Alice"
+    } else {
+        "Bob"
+    };
+    println!("{}", ans);
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut tokens = buf.split_whitespace();
+    // let t = 1;
+    let t = tokens.next().unwrap().parse().unwrap();
+    for _ in 0..t {
+        solve(&mut tokens);
+    }
+}
